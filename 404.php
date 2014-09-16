d42dae39778994f529affb5ceaa86a3b3b95a17e
Damien Brzoska <damien.brz@gmail.com>
2014-09-16 11:03:31 +1000

    Remove chat room from blog

--- 404.php
+++ 404.php
@@ -9,7 +9,7 @@
 <div id="text">
   <h2>How embarrassing: someone forgot to close the portholes! (404)</h2>
   <p>We can't find the page you're looking for.</p>
-  <p>If you see this error continually and would like us to investigate the missing page please get in touch by emailing the <a href="mailto:support@getvero.com">help desk</a> or joining our <a href="https://www.hipchat.com/g7f50rxXN" target="_blank">chat room</a>.</p> <p>We apologize for the inconvenience.</p>
+  <p>If you see this error continually and would like us to investigate the missing page please get in touch by emailing the <a href="mailto:support@getvero.com">help desk</a>.</p> <p>We apologize for the inconvenience.</p>
 </div>
 
 <?php
