4b3964ff78c2f83134bb3a53cf3db278353f22e2
Chris Hexton <chris.hexton@gmail.com>
2014-11-12 16:21:01 -0800

    fix jobs

--- single-jobs.php
+++ single-jobs.php
@@ -3,6 +3,12 @@
 Template Name: Jobs Single
 */
 
+//Remove header
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_open', 5 );
+remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+remove_action( 'genesis_entry_header', 'genesis_post_info', 12 );
+remove_action( 'genesis_entry_header', 'genesis_entry_header_markup_close', 15 );
+
 //Custom sidebar
 remove_action( 'genesis_after_content', 'genesis_get_sidebar' );
 add_action( 'genesis_after_content', 'get_job_sidebar' );
