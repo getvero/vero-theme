856d23a369079219a5183638954facecf86fc27b
Chris Hexton <chris.hexton@gmail.com>
2015-01-13 15:56:30 +1100

    push to other

--- lib/custom_post_types/guides.php
+++ lib/custom_post_types/guides.php
@@ -37,7 +37,7 @@ function disable_for_cpt( $default ) {
 
 //Force full width layout
 function guides_layout($opt) {
-  if ( 'guides' == get_post_type() )
+  if ( 'guides' == get_post_type() && !is_home() )
     $opt = 'full-width-content';
     return $opt;
 }
