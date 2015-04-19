a27cd7ae75fc788b9c042c5ae2f45a11c0b2f370
Chris Hexton <chris.hexton@gmail.com>
2015-04-19 16:27:58 -0700

    fix menu

--- assets/scripts/responsive-menu.js
+++ assets/scripts/responsive-menu.js
@@ -1,18 +1,16 @@
-( function( window, $, undefined ) {
-'use strict';
+  'use strict';
 
-$( 'nav' ).before( '<button class="menu-toggle" role="button" aria-pressed="false"></button>' ); // Add toggles to menus
-$( 'nav .sub-menu' ).before( '<button class="sub-menu-toggle" role="button" aria-pressed="false"></button>' ); // Add toggles to sub menus
+  jQuery( 'nav' ).before( '<button class="menu-toggle" role="button" aria-pressed="false"></button>' ); // Add toggles to menus
+  jQuery( 'nav .sub-menu' ).before( '<button class="sub-menu-toggle" role="button" aria-pressed="false"></button>' ); // Add toggles to sub menus
 
-// Show/hide the navigation
-$( '.menu-toggle, .sub-menu-toggle' ).on( 'click', function() {
-var $this = $( this );
-$this.attr( 'aria-pressed', function( index, value ) {
-return 'false' === value ? 'true' : 'false';
-});
+  // Show/hide the navigation
+  jQuery( '.menu-toggle, .sub-menu-toggle' ).on( 'click', function() {
+  var $this = jQuery( this );
+  $this.attr( 'aria-pressed', function( index, value ) {
+  return 'false' === value ? 'true' : 'false';
+  });
 
-$this.toggleClass( 'activated' );
-$this.next( 'nav, .sub-menu' ).slideToggle( 'fast' );
+  $this.toggleClass( 'activated' );
+  $this.next( 'nav, .sub-menu' ).slideToggle( 'fast' );
 
-});
-});
+  });
