71c6c44900e3bd14dd79bd2dd83c25ed4ee10093
Rory Colsell <probablyrory@gmail.com>
2015-09-01 16:26:03 +1000

    Prevent  FOUT - Flash of Unstyled Text

--- scripts.js
+++ scripts.js
@@ -166,4 +166,13 @@
     }
   });
 
+  //// Prevent the flash that happens before the font is loaded
+  // first, create the object that contains
+  // configuration variables
+  MTIConfig = {};
+
+  // next, add a variable that will control
+  // whether or not FOUT will be prevented
+  MTIConfig.EnableCustomFOUTHandler = true // true = prevent FOUT
+
 }).call(this);
