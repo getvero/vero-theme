4516c00fdcf184ce50360aa5dfc9dc038da3fd24
Rory Colsell <probablyrory@gmail.com>
2015-11-18 19:09:48 +1100

    homepage structure

--- page-homepage.php
+++ page-homepage.php
@@ -3,49 +3,54 @@
 ?>
 
 <section id="hero">
-  <div class="content">
-    <h1> <span class="emphesis emphesis-primary">Advanced Email<br></span> for <span class="emphesis emphesis-secondary">Product Companies</span></h1>
-    <p>Get your messages past all the noise.<br/>Its time to make sure you're sending the <i>right</i> message, to the <i>right</i> people, at the <i>right</i> time.</p>
-    <form class="horizontal-signup-form">
-      <input class="form-control" type="email" placeholder="Email Address" name="email">
-      <input class="btn btn-success" type="submit" value="Start your free trial">
-    </form>
-  </div>
-  <div class="image">
-    <img class="stick-right" src="/wp-content/themes/vero/assets/images/home/hero@2x.png"/>
+  <div class="inner clean-padding">
+    <div class="content">
+      <h1><span class="emphesis emphesis-primary">Advanced Email<br></span> for <span class="emphesis emphesis-secondary">B2C Businesses</span></h1>
+      <p>Get your messages past all the noise.<br/>Its time to make sure you're sending the <i>right</i> message, to the <i>right</i> people, at the <i>right</i> time.</p>
+      <form class="horizontal-signup-form">
+        <input class="form-control" type="email" placeholder="Email Address" name="email">
+        <input class="btn btn-success" type="submit" value="Start your free trial">
+      </form>
+      <p class="small">No credit card required</p>
+    </div>
+    <div class="image">
+      <img class="stick-right" src="/wp-content/themes/vero/assets/images/home/hero@2x.png"/>
+    </div>
   </div>
 </section>
-<section id="logos" class="full-width">
-  <ul class="list-unstyled list-inline">
-    <li><img src="/wp-content/themes/vero/assets/images/home/logos/pipedrive@2x.png" alt="Pipedrive"></li>
-    <li><img src="/wp-content/themes/vero/assets/images/home/logos/freelancer@2x.png" alt="Freelancer"></li>
-    <li><img src="/wp-content/themes/vero/assets/images/home/logos/oneill@2x.png" alt="O'Neill"></li>
-    <li><img src="/wp-content/themes/vero/assets/images/home/logos/canva@2x.png" alt="Canva"></li>
-    <li><img src="/wp-content/themes/vero/assets/images/home/logos/tidal@2x.png" alt="TIDAL"></li>
-    <li><img src="/wp-content/themes/vero/assets/images/home/logos/aboutme@2x.png" alt="about.me"></li>
-    <li><img src="/wp-content/themes/vero/assets/images/home/logos/sphero@2x.png" alt="Sphero"></li>
-  </ul>
+<section id="logos">
+  <div class="inner clean-padding full-width">
+    <ul class="unstyled-list inline-list">
+      <li class="logo pipedrive"><img src="/wp-content/themes/vero/assets/images/home/logos/pipedrive@2x.png" alt="Pipedrive"></li>
+      <li class="logo freelancer"><img src="/wp-content/themes/vero/assets/images/home/logos/freelancer@2x.png" alt="Freelancer"></li>
+      <li class="logo oneill"><img src="/wp-content/themes/vero/assets/images/home/logos/oneill@2x.png" alt="O'Neill"></li>
+      <li class="logo canva"><img src="/wp-content/themes/vero/assets/images/home/logos/canva@2x.png" alt="Canva"></li>
+      <li class="logo tidal"><img src="/wp-content/themes/vero/assets/images/home/logos/tidal@2x.png" alt="TIDAL"></li>
+      <li class="logo aboutme"><img src="/wp-content/themes/vero/assets/images/home/logos/aboutme@2x.png" alt="about.me"></li>
+      <li class="logo sphero"><img src="/wp-content/themes/vero/assets/images/home/logos/sphero@2x.png" alt="Sphero"></li>
+    </ul>
+  </div>
 </section>
 <section id="features-summary">
   <div class="inner center-text">
     <h5 class="section-label">What is Vero?</h5>
     <h2>Your single place to manage, test and send all your teams marketing or product emails</h2>
-    <ul class="list-unstyled list-inline feature-list">
+    <ul class="unstyled-list inline-list feature-list">
       <li>
         <img src="/wp-content/themes/vero/assets/images/home/feature-email.png"/>
-        <h3 class="feature-title">Deliver useful email experiences</h3>
+        <h4 class="feature-title">Deliver useful email experiences</h4>
         <p class="feature-desc">Engage your customers with messages perfectly tailored to them beased on their activity in your application or on your website.</p>
         <a href="/features/email">Learn More</a>
       </li>
       <li>
         <img src="/wp-content/themes/vero/assets/images/home/feature-target.png"/>
-        <h3 class="feature-title">Intelligently target your customers</h3>
+        <h4 class="feature-title">Intelligently target your customers</h4>
         <p class="feature-desc">Capture scattered meta data in one tool, not three or more, and segment in real time, using a visual rule builder.</p>
         <a href="/features/customers">Learn More</a>
       </li>
       <li>
         <img src="/wp-content/themes/vero/assets/images/home/feature-data.png"/>
-        <h3 class="feature-title">Spend less time in the code</h3>
+        <h4 class="feature-title">Spend less time in the code</h4>
         <p class="feature-desc">Track data accross any platform with Vero and empower your entire team with hassle free user insights and campaign creation.</p>
         <a href="/features/data">Learn More</a>
       </li>
@@ -53,53 +58,59 @@
   </div>
 </section>
 <section class="features" id="features-detail">
-  <div class="feature">
+  <div id="feature-crm" class="inner">
+    <div class="image">
+      <img src="/wp-content/themes/vero/assets/images/home/feature-crm@2x.png" width="471"/>
+    </div>
     <div class="content">
-      <h2>Email marketing meets CRM</h2>
+      <h2 class="content-title">Email marketing meets CRM</h2>
       <p>Every customer has a central record in Vero. All of their data is stored and organised so you can email them like the <strong>people</strong> they are.</p>
       <a href="/features/customers">Find out more</a>
     </div>
-    <div class="image">
-      <img src="/wp-content/themes/vero/assets/images/home/feature-crm@2x.png"/>
-    </div>
   </div>
-  <div class="feature">
+  <div id="feature-email-types" class="inner">
     <div class="content">
-      <h2>Email marketing meets CRM</h2>
+      <h2 class="content-title">No-limit emails for the whole team</h2>
       <p>Whether you are a team of one or 300, managing your campaigns is no problem. Easily run all of your <strong>promotional</strong>, <strong>product</strong> and <strong>transactional</strong> emails in one place.</p>
       <a href="/features/email">Find out more</a>
     </div>
     <div class="image">
-      <img class="stick-right" src="/wp-content/themes/vero/assets/images/home/feature-email-types@2x.png"/>
+      <img class="stick-right" src="/wp-content/themes/vero/assets/images/home/feature-email-types@2x.png" width="578"/>
     </div>
   </div>
 </section>
-<section id="testimonial" class="full-width">
-  <div class="inner">
-    <img class="quote-logo" src="/wp-content/themes/vero/assets/images/home/company2x.png">
+<section id="testimonial">
+  <div class="inner center-text">
+    <img class="quote-logo" src="/wp-content/themes/vero/assets/images/home/logos/freelancer@2x.png">
     <h1 class="quote">"This fit perfectly with my business and made us more money too!"</h1>
     <h4 class="quote-name">Jaimie Jameson</h4>
     <h5 class="quote-job-title">Marketing Manager</h5>
   </div>
 </section>
-<section id="blocks" class="full-width">
-  <div class="support block">
+<section id="blocks">
+  <div class="support block center-text">
     <h3>98.5% of customers love our support 100% of the time!</h3>
     <p><strong>24/7</strong>, mission critical, teams in in <strong>San Fransisco</strong> and <strong>Sydney</strong>. Enterprise support too.</p>
     <div class="btn btn-outline btn-warning">View the Help Docs</div>
   </div>
-  <div class="developers block">
+  <div class="developers block center-text">
     <h3>A platform engineers can rely on</h3>
     <p>Reliable, API-based, gets out of your way and gives you the power to work like hundreds of businesses that  track <strong>1B+</strong> customer actions and send <strong>200M+</strong> emails sent every months. libraries in Ruby, Python and more. </p>
-    <div class="btn btn-outline btn-warning">Read the API Docs</div>
+    <div class="btn btn-primary btn-outline">Read the API Docs</div>
   </div>
 </section>
 <section id="twitter">
   <div class="inner">
-    <h5 class="section-label center-text">What people are saying about Vero</h5>
-    <div class="tweet"></div>
-    <div class="tweet"></div>
-    <div class="tweet"></div>
+      <h5 class="section-label center-text">What people are saying about Vero</h5>
+      <div class="tweet">
+        <blockquote class="twitter-tweet" lang="en"><p lang="en" dir="ltr">Looking for a new <a href="https://twitter.com/hashtag/email?src=hash">#email</a> client? <a href="https://twitter.com/getvero">@getvero</a> is the bees knees. Co-founder <a href="https://twitter.com/chexton">@chexton</a> delivers a new standard for quality and customer service.</p>&mdash; Paris Rouzati (@parisrouz) <a href="https://twitter.com/parisrouz/status/593558485087846400">April 29, 2015</a></blockquote>
+      </div>
+      <div class="tweet">
+        <blockquote class="twitter-tweet" lang="en"><p lang="en" dir="ltr">Thanks <a href="https://twitter.com/getvero">@getvero</a>, we love working with you guys too!</p>&mdash; Ausmed (@Ausmed) <a href="https://twitter.com/Ausmed/status/654504020850032640">October 15, 2015</a></blockquote>
+      </div>
+      <div class="tweet">
+        <blockquote class="twitter-tweet" lang="en"><p lang="en" dir="ltr">Just sent an email to the <a href="https://twitter.com/getvero">@getvero</a> team that started with &quot;omg&quot; and ended with &quot;thank you thank you thank you&quot;. They are my favourite.</p>&mdash; Ophelie Lechat (@OphelieLechat) <a href="https://twitter.com/OphelieLechat/status/646560855128080384">September 23, 2015</a></blockquote>
+      </div>
   </div>
 </section>
 <section id="call-to-action" class="center-text">
