dcd6923f7966730f6c5775bc42245db557eeeb99
Chris Hexton <chris.hexton@gmail.com>
2016-03-21 13:47:24 +1100

    adjust image and adjust copy

--- page-multi-language-campaigns.php
+++ page-multi-language-campaigns.php
@@ -12,25 +12,25 @@
 <section id="multi-language-hero" class="hero border-bottom">
   <div class="inner center-text">
     <h1 class="hero-title">The simple way to send your multi language emails</h1>
-    <p>Send multiple language variations inside a single campaign. Thats it, no fancy pants stuff.</p>
+    <p>Send multiple language variations inside a single campaign. That's it, no fancy stuff.</p>
     <img class="center-block" src="/wp-content/themes/vero/assets/images/landing-pages/i18n/language-templates.png" srcset="/wp-content/themes/vero/assets/images/landing-pages/i18n/language-templates@2x.png 2x">
   </div>
 </section>
 <section id="multi-language-services" class="border-bottom">
   <div class="inner center-text">
-    <h2>Works in conjunction with the translation services you already use.</h2>
+    <h2>Designed to work in conjunction with the translation services you already use.</h2>
     <img src="/wp-content/themes/vero/assets/images/landing-pages/i18n/language-services-logos.png" srcset="/wp-content/themes/vero/assets/images/landing-pages/i18n/language-services-logos@2x.png 2x">
   </div>
 </section>
 <section id="multi-language-steps" class="border-bottom">
   <div class="inner full-width halfs">
     <div class="left">
-      <h2>Just add HTML, we do the rest.</h2>
+      <h2>Just add content, we do the rest.</h2>
       <ul class="steps unstyled-list">
-        <li class="step"><span class="number">1</span><p class="step-title"><strong>Add a Language Variation</strong></p><p>Every campaign can have multiple content variations, add as many as you need.</p></li>
-        <li class="step"><span class="number">2</span><p class="step-title"><strong>Select a Language</strong></p><p>Choose the language that you want to send. Each language has a code, Vero checks every one of your customers for the ‘language’ property and sends the right variation to each.</p></li>
-        <li class="step"><span class="number">3</span><p class="step-title"><strong>Add HTML</strong></p><p>Paste in your translated HTML code to the language variation, you can change what ever you like in here, you can also use liquid and external attributes just like normal.</p></li>
-        <li class="step"><span class="number">4</span><p class="step-title"><strong>There is no fourth step, just send the campaign and the right variation will be sent to the right user.</strong></p></li>
+        <li class="step"><span class="number">1</span><p class="step-title"><strong>Add a Language Variation</strong></p><p>Every campaign can have multiple content variations. Add as many as you need.</p></li>
+        <li class="step"><span class="number">2</span><p class="step-title"><strong>Select a Language</strong></p><p>Choose the language that you want to send. Vero automatically checks every one of your customers for the ‘language’ property and sends the right variation to each individual.</p></li>
+        <li class="step"><span class="number">3</span><p class="step-title"><strong>Add HTML</strong></p><p>Paste in your translated HTML codeto the language variation, or use our rich text editor. You can change what ever you like in each variation and use liquid and <a href="http://help.getvero.com/articles/what-are-external-attributes.html" target="_blank">External Attributes</a> just like normal.</p></li>
+        <li class="step"><span class="number">4</span><p class="step-title"><strong>There is no fourth step! Launch the campaign and the right variation will be sent to the right user when your email is sent.</strong></p></li>
       </ul>
     </div>
     <div class="right">
@@ -41,7 +41,7 @@
 <section id="multi-language-how-it-works" class="center-text border-bottom">
   <div class="inner center-text">
     <h2>How does it work?</h2>
-    <p>Vero reserves the <code>language</code> user property to keep track of the customers language preference. This means you can keep track of each customer’s language property by adding a language code like <code>es-ES</code> (Spanish) for example. Then, when you send a campaign, we make sure that each content variation is sent to the right customers. </p><p>View a list of the <a href="#">language codes</a>.</p>
+    <p>Vero reserves the <code>language</code> user property to keep track of each customer's language preference. This means you can easily define the version each customer will receive, simply by setting their <code>language</code> using the <a href="https://en.wikipedia.org/wiki/IETF_language_tag" target="_blank">IETF language codes</a>. For example, Spanish would be <code>es-ES</code>. When you send a campaign, we automatically make sure that each content variation is sent to the right customer.</p>
     <img src="/wp-content/themes/vero/assets/images/landing-pages/i18n/languages-how-it-works.png" srcset="/wp-content/themes/vero/assets/images/landing-pages/i18n/languages-how-it-works@2x.png 2x">
   </div>
 </section>
@@ -61,7 +61,7 @@ vero.users.track!({
       </div>
       <div class="clearfix"></div>
     </div>
-    <p>Learn more about tracking events and adding new customers at <a href="http://developers.getvero.com/?javascript" target="_blank">developers.getvero.com</a></p>
+    <p>Learn more about tracking events and adding new customers at <a href="http://developers.getvero.com/?javascript" target="_blank">developers.getvero.com</a>.</p>
       
   </div>
 </section>
