90c25cb27feb105824bd52deeed3ed17297fe1da
Rory Colsell <probablyrory@gmail.com>
2016-10-05 18:15:48 +1100

    getting back external attributes html

--- page-external-attributes.php
+++ page-external-attributes.php
@@ -9,54 +9,65 @@
   
   include 'pages-shared/static-header.php';
 ?>
-<section id="top">
-  <div class="t1"></div><div class="t2"></div><div class="t3"></div><div class="t4"></div><div class="t5"></div><div class="t6"></div>
+<section id="external-attributes-hero">
   <div class="inner">
-  <div class="center-text">
-    <h1>Use your own data to personalize emails</h1>
-    <h2>Query your own data or API's to customize emails as they are sent.</h2>
-    <div id="hero-images">
-      <div class="inside">
-      <img id="bikinis-web" src="/wp-content/themes/vero/assets/images/triggered/bikinis-web.png">
-      <img id="bikinis-email" src="/wp-content/themes/vero/assets/images/triggered/bikinis-email.png">
+    <h1 class="chunk underline-blue inline-block no-bottom-margin"><span class="light">Introducting</span> <span class="semi-bold">Fusion</span></h1>
+    <h1 class="hulk">Use external data to personalise your most effective emails yet</h1>
+    <p class="large regular">Fusion lets you pull your own data from an API endpoint into your emails at time of send.</p>
+  </div>
+</section>
+<section id="external-attributes-howitworks" class="double-padding">
+  <div class="inner center-text gradient-border-bottom flush-bottom">
+    <h1 class="biggie no-bottom-margin">Plug straight into your API</h1>
+      <p class="large">Pull data out of your API and into your emails, where it belongs, in two simple steps:</p>
+      <ul class="feature-list unstyled-list halfs">
+        <li><div class="number">1</div><p><strong>Connect API endpoint</strong></p><p>Add your external API url to a campaign email using the data inspector. To get specific user or event data, attatch a Vero property to the URL with liquid.</p></li>
+        <li><div class="number">2</div><p><strong>Insert via Liquid</strong></p><p>Add liquid tags to insert the returned JSON properties or HTML to your email content. Just like this: {{external.user_region}}.</p></li>
+      </ul>
+      <img src="/wp-content/themes/vero/assets/images/external-attributes/ext-scnshot.png" srcset="/wp-content/themes/vero/assets/images/external-attributes/ext-scnshot@2x.png 2x" class="center-block">
+    </div>
+  </div>
+</section>
+<section id="external-attributes-examples" class="double-padding">
+  <div class="inner">
+    <h1 class="chunk no-bottom-margin">Finally, email is a true extension of your product</h1>
+    <ul class="feature-list unstyled-list thirds sm-center">
+      <li class="center-text">
+        <img class="center-text" src="/wp-content/themes/vero/assets/images/external-attributes/recommendations.svg">
+        <p><strong>Recommendations</strong></p>
+        <p>Insert dynamically generated HTML into product recommendation emails tailored specifically to each customer.</p>
+      </li>
+      <li class="center-text">
+        <img class="center-text" src="/wp-content/themes/vero/assets/images/external-attributes/charts.svg">
+        <p><strong>Dynamic Data &amp; Charts</strong></p>
+        <p>Keep your users engaged by sending them unique weekly updates with usage details from your product.</p>
+      </li>
+      <li class="center-text">
+        <img class="center-text" src="/wp-content/themes/vero/assets/images/external-attributes/details.svg">
+        <p><strong>Delightful Details</strong></p>
+        <p>Impress your users with personalised content pulled from services like OpenWeather or Clearbit.</p>
+      </li>
+    </ul>
+  </div>
+</section>
+<section id="external-attributes-tidal" class="darker-box floating-section double-padding flush-bottom">
+  <div class="inner">
+    <div class="half">
+      <img src="" title="Tidal">
+      <p>Tidal recommends new music to millions of customers every week. By plugging their API into Vero, Tidal has been able to consistently deliver each and every customer a personalised list of new music that results in better engagement and happier listeners.</p>
+      <q>We literally saved 100s of hours with external attributes. It just plugs in and works.</q>
+      <div class="">
+        <img src="" width="53" height="53">
+        <p class="semi-bold">Eskild</p>
+        <p>Product Manager</p>
       </div>
     </div>
-  </div>
-  </div>
-</section>
-<section id="merge-tags" class="gray">
-  <div class="inner">
-  <div class="center-text">
-    <h2>Email that is truly data-driven</h2>
-      <p>Most email software keeps your data in a silo. Vero monitors behavior on your site to give you a more complete picture of your customers.</p>
-      <p>Even more exciting, you can use that data to send incredibly personalized emails. Insert contact attributes and event attributes using our merge tags. This could be a contact's first name, the list of products in their cart (including images and links) or the number of times they've done something.</p>
-      <p>Give your emails context with Vero's personalisation tools.</p>
-    <img src="/wp-content/themes/vero/assets/images/external-attributes/dynamic-fields.png">
-  </div>
-  </div>
-</section>
-<section id="external-attributes">
-  <div class="inner">
-  <div class="center-text">
-    <h2>Pull in even more data with our world-first External Attributes</h2>
-    <div class='half'>
-      <p>Our highly advanced, one-of-a-kind, <em>External Attributes</em> feature allows you to setup emails that call your own web server or API and return HTML or JSON that can be used, dynamically in the individual outgoing email.</p>
-      <p>Use this tool to insert product recommendations, run advanced algorithms on your own dataset, or insert custom chart images.</p>
-      <p>Bring the full power of your entire database into your email marketing, layering on top of our merge tags and dynamic data.</p>
-      <p><a href="/email-marketing-demo" class="btn btn-outline btn-primary">Get a demo</a></p>
+    <div class="half">
+      <div class=""></div>
     </div>
-    <img class="half" src="/wp-content/themes/vero/assets/images/external-attributes/external-attributes.png">
-  </div>
   </div>
 </section>
-<section id="call-to-action" class="center-text">
-  <div class="inner">
-    <p class="h1">Ready to go?</p>
-    <a href="/pricing" class="btn btn-warning">Start my trial</a>
-    <a href="/features" class="btn btn-white btn-outline">Restart tour &rarr;</a>
-    <p class="small">Free trial • No credit card required</p>
-  </div>
-</section>
+
 <?php 
   no_content_genesis_footer();
   add_page_tracking_code("ProductPage");
