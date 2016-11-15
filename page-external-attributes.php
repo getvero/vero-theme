c69eeb6f092a6a9da7564ae5b76f7fd3a6cefb78
Chris Hexton <chris.hexton@gmail.com>
2016-11-15 23:44:02 +1100

    change copy –tweaks from chris

--- page-external-attributes.php
+++ page-external-attributes.php
@@ -13,30 +13,30 @@
   <div class="inner center-text">
     <img src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-logo.svg" class="center-text bottom-margin-small">
     <h1 class="no-bottom-margin center-text bottom-margin-small"><span class="semi-bold underline-blue">Fusion</span></h1>
-    <p class="medium light center-text grid-10 shift-1">Use external data to personalise your most effective emails yet. Fusion lets you <span class="semi-bold">pull data from your own api</span> into your campaigns so you can dynamically deliver accurate and relevant information to your customers.</p>
+    <p class="medium light center-text grid-10 shift-1">Emails that can talk to APIs.<br>Load data from it's <span class="semi-bold">origin</span> for greater <span class="semi-bold">accuracy</span> and <span class="semi-bold">flexibility</span>.</p>
     <img src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-hero.svg" class="center-text top-margin-medium responsive-image">
     <div class="top-margin-medium">
       <form action="https://app.getvero.com/forms/f05320d522ab0c6aba97cc5d7145a841" method='post' class="centered-form">
         <input class="form-control-large display-block" type="email" placeholder="Email Address" name="email">
-        <input class="btn btn-success display-block btn-submit top-margin-small" type="submit" value="Register for Early Access">
+        <input class="btn btn-success display-block btn-submit top-margin-small" type="submit" value="Register for early access">
       </form>
     </div> 
   </div>
 </section>
 <section id="external-attributes-howitworks" class="double-padding flush-bottom">
   <div class="inner center-text gradient-border-bottom">
-    <h1 class="chunk no-bottom-margin">Plug straight into your API</h1>
-    <p class="medium">Pull data out of your API and into your emails, where it belongs, in two simple steps.</p>
+    <h1 class="chunk no-bottom-margin">Load data from its source</h1>
+    <p class="medium">Use APIs to access data where it originates: databases, data models or third party platforms.</p>
     <img src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-api-diagram.svg" class="center-text top-margin-medium responsive-image">
     <ul class="feature-list unstyled-list halfs top-margin-small">
-      <li><p class="semi-bold medium">Connect your API endpoint</p><p class="light smallish">Add your external API url to a campaign email using the data inspector. To get specific user or event data, attatch a Vero property to the URL with Liquid.</p></li>
-      <li><p class="semi-bold medium">Add data to emails using Liquid</p><p class="light smallish">Add liquid tags to insert the returned JSON properties or HTML to your email content. Just like this: {{external.user_region}}.</p></li>
+      <li><p class="semi-bold medium">Connect an API endpoint</p><p class="light smallish">Add an external data source URL to an email using Vero's data inspector. To load specific user or event data, Vero can pass identifying attributes with every API request.</p></li>
+      <li><p class="semi-bold medium">Add data to emails using Liquid</p><p class="light smallish">Data is loaded for access via Liquid tags, enabling you access the full JSON object and insert attributes (or HTML) into your email content, just like this: <code>{{external.user_region}}</code>.</p></li>
     </ul>
   </div>
 </section>
 <section id="external-attributes-code" class="flush-bottom">
   <div class="inner center-text gradient-border-bottom">
-    <div><p class="smallish semi-bold no-margin">Example API endpoint</p><p class="no-top-margin">http://api.example.com/friend/{event.new_friend_id}</p></div>
+    <div><p class="smallish semi-bold no-margin">Example API endpoint</p><p class="no-top-margin">https://api.yoursite.com/people-you-may-know/{{user.id}}</p></div>
     <div class="ext-example">
       <div class="ext-example-json">
         <pre class="top-margin-small okaidia" data-src="/wp-content/themes/vero/snippets/fusion-json.json"></pre>
@@ -49,23 +49,23 @@
 </section>
 <section id="external-attributes-examples" class="double-padding">
   <div class="inner center-text">
-    <h1 class="chunk no-bottom-margin">Finally, email is a true extension of your product</h1>
-    <p class="medium">The exact same data powering your product on-site can now power interactions off-site.</p>
+    <h1 class="chunk no-bottom-margin">Email as a true extension of your product</h1>
+    <p class="medium">Fusion leverages the data powering your product on-site, enabling you to craft superior interactions off-site.</p>
     <ul class="feature-list unstyled-list thirds sm-center top-margin-medium">
       <li class="center-text">
         <img class="center-text top-margin-small" src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-recommendations.svg">
         <p class="semi-bold medium">Recommendations</p>
-        <p class="light smallish">Insert dynamically generated HTML into product recommendation emails tailored specifically to each customer.</p>
+        <p class="light smallish">Access data models and insert dynamically calculated product recommendation emails tailored specifically to each customer.</p>
       </li>
       <li class="center-text">
         <img class="center-text top-margin-small" src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-charts.svg">
         <p class="semi-bold medium">Dynamic Data &amp; Charts</p>
-        <p class="light smallish">Keep your users engaged by sending them unique weekly updates with usage details from your product.</p>
+        <p class="light smallish">Pull complex data structures or even fully rendered HTML charts to deliver unique weekly updates with usage details to each customer.</p>
       </li>
       <li class="center-text">
         <img class="center-text top-margin-small" src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-delights.svg">
         <p class="semi-bold medium">Delightful Details</p>
-        <p class="light smallish">Impress your users with personalised content pulled from services like OpenWeather or Clearbit.</p>
+        <p class="light smallish">Impress your users with personalised enriched data pulled from services like OpenWeather, Clearbit and more.</p>
       </li>
     </ul>
   </div>
@@ -74,7 +74,7 @@
   <div class="inner">
     <div class="half left">
       <img src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-tidal.png" src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-tidal@2x.png 2x" title="Tidal" class="bottom-margin-small">
-      <p>Tidal recommends new music to millions of customers every week. By plugging their API into Vero, Tidal has been able to consistently deliver each and every customer a personalised list of new music that results in better engagement and happier listeners.</p>
+      <p>Tidal helps millions of customers find new music. By leveraging their recommendation APIs with Fusion, Tidal has created a series of highly personalized emails that deliver the same quality on-site and in-app experience via email – ensuring each and every customer finds new music that results in both happier listeners and better engagement.</p>
       <q>We literally saved 100s of hours with external attributes. It just plugs in and works.</q>
       <div class="top-margin-medium">
         <img src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-customer-profile.png" src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-customer-profile@2x.png 2x" width="53" height="53" class="pull-left right-margin-small">
@@ -103,23 +103,23 @@
     <ul class="feature-list left-align unstyled-list halfs">
       <li class="bottom-margin-medium">
         <img src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-real-time.svg">
-        <div class="right"><p class="medium semi-bold no-top-margin">Access your data in real time</p>
-        <p class="smallish">Spend less time normalizing your data and building push-based data pipelines. With Fusion, just access the data when you need it.</p></div>
-      </li>
-      <li class="bottom-margin-medium">
-        <img src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-3rd-party.svg">
-        <div class="right"><p class="medium semi-bold no-top-margin">Works with 3rd Party API’s too…</p>
-        <p class="smallish">Delight your customers by enhancing your emails with information pulled from any number of openly available API’s.</p></div>
+        <div class="right"><p class="medium semi-bold no-top-margin">Access live data</p>
+        <p class="smallish">Spend less time normalizing your data and building push-based data pipelines. With Fusion, you can access the most accurate data, when you need it.</p></div>
       </li>
       <li>
         <img src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-manage-endpoints.svg">
-        <div class="right"><p class="medium semi-bold no-top-margin">Easily manage multiple endpoints</p>
-        <p class="smallish">Delight your customers by enhancing your emails with information pulled from any number of openly available API’s.<br/><span class="semi-bold green-text top-padding-tiny inline-block">Coming Soon</span></p></div>
+        <div class="right"><p class="medium semi-bold no-top-margin">Smart caching</p>
+        <p class="smallish">Fusion caches content as efficiently as possible. It loads all the permutations and keeps them on hand for the duration of your send.</p></div>
+      </li>
+      <li class="bottom-margin-medium">
+        <img src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-3rd-party.svg">
+        <div class="right"><p class="medium semi-bold no-top-margin">Works with third party APIs too…</p>
+        <p class="smallish">Delight your customers by enriching your emails with data pulled from any number APIs.</p></div>
       </li>
       <li>
         <img src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-secure.svg">
-        <div class="right"><p class="medium semi-bold no-top-margin">Secure Authentication</p>
-        <p class="smallish">Delight your customers by enhancing your emails with information pulled from any number of openly available API’s.<br/><span class="semi-bold green-text top-padding-tiny inline-block">Coming Soon</span></p></div>
+        <div class="right"><p class="medium semi-bold no-top-margin">Multiple Authentication Methods</p>
+        <p class="smallish">Fusion supports several common authentication methods to give you fast and easy access to your APIs.<br/><span class="semi-bold green-text top-padding-tiny inline-block">Coming Soon</span></p></div>
       </li>
     </ul>
   </div>
@@ -127,7 +127,7 @@
 <section id="external-attributes-scale" class="double-padding border-bottom">
   <div class="inner center-text flush-bottom">
     <h1 class="chunk no-bottom-margin">Built to Scale</h1>
-    <p class="medium bottom-padding-medium">As your business grows, Vero automatically scales with it, so you can concentrate on sending emails your customers love. We send over <span>250,000,000 emails a month so you know you’re in good hands.</p>
+    <p class="medium bottom-padding-medium">As your business grows, Vero scales with you. Concentrate on designing product experiences you customers love and let us handle delivering them, whether you have just one customer or 100 million.</p>
     <img src="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-scale.png" srcset="/wp-content/themes/vero/assets/images/landing-pages/external-attributes/fusion-scale@2x.png 2x" class="center-text display-block responsive-image top-margin-large">
   </div>
 </section>
@@ -135,14 +135,14 @@
   <div class="inner halfs">
     <p class="semi-bold center-text">Fusion is just the beginning...</p>
     <div class="center-text">
-      <h1 class="chunk">We beleive marketing workflows are 10x better when you have a single unified view of your customer.</h1>
-      <p class="medium">Fusion is the first step towards fully utilising the data you already have to create truly personalised customer experiences. Stop trying to manage mutliple versions of the same data in multiple apps. Go directly to the source…</p>
+      <h1 class="chunk">Data is more actionable when it's complete and accurate.</h1>
+      <p class="medium">Fusion is the first step towards designing, deploying and testing personalised product experiences powered by all of the data you store. Fusion enables you to load data from its origin, delivery better accuracy without requiring you to invest in custom API, push-based synchronization code. Stay tuned.</p>
     </div> 
     <div class="clearfix"></div>
     <div class="top-margin-medium">
       <form action="https://app.getvero.com/forms/f05320d522ab0c6aba97cc5d7145a841" method='post' class="centered-form">
         <input class="form-control-large" type="email" placeholder="Email Address" name="email">
-        <input class="btn btn-success btn-submit top-margin-small" type="submit" value="Register for Early Access">
+        <input class="btn btn-success btn-submit top-margin-small" type="submit" value="Register for early access">
       </form> 
     </div> 
   </div>
