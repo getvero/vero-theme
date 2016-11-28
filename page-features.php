35a51133e248a8db5470efe4c85abab33cc70e88
Rory Colsell <probablyrory@gmail.com>
2016-11-29 10:40:57 +1100

    features page

--- page-features.php
+++ page-features.php
@@ -10,8 +10,8 @@
   include 'pages-shared/static-header.php';
 ?>
 <section id="features-menu" class="border-bottom">
-  <div class="inner center-text large-inner">
-    <ul class="feature-list unstyled-list fourths">
+  <div class="inner center-text full-width">
+    <ul class="feature-list unstyled-list fourths full-width">
       <li><a href="#features-collection"><img src="/wp-content/themes/vero/assets/images/home/features/home-capture.svg"><p class="semi-bold small uppercase font-brand-warning center-text">Collection</p></a></li>
       <li><a href="#features-modelling"><img src="/wp-content/themes/vero/assets/images/home/features/home-model.svg"><p class="semi-bold small uppercase font-brand-success center-text">Modelling</p></a></li>
       <li><a href="#features-automation"><img src="/wp-content/themes/vero/assets/images/home/features/home-manage.svg"><p class="semi-bold small uppercase font-brand-primary center-text">Automation</p></a></li>
@@ -22,7 +22,7 @@
 <section id="features-collection" class="feature-section double-padding border-bottom">
   <div class="inner halfs large-inner left-padding-large right-padding-large">
     <p class="semi-bold center-text small uppercase font-brand-warning center-text no-top-margin bottom-margin-medium">Collection</p>
-    <div class="half left">
+    <div class="left">
       <pre class="okaidia" data-src="/wp-content/themes/vero/snippets/features-collection-js.js"></pre>
       <ul class="button-list unstyled-list thirds sm-sixths center-text">
         <li class="active"><a href="#js"><img src="/wp-content/themes/vero/assets/images/home/features/collection/data-js.png" srcset="/wp-content/themes/vero/assets/images/home/features/collection/data-js@2x.png 2x" width="40"><p class="no-margin hide-on-medium">Javascript</p></a></li>
@@ -33,7 +33,7 @@
         <li><a href="#js"><img src="/wp-content/themes/vero/assets/images/home/features/collection/data-apple.png" srcset="/wp-content/themes/vero/assets/images/home/features/collection/data-apple@2x.png 2x" width="40"><p class="no-margin hide-on-medium">Objective-C</p></a></li>
       </ul>
     </div>
-    <div class="half right left-padding-large">
+    <div class="right left-padding-large">
       <h1 class="tubs regular no-bottom-margin">Capture and store your customers data with our robust API</h1>
       <p class="smallish top-margin-medium no-bottom-margin">Identify your customers and track the actions they perform with a <span class="semi-bold">real time event stream</span> of powerful and actionable data. Getting this data into Vero is the first step towards personalised off side product experiences that perform.</p>
       <ul class="feature-list unstyled-list">
@@ -46,7 +46,7 @@
 <section id="features-modelling" class="double-padding border-bottom">
   <div class="inner halfs large-inner">
     <p class="semi-bold center-text small uppercase font-brand-success center-text bottom-margin-medium">Modelling</p>
-    <div class="half left">
+    <div class="left">
       <h1 class="tubs regular no-bottom-margin">View and model your customer data with accuracy</h1>
       <p class="smallish top-margin-medium no-bottom-margin">Mauris non tempor quam, et lacinia sapien. Mauris accumsan eros eget libero posuere vulputate. Etiam elit elit, elementum sed varius at, adipiscing vitae est..</p>
       <ul class="feature-list button-list unstyled-list">
@@ -54,18 +54,18 @@
         <li><a href="#crm" class="unstyled"><p class="semi-bold smallish no-margin">View and debug each event</p><p class="light top-margin-tiny no-bottom-margin">Get a full view all of the events and event data coming into Vero.</p></a></li>
       </ul>
     </div>
-    <div class="half right">
-      <img src=""/>
+    <div class="right">
+      <img src="/wp-content/themes/vero/assets/images/home/features/feature-modelling-1.png" srcset="/wp-content/themes/vero/assets/images/home/features/feature-modelling-1@2x.png 2x"/>
     </div>
   </div>
 </section>
 <section id="features-automation" class="double-padding border-bottom bottom-margin-medium">
   <div class="inner halfs large-inner">
     <p class="semi-bold center-text small uppercase font-brand-primary center-text">Automation</p>
-    <div class="half left">
+    <div class="left">
       <img src=""/>
     </div>
-    <div class="half right">
+    <div class="right">
       <h1 class="chunk no-bottom-margin">Main Title</h1>
       <p class="medium">Main description</p>
       <ul class="feature-list unstyled-list">
@@ -78,7 +78,7 @@
 <section id="features-deployment" class="double-padding border-bottom bottom-margin-medium">
   <div class="inner halfs large-inner">
     <p class="semi-bold center-text small uppercase font-brand-error center-text">Deployment</p>
-    <div class="half left">
+    <div class="left">
       <h1 class="chunk no-bottom-margin">Main title</h1>
       <p class="medium">Main description</p>
       <ul class="feature-list unstyled-list">
@@ -86,7 +86,7 @@
         <li><p class="semi-bold medium">Feature title</p><p class="light smallish">Feature description</p></li>
       </ul>
     </div>
-    <div class="half right">
+    <div class="right">
       <ul class="button-list unstyled-list halfs">
         <li></li>
         <li></li>
