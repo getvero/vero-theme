a2dfc9686972fd67dcc9de90eb0236542aeaf978
Chris Hexton <chris.hexton@gmail.com>
2017-01-09 16:54:53 +1100

    update copy

--- page-features.php
+++ page-features.php
@@ -12,7 +12,7 @@
 <section id="features-menu" class="border-bottom">
   <div class="inner large-inner center-text">
     <h1 class="bottom-margin-small">The definitive workflow for creating best-in-class email experiences</h1>
-    <p class="no-top-margin">Vero's helps engineers, marketers and product teams move faster to design valuable customer experiences.</p>
+    <p class="no-top-margin">Vero helps engineering, marketing and product teams move faster to design valuable customer experiences.</p>
     <ul class="feature-list feature-menu-list unstyled-list full-width">
       <li class="no-top-margin"><a href="#features-collection"><img src="/wp-content/themes/vero/assets/images/home/features/feature-menu/home-capture.svg"><p class="semi-bold small uppercase font-brand-warning center-text">Collection</p></a></li>
       <li class="no-top-margin"><a href="#features-modelling"><img src="/wp-content/themes/vero/assets/images/home/features/feature-menu/home-model.svg"><p class="semi-bold small uppercase font-brand-success center-text">Modelling</p></a></li>
@@ -55,15 +55,15 @@
     </div>
     <div class="right secondary">
       <h1 class="tubs regular no-bottom-margin">Get your data into Vero</h1>
-      <p class="smallish top-margin-medium no-bottom-margin">Connect to your <span class="semi-bold">real time event stream</span> in Segment, push real time events via our own libraries, upload adhoc CSVs and pull in data from your own APIs. Data is critical for segmentation and email personalisation, so when it comes to data we've got your back.</p>
+      <p class="smallish top-margin-medium no-bottom-margin">Connect to your <span class="semi-bold">real time event stream</span> in Segment, push real time events via Vero's own libraries, upload ad hoc CSVs and pull in data from your own APIs. Data is critical for segmentation and email personalization, so when it comes to data we've got your back.</p>
       <ul class="feature-list left-image unstyled-list">
         <li class="top-margin-large no-bottom-margin">
           <img src="/wp-content/themes/vero/assets/images/home/features/collection/segment-logo.png">
-          <p class="semi-bold smallish no-margin">Works with Segment</p><p class="light">If you’re already using segment, hooking event data into Vero is as simple as flipping a switch.</p>
+          <p class="semi-bold smallish no-margin">Works with Segment</p><p class="light">If you’re already using segment, sending event data to Vero is as simple as flipping a switch.</p>
         </li>
         <li>
           <img src="/wp-content/themes/vero/assets/images/home/features/collection/logs.svg">
-          <p class="semi-bold smallish top-margin-medium no-bottom-margin">Understand users and events</p><p class="light">Vero understands data about users and their actions ("events", in the industry). We cache this data to enable powerful segmentation and automation.</p>
+          <p class="semi-bold smallish top-margin-medium no-bottom-margin">Understand users and events</p><p class="light">Vero understands data about users and their actions ("events"). We cache this data to enable powerful segmentation and automation.</p>
         </li>
       </ul>
     </div>
@@ -74,15 +74,15 @@
     <p class="semi-bold full center-text small uppercase font-brand-success center-text bottom-margin-medium">Modelling</p>
     <div class="left secondary">
       <h1 class="tubs regular no-bottom-margin">View and model your customer data in one place</h1>
-      <p class="smallish top-margin-medium no-bottom-margin">We provide a scalable system to turn your customer data into meaningful segments. Forget "lists", forget manual maintenance of user states. Remove scattered segmentation from your application and backend architecture: design segments in our UI and store them centrally in one place instead. </p>
+      <p class="smallish top-margin-medium no-bottom-margin">We provide a scalable system to turn your customer data into meaningful segments. Forget "lists", forget manual maintenance of user states and remove scattered segmentation from your application and backend architecture. Use Vero to design segments via our UI and store them centrally in one place.</p>
       <ul class="feature-list left-image unstyled-list">
         <li class="top-margin-large no-bottom-margin">
           <img src="/wp-content/themes/vero/assets/images/home/features/modelling/segmentation.svg">
-          <p class="semi-bold smallish no-margin">Powerful Segmentation</p><p class="light">Create, save and deploy completely dynamic groups of your customers based on their attributes and actions.</p>
+          <p class="semi-bold smallish no-margin">Powerful segmentation</p><p class="light">Create, save and deploy completely dynamic groups of your customers based on their attributes and actions.</p>
         </li>
         <li>
           <img src="/wp-content/themes/vero/assets/images/home/features/modelling/customers.svg">
-          <p class="semi-bold smallish top-margin-medium no-bottom-margin">Customer Profiles</p><p class="light">Troubleshoot, investigate and deep dive on the data Vero knows about your customers.</p>
+          <p class="semi-bold smallish top-margin-medium no-bottom-margin">Customer profiles</p><p class="light">Troubleshoot, investigate and deep dive into your customer lifecycle. View all of the data Vero knows about each of your customers.</p>
         </li>
       </ul>
     </div>
@@ -129,7 +129,7 @@
       <div class="right secondary">
         <h1 class="tubs regular no-bottom-margin">Complete template manangement. Advanced scheduling design.</h1>
        <!--  <p class="smallish top-margin-medium no-bottom-margin">Manage templates for every email in your stack, centrally. Upload base templates and empower marketing and product teams to reuse and safely adjust content.</p> -->
-        <p class="smallish top-margin-medium no-bottom-margin">Craft any email automation you can dream up using Vero's simple, powerful tools: schedule one-off campaigns, recurring campaigns hourly, daily or weekly, design drip and automated campaign workflows and trigger transactional emails, all leveraging your customer data.
+        <p class="smallish top-margin-medium no-bottom-margin">Craft any email automation you can dream up using Vero's powerful tools: schedule one-off campaigns, recurring campaigns (hourly, daily or weekly), design drip and automated campaign workflows and trigger transactional emails, all in one place.
         <ul class="feature-list left-image unstyled-list">
           <li class="top-margin-large no-bottom-margin">
             <img src="/wp-content/themes/vero/assets/images/home/features/automation/campaigns.svg">
@@ -137,14 +137,14 @@
           </li>
           <li>
             <img src="/wp-content/themes/vero/assets/images/home/features/automation/templates.svg">
-            <p class="semi-bold smallish top-margin-medium no-margin">Template Management</p><p class="light">Store base templates centrally, add "editable" regions and empower the full team to design campaigns safely.</p>
+            <p class="semi-bold smallish top-margin-medium no-margin">Template management</p><p class="light">Store base templates centrally, add "editable" regions and empower your entire team to design campaigns safely.</p>
           </li>
         </ul>
       </div>
     </div>
     <div class="outlined-box top-margin-medium">
       <img src="/wp-content/themes/vero/assets/images/home/features/automation/fusion-logo.svg" class="inline-block right-margin-medium">
-      <div class="inline-block"><p class="semi-bold smallish no-bottom-margin no-top-margin"><span class="semi-bold">Fusion.</span> Load data externally</p><p class="light">Fusion loads data from your APIs at send time, giving you more flexibility in your email workflow, and enabling you to use your full CMS, data and application stack. <a href="http://www.getvero.com/fusion">Read more about Fusion</a></p></div>
+      <div class="inline-block"><p class="semi-bold smallish no-bottom-margin no-top-margin"><span class="semi-bold">Fusion.</span> Load data externally.</p><p class="light">Fusion loads data from your APIs at send time, giving you more flexibility in your email workflow, and enabling you to use your full CMS, data and application stack with Vero. <a href="http://www.getvero.com/fusion">Read more about Fusion</a></p></div>
     </div>
   </div>
 </section>
@@ -153,7 +153,7 @@
     <p class="semi-bold center-text small uppercase font-brand-error center-text">Deployment</p>
     <div class="left secondary">
       <h1 class="tubs regular no-bottom-margin">Scalability and centralised reporting</h1>
-      <p class="smallish top-margin-medium">We will deploy your content and email schedules reliabily whether it's to one or 100+ million customers. Let us handle deliverability or BYO email infrastructure platform – we'll track and centralise your reporting.</p>
+      <p class="smallish top-margin-medium">Deploy your email automation and content reliabily whether it's to one or 100+ million customers. Let us handle deliverability or BYO email infrastructure platform – we'll track and centralise your reporting.</p>
       <ul class="feature-list left-image unstyled-list">
         <li class="top-margin-medium no-bottom-margin">
           <img src="/wp-content/themes/vero/assets/images/home/features/deployment/multi-providers.svg">
@@ -163,7 +163,7 @@
         <li>
           <img src="/wp-content/themes/vero/assets/images/home/features/deployment/webhooks.svg">
           <p class="semi-bold smallish top-margin-medium no-bottom-margin">Data warehouse every interaction</p>
-          <p class="light">Output data about every email interaction to your own API using webhooks. We also support <a href="https://segment.com/sources" target="_blank">Segment's Sources</a>.</p>
+          <p class="light">Output data about every email interaction to your own API for data warehousing using webhooks. We also support <a href="https://segment.com/sources" target="_blank">Segment's Sources</a> to export data with a single click.</p>
         </li>
       </ul>
     </div>
@@ -185,7 +185,7 @@
       <ul class="button-list unstyled-list">
         <li class="padding-small">
           <!-- <img src="/wp-content/themes/vero/assets/images/home/features/deployment/64.png" width="36" class="pull-left right-margin-small"> -->
-          <p class="small no-margin"><span class="semi-bold">Don’t have your own email provider?</span> No problem, let us send your emails on our managed infrastructure. We partner with Mailgun and can support any custom configuration, including dedicated IPs and custom domains.</p>
+          <p class="small no-margin"><span class="semi-bold">Don’t have your own email provider?</span> No problem, let us send your emails on our managed infrastructure. We partner with Mailgun and can support any custom configuration, including dedicated IPs and custom domains. <a href="mailto:support@getvero.com">Email our Support Team</a>.</p>
         </li>
       </ul>
     </div>
@@ -195,21 +195,21 @@
   <div class="inner large-inner">
     <h2 class="semi-bold center-text bottom-margin-medium no-top-margin">Here's a complete list of our most popular features</h2>
     <ul class="feature-list unstyled-list thirds center-text">
-      <li class="bottom-margin-medium"><img src="/wp-content/themes/vero/assets/images/home/features/more/newsletter.svg"><p class="semi-bold">Newsletter Blasts</p><p class="light">Send static campaigns to the right customers – instantly, or at a scheduled time in the future.</p></li>
-      <li class="bottom-margin-medium"><img src="/wp-content/themes/vero/assets/images/home/features/more/reports.svg"><p class="semi-bold">Detailed Reporting</p><p class="light">Aggregate and day-by-day breakdowns of every email campaign sent through Vero. Plays nice with data warehouses too.</p></li>
-      <li class="bottom-margin-medium"><img src="/wp-content/themes/vero/assets/images/home/features/more/liquid.svg"><p class="semi-bold">Liquid Content</p><p class="light">Personalise every email using liquid tags to dynamically add customer properties.</p></li>
-      <li class="bottom-margin-medium"><img src="/wp-content/themes/vero/assets/images/home/features/more/templates.svg"><p class="semi-bold">Manage HTML templates</p><p class="light">Upload, organise and edit your fully responsive email designs and use them in all your campaigns.</p></li>
-      <li class="bottom-margin-medium"><img src="/wp-content/themes/vero/assets/images/home/features/more/editables.svg"><p class="semi-bold">Vero Editables</p><p class="light">Define specific areas of your email content that the whole team can edit without touching the HTML.</p></li>
-      <li class="bottom-margin-medium"><img src="/wp-content/themes/vero/assets/images/home/features/more/timezones.svg"><p class="semi-bold">Easy send in timezones</p><p class="light">Send your emails at the right time for every user no matter where in the world they are.</p></li>
-      <li><img src="/wp-content/themes/vero/assets/images/home/features/more/conversions.svg"><p class="semi-bold">Track conversions</p><p class="light">Go beyond just clicks and tracks the true success of each campaign using any tracked event as a conversion goal.</p></li>
-      <li><img src="/wp-content/themes/vero/assets/images/home/features/more/abtesting.svg"><p class="semi-bold">A/B Testing</p><p class="light">Multi-variate testing for subject, from addresses, content and templates across any email type, including automated workflows.</p></li>
-      <li><img src="/wp-content/themes/vero/assets/images/home/features/more/csv.svg"><p class="semi-bold">CSV data import</p><p class="light">Hit the ground running by importing all your historical event and customer data using the CSV importer.</p></li>
+      <li class="bottom-margin-medium"><img src="/wp-content/themes/vero/assets/images/home/features/more/newsletter.svg"><p class="semi-bold">Scheduled Newsletters</p><p class="light">Send one-off campaigns to individual segments – instantly, or at a scheduled time in the future.</p></li>
+      <li class="bottom-margin-medium"><img src="/wp-content/themes/vero/assets/images/home/features/more/reports.svg"><p class="semi-bold">Detailed Reporting</p><p class="light">Get aggregate and day-by-day breakdowns of every email campaign sent through Vero. Plays nice with data warehouses too.</p></li>
+      <li class="bottom-margin-medium"><img src="/wp-content/themes/vero/assets/images/home/features/more/liquid.svg"><p class="semi-bold">Liquid Content</p><p class="light">Personalise every email using the <a href="https://github.com/Shopify/liquid/wiki/Liquid-for-Designers" target="_blank">Liquid templating language</a> and utilise your customer data.</p></li>
+      <li class="bottom-margin-medium"><img src="/wp-content/themes/vero/assets/images/home/features/more/templates.svg"><p class="semi-bold">Manage HTML templates</p><p class="light">Upload, organise and edit your fully responsive email designs and reuse them in all of your campaigns.</p></li>
+      <li class="bottom-margin-medium"><img src="/wp-content/themes/vero/assets/images/home/features/more/editables.svg"><p class="semi-bold">Visual Editor</p><p class="light">Define areas of your email content that the whole team can edit in Vero's point-and-click content editor.</p></li>
+      <li class="bottom-margin-medium"><img src="/wp-content/themes/vero/assets/images/home/features/more/timezones.svg"><p class="semi-bold">Send per Timezone</p><p class="light">Schedule your emails at the right time for every customer, no matter where in the world they are.</p></li>
+      <li><img src="/wp-content/themes/vero/assets/images/home/features/more/conversions.svg"><p class="semi-bold">Track Conversions</p><p class="light">Go beyond just clicks and track the true success of each campaign using user actions as converion goals.</p></li>
+      <li><img src="/wp-content/themes/vero/assets/images/home/features/more/abtesting.svg"><p class="semi-bold">A/B Testing</p><p class="light">Multi-variate test the subject, from address, content and template for any email campaign, including triggered workflows.</p></li>
+      <li><img src="/wp-content/themes/vero/assets/images/home/features/more/csv.svg"><p class="semi-bold">CSV Imports</p><p class="light">Hit the ground running and import your customer attributes and actions using Vero's CSV import tools.</p></li>
     </ul>
   </div>
 </section>
 <section id="call-to-action" class="center-text">
   <div class="inner">
-    <h1 class="cta-title">Put your data first and craft better product experiences</h1>
+    <h1 class="cta-title">Put your data first and craft better customer experiences</h1>
     <p class="sub-heading">Create a free account, import your data and see how Vero can help your team.</p>
     <form action="https://app.getvero.com/pre_signups" method='post' class="horizontal-signup-form">
       <input class="form-control" type="email" placeholder="Email Address" name="email">
