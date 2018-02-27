3f8d679b28da115370ac3c05ba29aad109ada9d7
Rory Colsell <probablyrory@gmail.com>
2018-02-26 21:03:00 -0800

    add event manager & reporting page + layout + image assets

--- page-event-manager.php
+++ page-event-manager.php
@@ -12,7 +12,7 @@
 <section id="event-manager-heading">
   <div class="inner small-inner center-text bottom-padding-medium">
     <p class="semi-bold center-text small uppercase font-brand-primary center-text no-top-margin bottom-margin-medium">Event Manager</p>
-    <h1 class="bottom-margin-small">Confidently send emails with real-time customer data</h1>
+    <h1 class="bottom-margin-small biggie">Confidently send emails with real-time customer data</h1>
     <p class="no-top-margin medium">Vero's Event Manager gives marketing and engineering teams the visibility and clarity to collaborate and rapidly implement personalized email campaigns. </p>
   </div>
 </section>
@@ -20,25 +20,25 @@
   <div class="inner large-inner center-text flush-bottom flush-top">
     <!-- <h1 class="tubs no-bottom-margin">Manage your event schema</h1>
     <p class="medium bottom-padding-medium">The "event stream" has become an industry standard for tracking real-time customer activity. Vero's Event Manager enables you to clearly see the customer activity you are tracking and brings clarity to your overall data pipeline.</p> -->
-    <img src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/eventschema.png" srcset="/wp-content/themes/vero/assets/images/landing-pages/event-manager/eventschema@2x.png 2x" class="center-text display-block responsive-image">
+    <img src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/eventschema.png" srcset="/wp-content/themes/vero/assets/images/landing-pages/event-manager/eventschema@2x.png 2x" class="center-text display-block responsive-image small-cover-image-150">
   </div>
 </section>
 <section id="event-manager-add-event" class="feature-section double-padding">
-  <div class="inner halfs large-inner left-padding-large right-padding-large flush-bottom">
-    <div class="left">
+  <div class="inner halfs medium-inner left-padding-large right-padding-large flush-bottom">
+    <div class="left primary">
       <h1 class="tubs regular no-bottom-margin">Design email campaigns with placeholder events</h1>
       <p class="medium top-margin-medium no-bottom-margin">With Vero's Event Manager you can use placeholder events to build and test triggered email campaigns before integrating any code into your website or application.<br><br>
       Marketing teams can design personalized customer journeys and then collaborate with engineering to rapidly implement them.</p>
     </div>
-    <div class="right">
-      <img src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-add.png" srcset="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-add@2x.png 2x" class="left-padding-large">
+    <div class="right secondary">
+      <img src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-add.png" srcset="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-add@2x.png 2x" class="small-responsive-image hide-on-mobile">
     </div>
   </div>
 </section>
 <section id="event-manager-test-event" class="feature-section double-padding">
-  <div class="inner halfs large-inner left-padding-large right-padding-large flush-bottom">
+  <div class="inner halfs medium-inner small-reverse left-padding-large right-padding-large flush-bottom flush-top">
     <div class="left right-text">
-      <img src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-test.png" srcset="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-test@2x.png 2x" class="right-padding-large pull-right">
+      <img src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-test.png" srcset="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-test@2x.png 2x" class="pull-right small-responsive-image right-padding-medium hide-on-mobile">
     </div>
     <div class="right">
       <h1 class="tubs regular no-bottom-margin">Test and debug automated emails</h1>
@@ -50,10 +50,10 @@
   <div class="inner medium-inner top-padding-large bottom-padding-large">
     <p class="semi-bold center-text small uppercase font-brand-gray center-text no-top-margin bottom-margin-medium">AND MORE</p>
     <ul class="feature-list left-align unstyled-list halfs">
-      <li class="bottom-margin-medium padding-small"><img class="right-margin-small" src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-error.svg"><div class="right"><p class="medium regular no-top-margin">See when data stops flowing</p><p class="light">See events that are no longer firing and the campaigns they're linked to. Debug and troubleshoot campaigns that are not working with greater clarity.</p></div></li>
-      <li class="bottom-margin-medium padding-small"><img src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-detail.svg"><div class="right"><p class="medium regular no-top-margin">Troubleshoot every inch of data</p><p class="light">View recently fired events and see frequently attached properties and make sure your data reflects the personalisations in your emails.</p></div></li>
-      <li class="bottom-margin-medium padding-small"><img class="right-margin-small" src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-tidy.svg"><div class="right"><p class="medium regular no-top-margin">Keep your account clean</p><p class="light">Rename events in plain English, record event descriptions and know which events you can remove due to inactivity to keep your Vero account in order.</p></div></li>
-      <li class="bottom-margin-medium padding-small"><img src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-speed.svg"><div class="right"><p class="medium regular no-top-margin">Move faster</p><p class="light">Set up your entire Vero account or any new email campaign, and design your ideal workflow before integrating code.</p></div></li>
+      <li><img class="right-margin-small" src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-error.svg"><div class="right"><p class="medium regular no-top-margin">See when data stops flowing</p><p class="light">See events that are no longer firing and the campaigns they're linked to. Debug and troubleshoot campaigns that are not working with greater clarity.</p></div></li>
+      <li><img src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-detail.svg"><div class="right"><p class="medium regular no-top-margin">Troubleshoot every inch of data</p><p class="light">View recently fired events and see frequently attached properties and make sure your data reflects the personalisations in your emails.</p></div></li>
+      <li><img class="right-margin-small" src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-tidy.svg"><div class="right"><p class="medium regular no-top-margin">Keep your account clean</p><p class="light">Rename events in plain English, record event descriptions and know which events you can remove due to inactivity to keep your Vero account in order.</p></div></li>
+      <li><img src="/wp-content/themes/vero/assets/images/landing-pages/event-manager/event-speed.svg"><div class="right"><p class="medium regular no-top-margin">Move faster</p><p class="light">Set up your entire Vero account or any new email campaign, and design your ideal workflow before integrating code.</p></div></li>
     </ul>
   </div>
 </section>
