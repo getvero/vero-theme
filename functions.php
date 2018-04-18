3b7e5c324ab7e7b5063b5c3c7b04afb141d44be0
Chris Hexton <chris.hexton@gmail.com>
2018-04-18 14:34:49 +1000

    remove missing function callg

--- functions.php
+++ functions.php
@@ -53,7 +53,6 @@ function genesischild_theme_setup() {
 
   register_nav_menu('blue-nav-left' , __( 'Blue Navbar'));
   register_nav_menu('api-languages' , __( 'API Languages'));
-  add_action( 'genesis_after_header', 'add_blue_navbar_logic' );  
   remove_action( 'genesis_footer', 'genesis_do_footer' );
   add_action( 'genesis_footer', 'custom_footer');
   add_filter( 'wp_nav_menu', 'add_logo_and_menu_toggle_to_navbar', 10, 2 );
