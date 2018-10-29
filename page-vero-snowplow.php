a54449276da31923eceb9a03f27496beb0300ecc
Ka-Chun Chau <kachun@kachunchau.com>
2018-10-29 17:03:14 +0800

    update copy

--- page-vero-snowplow.php
+++ page-vero-snowplow.php
@@ -3,7 +3,7 @@
   function add_class($classes) {
     global $post;
 
-    $classes[] = 'integration-page segment-integration';
+    $classes[] = 'integration-page snowplow-integration';
     return $classes;
   }
 
@@ -23,23 +23,9 @@
         <img src="/wp-content/themes/vero/assets/images/integrations/segment/segment-logo.png" srcset="/wp-content/themes/vero/assets/images/integrations/segment/segment-logo@2x.png 2x" class="">
       </div>
     </div>
-    <p class="no-top-margin no-bottom-margin large">Use <strong>Segment</strong> to collect data from multiple sources, including mobile apps, websites, servers, and cloud apps and send it to <strong>Vero</strong> to trigger personalized marketing communications.</p>
+    <p class="no-top-margin no-bottom-margin large">Send all the email-related events generated in Vero to your data warehouse, alongside your data from other sources such as Zendesk and Stripe.</p>
     <a href="http://app.getvero.com" class="btn btn-primary btn-large top-margin-large btn-wide">Try Vero</a>
     <div class="learn-more"><p>Learn More</p><img src="/wp-content/themes/vero/assets/images/integrations/learn-more-carat.svg"></div>
-  </div>
-</section>
-<section id="vero-segment-info" class="">
-  <div class="inner large-inner top-padding-small bottom-padding-large">
-    <div class="integration-platform-info segment">
-      <div class="left">
-        <p class="medium regular no-top-margin">What is Segment?</h2>
-        <p class="medium no-bottom-margin">Segment is a customer data platform that connects over 200 sources and destinations to empower your team to use their favorite tools to personalize campaigns, analyze product usage, and more.</p>
-      </div>
-      <div class="right">
-        <p class="pill pill-medium pill-primary"><a href="https://segment.com/" target="_blank">Website</a></p>
-        <p class="pill pill-medium pill-primary"><a href="https://segment.com/docs/sources/" target="_blank">Documentation</a></p>
-      </div>
-    </div>
   </div>
 </section>
 <section id="features-more" class="border-bottom">
@@ -47,11 +33,18 @@
     <h2 class="center-text bottom-margin-large">Connecting Vero and Segment helps your team move quickly</h2>
     <ul class="feature-list left-align unstyled-list halfs">
       <li><img class="right-margin-small" src="/wp-content/themes/vero/assets/images/integrations/segment/sources.svg"><div class="right"><p class="medium regular no-top-margin">Access to 200+ data sources</p><p class="light">Capture data from 200+ sources, using Segment and it will translate and load your real-time data to Vero and automatically trigger personalized emails.</p></div></li>
-      <li><img src="/wp-content/themes/vero/assets/images/integrations/segment/customer-data.svg"><div class="right"><p class="medium regular no-top-margin">Automatically ingest customer data</p><p class="light">When you use Segment to connect to Vero, all your customer data from identify and track calls will show up in Vero automatically, and with no additional coding required from you.
-</p></div></li>
+      <li><img src="/wp-content/themes/vero/assets/images/integrations/segment/customer-data.svg"><div class="right"><p class="medium regular no-top-margin">Automatically ingest customer data</p><p class="light">When you use Segment to connect to Vero, all your customer data from identify and track calls will show up in Vero automatically, and with no additional coding required from you.</p></div></li>
       <li><img class="right-margin-small" src="/wp-content/themes/vero/assets/images/integrations/segment/sdks.svg"><div class="right"><p class="medium regular no-top-margin">Segment SDKs</p><p class="light">With Segment’s wide range of libraries and mobile SDKs you can get data into Vero quickly from all the places your customers interact with your product.</p></div></li>
       <li><img src="/wp-content/themes/vero/assets/images/integrations/segment/events.svg"><div class="right"><p class="medium regular no-top-margin">Event Management</p><p class="light">Complete visibility of the events and properties being tracked in Vero, including when each event was last tracked, which customers triggered an event and the campaigns that triggered.</p></div></li>
     </ul>
+  </div>
+</section>
+<section id="vero-integration-quote" class="feature-section border-bottom">
+  <div class="inner small-inner center-text">
+    <div><img src="/wp-content/themes/vero/assets/images/integrations/segment/quote-helloprint.jpg" class="quote-image">
+      <h1 class="tubs italic regular">“The Vero and Segment integration was a matter of ticking a box, putting in an API key and our CRM team was able to set up their first campaign in just a few hours”.</h1>
+      <p class="faded large regular">Michael Heerkens, CTO Helloprint</p>
+    </div>
   </div>
 </section>
 <section id="vero-segment-test-event" class="feature-section border-bottom">
@@ -67,11 +60,17 @@
     </div>
   </div>
 </section>
-<section id="vero-integration-quote" class="feature-section">
-  <div class="inner small-inner center-text">
-    <div><img src="/wp-content/themes/vero/assets/images/integrations/segment/quote-helloprint.jpg" class="quote-image">
-      <h1 class="tubs italic regular">“The Vero and Segment integration was a matter of ticking a box, putting in an API key and our CRM team was able to set up their first campaign in just a few hours”.</h1>
-      <p class="faded large regular">Michael Heerkens, CTO Helloprint</p>
+<section id="vero-segment-info" class="">
+  <div class="inner large-inner top-padding-small bottom-padding-large">
+    <div class="integration-platform-info segment">
+      <div class="left">
+        <p class="medium regular no-top-margin">What is Segment?</h2>
+        <p class="medium no-bottom-margin">Segment is a customer data platform that connects over 200 sources and destinations to empower your team to use their favorite tools to personalize campaigns, analyze product usage, and more.</p>
+      </div>
+      <div class="right">
+        <p class="pill pill-medium pill-primary"><a href="https://segment.com/" target="_blank">Website</a></p>
+        <p class="pill pill-medium pill-primary"><a href="https://segment.com/docs/sources/" target="_blank">Documentation</a></p>
+      </div>
     </div>
   </div>
 </section>
