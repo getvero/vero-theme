a2b70c92d7bd410a48de10d50d517a645547b63c
kachunchau <kachun@kachunchau.com>
2018-12-06 16:23:10 +0800

    edit customer links

--- page-pricing.php
+++ page-pricing.php
@@ -119,22 +119,22 @@ include 'pages-shared/static-header.php';
         </a>
       </li>
       <li class="logos-list-item no-top-margin">
-        <a class="display-block" href="http://monzo.com">
+        <a class="display-block" href="https://monzo.com">
           <img src="/wp-content/themes/vero/assets/images/home/pricing/logos/pricing-monzo.svg" alt="Monzo" height="76">
         </a>
       </li>
       <li class="logos-list-item no-top-margin">
-        <a class="display-block" href="http://helloprint.co.uk">
+        <a class="display-block" href="https://helloprint.co.uk">
           <img src="/wp-content/themes/vero/assets/images/home/pricing/logos/pricing-helloprint.svg" alt="Helloprint" height="76">
         </a>
       </li>
       <li class="logos-list-item no-top-margin">
-        <a class="display-block" href="http://tidal.com">
+        <a class="display-block" href="https://tidal.com">
           <img src="/wp-content/themes/vero/assets/images/home/pricing/logos/pricing-tidal.svg" alt="Tidal" height="76">
         </a>
       </li>
       <li class="logos-list-item no-top-margin">
-        <a class="display-block" href="http://pipedrive.com">
+        <a class="display-block" href="https://pipedrive.com">
           <img src="/wp-content/themes/vero/assets/images/home/pricing/logos/pricing-pipedrive.svg" salt="Pipedrive" height="76">
         </a>
       </li>
