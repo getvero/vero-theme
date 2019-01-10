09227b8ad4ca37530456d465718f877a196374e1
kachunchau <kachun@kachunchau.com>
2019-01-10 17:52:37 +0800

    add first draft content for react page

--- page-react.php
+++ page-react.php
@@ -10,7 +10,7 @@ function add_class($classes) {
 
 include 'pages-shared/static-header.php';
 ?>
-<section id="hero">
+<section class="double-padding" id="hero">
   <div class="inner xlarge-inner">
     <div class="grid intro">
       <div class="hero-body">
@@ -22,7 +22,7 @@ include 'pages-shared/static-header.php';
       </div>
       <div class="hero-footer">
         <a class="btn btn-success btn-large" href="https://app.getvero.com/signup">Start a free trial</a>
-        <a class="regular underline" id="demo-trigger" rel="leanModal" href="#demo">Talk to our sales team</a>
+        <a class="regular underline-link" id="demo-trigger" rel="leanModal" href="#demo">Talk to our sales team</a>
       </div>
     </div>
   </div>
@@ -32,22 +32,36 @@ include 'pages-shared/static-header.php';
     <h2 class=" d-block annotation uppercase regular faded bottom-margin-medium">How it works</h2>
 
     <div class="grid grid-thirds">
-      <div>
-        <h3 class="tubs semi-bold">Integrate</h3>
+      <div class="border-radius-10 border-primary padding-xsmall offset-shadow offset-shadow-primary">
+        <h3 class="micro semi-bold">Integrate</h3>
         <p class="medium">Collect real-time events and customer data from your website, app or systems.</p>
-        <a class="underline-link" href="#none">Learn more</a>
+        <a class="medium regular underline-link" href="#none">Learn more</a>
       </div>
-      <div>
-        <h3 class="tubs semi-bold">Notify</h3>
-        <p class="medium">Collect real-time events and customer data from your website, app or systems.</p>
-        <a class="underline-link" href="#none">Learn more</a>
+      <div class="border-radius-10 border-primary padding-xsmall offset-shadow offset-shadow-primary">
+        <h3 class="micro semi-bold">Notify</h3>
+        <p class="medium">Filter, branch, and route messages to the right team members.</p>
+        <a class="medium regular underline-link" href="#none">Learn more</a>
       </div>
-      <div>
-        <h3 class="tubs semi-bold">Reach</h3>
-        <p class="medium">Collect real-time events and customer data from your website, app or systems.</p>
-        <a class="underline-link" href="#none">Learn more</a>
+      <div class="border-radius-10 border-primary padding-xsmall offset-shadow offset-shadow-primary">
+        <h3 class="micro semi-bold">Reach</h3>
+        <p class="medium">Reach your team members where they are using email, Slack and SMS.</p>
+        <a class="medium regular underline-link" href="#none">Learn more</a>
       </div>
     </div>
+  </div>
+</section>
+<section class="double-padding">
+  <div class="inner xlarge-inner">
+    <h2 class="chunk regular">Integrate</h2>
+
+    <p class="medium">Your product or store is the core of your business. Respond to and learn from customer behavior, and offer customers a better experience.</p>
+
+    <div class="flexbox">
+      <span class="tubs">CDP integration</span>
+      <span class="tubs">Flexible API</span>
+    </div>
+
+    <p class="medium">Connect with customer data platforms such as Segment, Snowplow Analytics, and other data pipelines and easily pull and push data to and from Vero.</p>
   </div>
 </section>
 <?php
