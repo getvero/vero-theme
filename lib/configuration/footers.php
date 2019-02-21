1d7c580c20b296a6d125a6bbc7c438172ab35546
kachunchau <kachun@kachunchau.com>
2019-02-21 11:47:39 +0800

    add custom footer

--- lib/configuration/footers.php
+++ lib/configuration/footers.php
@@ -17,47 +17,57 @@ function custom_footer_html_with_wrap() {
 
 function custom_footer_html() {
   ?>
-  <div class="footer-aside">
-    <a href="https://www.getvero.com">
-      <img src="/wp-content/themes/vero/assets/images/home/logo/logo-white.svg" width="104">
-    </a>
-  </div>
-  <div class="footer-main">
-  	<ul class="unstyled-list footer-list">
-  	  <li class="header annotation uppercase faded">Product</li>
-  	  <li><a href="/features/email">Features</a></li>
-  	  <li><a href="/workflows">Workflows</a></li>
-  	  <li><a href="/event-manager">Event Management</a></li>
-  	  <li><a href="/fusion">Fusion</a></li>
-  	  <li><a href="/multi-language-campaigns">Multi-language</a></li>
-  	  <li><a href="/reporting">Reporting</a></li>
-  	  <li><a href="/integrations/vero-segment">Vero &plus; Segment</a></li>
-  	  <li><a href="/integrations/vero-stitch">Vero &plus; Stitch</a></li>
-  	  <li><a href="/integrations/vero-snowplow">Vero &plus; Snowplow</a></li>
-  	  <li><a href="/pricing">Pricing</a></li>
-  	</ul>
-  	<ul class="unstyled-list footer-list">
-  	  <li class="header annotation uppercase faded">Resources</li>
-  	  <!--<li>About</li>-->
-  	  <li><a href="https://www.getvero.com/resources" target="_blank">Blog</a></li>
-  	  <li ><a href="https://www.getvero.com/email-personalization-liquid-guide" target="_blank">Liquid Guide</a></li>
-  	  <li><a href="https://help.getvero.com" target="_blank">Help Center</a></li>
-  	  <li><a href="https://developers.getvero.com" target="_blank">API Reference</a></li>
-  	  <li><a href="https://releasenotes.getvero.com" target="_blank">Release Notes</a></li>
-  	  <li><a href="https://status.getvero.com/" target="_blank">Status</a></li>
-  	</ul>
-  	<ul class="unstyled-list footer-list">
-  	  <li class="header annotation uppercase faded">Company</li>
-  	  <li><a href="https://www.getvero.com/careers">Careers</a><a href="https://www.getvero.com/careers" class="hiring-pill left-margin-tiny">We're Hiring!</a></li>
-  	  <li><a href="https://www.getvero.com/gdpr">GDPR</a></li>
-  	  <li><a href="https://twitter.com/getvero" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @getvero</a>
-  	  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></li>
-  	</ul>
-    <ul class="unstyled-list footer-legal annotation">
-      <li class=" d-inline-block"><a href="https://www.getvero.com/terms-of-service">Terms of Service</a></li>
-  	  <li class=" d-inline-block"><a href="https://www.getvero.com/privacy">Privacy Policy</a></li>
-    </ul>
-  </div>
+  <footer class="footer">
+    <div class="inner xlarge-inner">
+      <div class="flex">
+        <div class="footer-aside">
+          <a href="https://www.getvero.com">
+            <img src="/wp-content/themes/vero/assets/images/home/logo/logo-white.svg" width="104">
+          </a>
+        </div>
+        <div class="footer-main">
+          <ul class="unstyled-list footer-list semi-bold">
+            <li class="header annotation uppercase faded">Product</li>
+            <li><a href="/features/email">Features</a></li>
+            <li><a href="/workflows">Workflows</a></li>
+            <li><a href="/event-manager">Event Management</a></li>
+            <li><a href="/fusion">Fusion</a></li>
+            <li><a href="/multi-language-campaigns">Multi-language</a></li>
+            <li><a href="/reporting">Reporting</a></li>
+            <li><a href="/pricing">Pricing</a></li>
+          </ul>
+          <ul class="unstyled-list footer-list semi-bold">
+            <li class="header annotation uppercase faded">Integrations</li>
+            <li><a href="/integrations/vero-segment">Vero &plus; Segment</a></li>
+            <li><a href="/integrations/vero-stitch">Vero &plus; Stitch</a></li>
+            <li><a href="/integrations/vero-snowplow">Vero &plus; Snowplow</a></li>
+          </ul>
+          <ul class="unstyled-list footer-list semi-bold">
+            <li class="header annotation uppercase faded">Resources</li>
+            <li><a href="https://www.getvero.com/resources" target="_blank">Blog</a></li>
+            <li ><a href="https://www.getvero.com/email-personalization-liquid-guide" target="_blank">Liquid Guide</a></li>
+            <li><a href="https://help.getvero.com" target="_blank" rel="noopener">Help Center</a></li>
+            <li><a href="https://developers.getvero.com" target="_blank" rel="noopener">API Reference</a></li>
+            <li><a href="https://releasenotes.getvero.com" target="_blank" rel="noopener">Release Notes</a></li>
+            <li><a href="https://feedback.getvero.com/" target="_blank" rel="noopener">Product Roadmap</a></li>
+            <li><a href="https://feedback.getvero.com/feature-requests" target="_blank" rel="noopener">Feature Requests</a></li>
+            <li><a href="https://status.getvero.com/" target="_blank" rel="noopener">Status</a></li>
+          </ul>
+          <ul class="unstyled-list footer-list semi-bold">
+            <li class="header annotation uppercase faded">Company</li>
+            <li><a href="https://www.getvero.com/careers">Careers</a></li>
+            <li><a href="https://www.getvero.com/gdpr">GDPR</a></li>
+            <li><a href="https://twitter.com/getvero" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @getvero</a>
+            <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></li>
+          </ul>
+          <ul class="unstyled-list footer-legal annotation">
+            <li class=" d-inline-block"><a href="https://www.getvero.com/terms-of-service">Terms of Service</a></li>
+            <li class=" d-inline-block"><a href="https://www.getvero.com/privacy">Privacy Policy</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
   <?php
 }
 
