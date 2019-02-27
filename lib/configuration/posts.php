19b50b590806bb541b390b87800011c647416383
kachunchau <kachun@kachunchau.com>
2019-02-27 21:31:33 +0800

    change the redirect page

--- lib/configuration/posts.php
+++ lib/configuration/posts.php
@@ -92,7 +92,7 @@ function add_subscribe_form() {
       <h2 class="micro center-text">Get our latest blog posts, product news and tips straight to your inbox</h2>
       <form class="bottom-margin-small" action='https://app.getvero.com/forms/0eefc98b2dc881e7c0888ae698833577' method='post'>
         <input id="footer-email-address"  name='email' type='email' class="form-control" placeholder="email@address.com">
-        <input name='redirect_on_success' type='hidden' value='https://veropublic.wpengine.com/subscribed-to-the-blog/'>
+        <input name='redirect_on_success' type='hidden' value='https://www.getvero.com/subscribed-to-the-blog/'>
         <input name='user[consent_marketing]' type='hidden' value='true'>
         <input name='user[consent_product_updates]' type='hidden' value='true'>
         <input name='user[contact_by_fax_only]' type='checkbox' value='1' style='display:none !important' tabindex="-1" autocomplete="false" />
