e9bfffda24d5536cc5790749783d2050bf68aa34
kachunchau <kachun@kachunchau.com>
2019-03-07 13:07:24 +0800

    update fusion footer cta

--- page-fusion.php
+++ page-fusion.php
@@ -123,13 +123,8 @@
 </section>
 <section id="call-to-action" class="center-text">
   <div class="inner">
-    <h1 class="cta-title chunk semi-bold bottom-margin-small">Put your data first and craft better product experiences</h1>
-    <p class="sub-heading">Create a free account, import your data and see how Vero can help your team.</p>
-    <form action="https://app.getvero.com/pre_signups" method='post' class="horizontal-signup-form">
-      <input class="form-control" type="email" placeholder="Email Address" name="email">
-      <input class="btn btn-success" type="submit" value="Create your account">
-    </form>
-    <p class="mini faded center-text mf-half center-block top-margin-small horizontal-padding-small">In order to provide our service to you, we must store your personal information. Any personal data that we collect will be handled in accordance with our <a href="/privacy">Privacy Notice</a>.</p>
+    <h3 class="cta-title chunk semi-bold bottom-margin-small">Create better customer experiences</h3>
+    <a class="d-inline-block btn btn-large btn-success" href="https://app.getvero.com/signup">Start a free trial</a>
   </div>
 </section>
 <?php
