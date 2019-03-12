7453bb7665223b077b4bc9f6596deda7f35bdd1e
kachunchau <kachun@kachunchau.com>
2019-03-12 20:39:37 +0800

    exclude coffeescript folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp   = require('gulp'),
-    del    = require('del'),
-    rename = require('gulp-rename'),
-    uglify = require('gulp-uglify');
+      del    = require('del'),
+      rename = require('gulp-rename'),
+      uglify = require('gulp-uglify');
 
 const paths = {
   scripts: {
@@ -23,11 +23,11 @@ function devMessage() {
 }
 
 // Uglify scripts
-function compress() {
+function scripts() {
   return gulp
   .src([
     'assets/dev/scripts/**/*.js',
-    '!assets/dev/scripts/source/',
+    '!assets/dev/scripts/source/*',
     '!assets/dev/scripts/dev_message.js'
   ])
   .pipe(uglify())
@@ -37,14 +37,15 @@ function compress() {
   .pipe(gulp.dest(paths.scripts.dest));
 }
 
+// Watch scripts and uglify
 function watch() {
   return gulp
-  .watch(paths.scripts.src, gulp.series(compress));
+  .watch(paths.scripts.src, gulp.series(scripts));
 }
 
-const js    = gulp.series(devMessage, compress);
+const js    = gulp.series(devMessage, scripts);
 const build = gulp.series(clean, gulp.parallel(js, watch));
 
-exports.compress = compress;
+exports.scripts = scripts;
 exports.watch = watch
 exports.default = build;
