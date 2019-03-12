a6a94349708147303b68925ab1ab962968e68f9a
kachunchau <kachun@kachunchau.com>
2019-03-12 13:42:40 +0800

    remove worflows hover-box js as not used anywhere

--- assets/dev/scripts/landing-pages.js
+++ assets/dev/scripts/landing-pages.js
@@ -3,15 +3,6 @@ jQuery(window).load(function(){
 });
 
 jQuery(document).ready(function(){
-  jQuery('#workflows-ui-nodes li.hover-box').click(function(e){
-    console.log('hello');
-    var node = jQuery(this);
-    if(!node.hasClass('active')){
-      node.siblings('.hover-box').removeClass('active');
-      node.addClass('active');
-    }
-  });
-
   // Feature image switcher
   jQuery('.feature-swapper-option').click(function(e){
     var swapTo = jQuery(this).data('swap-to');
