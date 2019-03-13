9d8b67b108995674bff27892e1d29b0a61a0cf5a
kachunchau <kachun@kachunchau.com>
2019-03-13 11:24:46 +0800

    install imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
-const gulp   = require('gulp'),
-      del    = require('del'),
-      rename = require('gulp-rename'),
-      uglify = require('gulp-uglify');
+const gulp     = require('gulp'),
+      del      = require('del'),
+      rename   = require('gulp-rename'),
+      imagemin = require('gulp-imagemin'),
+      uglify   = require('gulp-uglify');
 
 const paths = {
+  images: {
+    src : 'assets/dev/images/**/*',
+    dest: 'assets/dist/images/'
+  },
   scripts: {
     src : 'assets/dev/scripts/**/*.js',
     dest: 'assets/dist/scripts/'
@@ -12,7 +17,7 @@ const paths = {
 
 // Clean dist folder
 function clean() {
-  return del(paths.scripts.dest);
+  return del('assets/dist/**/*');
 }
 
 // Move dev message JS to dist folder
@@ -20,6 +25,28 @@ function devMessage() {
   return gulp
   .src('assets/dev/scripts/dev_message.js')
   .pipe(gulp.dest(paths.scripts.dest));
+}
+
+function images() {
+  return gulp
+  .src('assets/dev/images/**/*')
+  // .pipe(newer("./_site/assets/img"))
+  .pipe(
+    imagemin([
+      imagemin.gifsicle({ interlaced: true }),
+      imagemin.jpegtran({ progressive: true }),
+      imagemin.optipng({ optimizationLevel: 5 }),
+      imagemin.svgo({
+        plugins: [
+          {
+            removeViewBox: false,
+            collapseGroups: true
+          }
+        ]
+      })
+    ])
+  )
+  .pipe(gulp.dest('assets/dist/images'));
 }
 
 // Uglify scripts
@@ -39,13 +66,15 @@ function scripts() {
 
 // Watch scripts and uglify
 function watch() {
-  return gulp
-  .watch(paths.scripts.src, gulp.series(scripts));
+  gulp.watch(paths.scripts.src, gulp.series(scripts))
+  gulp.watch('assets/dev/images/**/*', images);
 }
 
 const js    = gulp.series(devMessage, scripts);
-const build = gulp.series(clean, gulp.parallel(js, watch));
+const build = gulp.series(clean, gulp.parallel(images, js, watch));
 
+exports.clean   = clean;
+exports.images  = images;
 exports.scripts = scripts;
 exports.watch   = watch
 exports.local   = build;
