0ccdbd73facdfc47d05a5883a88a56e61116a34a
kachunchau <kachun@kachunchau.com>
2019-03-19 18:34:47 +0800

    remove double padding

--- page-homepage.php
+++ page-homepage.php
@@ -1,7 +1,7 @@
 <?php
   include 'pages-shared/static-header.php';
 ?>
-<section class="home-hero double-padding overflow-hidden">
+<section class="home-hero overflow-hidden">
   <?php
     do_action( 'genesis_header' );
     do_action( 'genesis_after_header' );
