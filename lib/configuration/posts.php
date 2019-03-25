dde2c91ece76b61c86a7c350a352d32f19f7b2f8
kachunchau <kachun@kachunchau.com>
2019-03-25 13:34:25 +0800

    change bottom blog cta heading tag

--- lib/configuration/posts.php
+++ lib/configuration/posts.php
@@ -89,7 +89,7 @@ function add_subscribe_form() {
   if( is_blog_post_or_guide() ){
     ?>
     <div class='subscribe-form center-block'>
-      <h2 class="micro center-text">Get our latest blog posts, product news and tips straight to your inbox</h2>
+      <h3 class="micro center-text">Get our latest blog posts, product news and tips straight to your inbox</h3>
       <form class="bottom-margin-small" action='https://app.getvero.com/forms/0eefc98b2dc881e7c0888ae698833577' method='post'>
         <input id="footer-email-address"  name='email' type='email' class="form-control" placeholder="email@address.com">
         <input name='redirect_on_success' type='hidden' value='https://www.getvero.com/subscribed-to-the-blog/'>
