94f564b3529bb9dc5aacf105eebb1e3e31303e68
kachunchau <kachun@kachunchau.com>
2019-03-27 14:04:06 +0800

    use excerpt length

--- functions.php
+++ functions.php
@@ -71,7 +71,7 @@ function genesischild_theme_setup() {
   add_filter( 'genesis_next_link_text', 'next_link_text' );
 
   // Entry post structure
-  add_filter( 'get_the_content_more_link', 'remove_read_more_link' );
+  add_filter( 'get_the_content_more_link', 'remove_read_more_link', 99 );
   add_action( 'genesis_entry_footer', 'add_custom_read_more_link' );
   remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
   add_action( 'genesis_entry_header', 'add_feature_image_to_posts', 12 );
@@ -96,7 +96,7 @@ function genesischild_theme_setup() {
   // Post Page
   add_action( 'genesis_entry_footer', 'post_remove_footer' );
   // add_action( 'genesis_before_footer', 'blog_related_posts');
-  add_action( 'genesis_entry_content', 'custom_excerpt_length', 99 );
+  add_filter( 'excerpt_length', 'custom_excerpt_length' );
   add_filter( 'excerpt_more', 'new_excerpt_more' );
 
   // Search page
