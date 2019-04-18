fe3bbc083a0fb7ce303a95e8e4a1f1f58f17f799
kachunchau <kachun@kachunchau.com>
2019-04-18 12:00:19 +0800

    fix subscription form markup

--- lib/configuration/posts.php
+++ lib/configuration/posts.php
@@ -95,31 +95,29 @@ function add_subscribe_form() {
   if( is_blog_post_or_guide() ){
     ?>
     <div class="js-overlay overlay">
-      <div class="js-modal-subscription flex items-center justify-center" style="height: 100%;">
-        <div class="modal modal-subscription show">
-          <div class="center-text bottom-margin-small js-enquire-intro" id="enquire-intro">
-            <h3>Get our latest blog posts, news and tips straight to your inbox.</h3>
+      <div class="js-newsletter newsletter">
+        <div class="center-text bottom-margin-small js-enquire-intro" id="enquire-intro">
+          <h3>Get our latest blog posts, news and tips straight to your inbox.</h3>
+        </div>
+        <div class="center-text js-thanks" id="thanks">
+          <h3 class="atomic regular bottom-margin-small">Almost there!</h3>
+          <p class="no-margin">We've sent you an email to confirm your subscription.</p>
+        </div>
+        <form class="js-blog-subscribe-form" action='https://app.getvero.com/forms/0eefc98b2dc881e7c0888ae698833577' method='post'>
+          <div class="flex justify-center">
+            <input class="form-control" id="sender_email_address" name="email" type="email" placeholder="email@address.com">
+            <input name="user[consent_marketing]" type="hidden" value="true">
+            <input name="user[consent_product_updates]" type="hidden" value="true">
+            <input name="user[contact_by_fax_only]" type="checkbox" value="1" style="display:none !important" tabindex="-1" autocomplete="false">
+            <input class="btn btn-medium btn-success" type="submit" value="Subscribe to updates">
           </div>
-          <div class="center-text js-thanks" id="thanks">
-            <h3 class="atomic regular bottom-margin-small">Almost there!</h3>
-            <p class="no-margin">We've sent you an email to confirm your subscription.</p>
+          <div class="inner flush-top flush-bottom">
+            <p class="mini center-text"><span class="faded">We're committed to keeping your information safe. Read our</span> <a href="/privacy">Privacy Policy</a>.</p>
           </div>
-          <form class="js-blog-subscribe-form" action='https://app.getvero.com/forms/0eefc98b2dc881e7c0888ae698833577' method='post'>
-            <div class="flex justify-center">
-              <input class="form-control" id="sender_email_address" name="email" type="email" placeholder="email@address.com">
-              <input name="user[consent_marketing]" type="hidden" value="true">
-              <input name="user[consent_product_updates]" type="hidden" value="true">
-              <input name="user[contact_by_fax_only]" type="checkbox" value="1" style="display:none !important" tabindex="-1" autocomplete="false">
-              <input class="btn btn-medium btn-success" type="submit" value="Subscribe to updates">
-            </div>
-            <div class="inner flush-top flush-bottom">
-              <p class="mini center-text"><span class="faded">We're committed to keeping your information safe. Read our</span> <a href="/privacy">Privacy Policy</a>.</p>
-            </div>
-          </form>
+        </form>
 
-          <div class="modal-close">
-            <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19"><path fill="#384254" fill-rule="evenodd" d="M8.086 9.5L4.35 5.765a1 1 0 0 1 1.414-1.414L9.5 8.086l3.735-3.735a1 1 0 1 1 1.414 1.414L10.914 9.5l3.735 3.735a1 1 0 1 1-1.414 1.414L9.5 10.914 5.765 14.65a1 1 0 0 1-1.414-1.414L8.086 9.5z"/></svg>
-          </div>
+        <div class="js-newsletter-close newsletter-close">
+          <svg xmlns="http://www.w3.org/2000/svg" width="19" height="19"><path fill="#384254" fill-rule="evenodd" d="M8.086 9.5L4.35 5.765a1 1 0 0 1 1.414-1.414L9.5 8.086l3.735-3.735a1 1 0 1 1 1.414 1.414L10.914 9.5l3.735 3.735a1 1 0 1 1-1.414 1.414L9.5 10.914 5.765 14.65a1 1 0 0 1-1.414-1.414L8.086 9.5z"/></svg>
         </div>
       </div>
     </div>
