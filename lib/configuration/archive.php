ed7f4f4bc2570f82b641db85f5f1e03d32c5ef75
kachunchau <kachun@kachunchau.com>
2019-05-16 18:46:22 +0800

    increase paragraph size

--- lib/configuration/archive.php
+++ lib/configuration/archive.php
@@ -74,7 +74,7 @@ function add_featured_post() {
     <div class="resources-section featured-post">
       <h2 class="micro regular">Featured</h2>
 
-      <div class="grid post">
+      <div class="grid">
       <?php
         $custom_query = new WP_Query(array(
           'post_type' => array('post', 'guides'),
@@ -91,20 +91,22 @@ function add_featured_post() {
 
           <div class="post-body">
             <div class="post-header">
-              <span class="mini semi-bold bottom-margin-micro uppercase"><?php echo $category[0]->cat_name; ?></span>
+              <span class="d-block mini semi-bold bottom-margin-tiny uppercase"><?php echo $category[0]->cat_name; ?></span>
 
               <h3 class="chunk regular"><a class="unstyled" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
             </div>
 
-            <div class="post-content">
-              <p><?php $content = get_the_content(); echo mb_strimwidth($content, 0, 100, '…');?></p>
+            <div class="post-content bottom-margin-smedium">
+              <p class="medium"><?php $content = get_the_content(); echo mb_strimwidth($content, 0, 100, '…');?></p>
             </div>
 
-            <div class="post-meta">
-              <span class="d-block"><?php echo get_the_author_meta('display_name', $author_id); ?></span>
-              <?php echo get_the_author_meta('description'); ?>
-
+            <div class="flex post-meta">
               <?php echo get_avatar( get_the_author_meta( 'ID' ), 32 ); ?>
+
+              <div>
+                <span class="d-block annotation semi-bold uppercase"><?php echo get_the_author_meta('display_name', $author_id); ?></span>
+                <span class="d-block annotation"><?php echo get_the_author_meta('description'); ?></span>
+              </div>
             </div>
           </div>
 
@@ -141,7 +143,7 @@ function add_news_and_updates_posts() {
 
               <div class="post-body">
                 <div class="post-header">
-                  <span class="mini semi-bold bottom-margin-micro uppercase"><?php echo $category[0]->cat_name; ?></span>
+                  <span class="d-block mini semi-bold bottom-margin-tiny uppercase"><?php echo $category[0]->cat_name; ?></span>
 
                   <h3 class="micro regular"><a class="unstyled" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                 </div>
@@ -193,7 +195,7 @@ function add_other_posts() {
 
               <div class="post-body">
                 <div class="post-header">
-                  <span class="mini semi-bold bottom-margin-micro uppercase"><?php echo $category[0]->cat_name; ?></span>
+                  <span class="d-block mini semi-bold bottom-margin-tiny uppercase"><?php echo $category[0]->cat_name; ?></span>
 
                   <h3 class="regular"><a class="unstyled" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                 </div>
@@ -289,33 +291,35 @@ function add_categories_and_search() {
 
     <div class="resources-sidebar">
       <div class="flex flex-column nav-resources wrap">
-        <p class="bottom-margin-medium">Insights, tips and tools for creating better customer messaging and product experiences.</p>
+        <p class="bottom-margin-medium bottom-padding-medium border-bottom">Insights, tips and tools for creating better customer messaging and product experiences.</p>
 
-        <div class="nav-blog-categories">
+        <div class="resources-sidebar-categories">
           <h3 class="semi-bold annotation uppercase faded">Categories</h3>
 
-          <ul class="nav-blog-category-list">
-            <li class="<?php echo is_active('all'); ?>"><a href="/resources">All</a></li>
-            <li class="<?php echo is_active('vero-updates'); ?>"><a href="/resources/category/vero-updates/">Vero Updates</a></li>
-            <li class="<?php echo is_active('how-to'); ?>"><a href="/resources/category/how-to">How To's</a></li>
-            <li class="<?php echo is_active('case-studies'); ?>"><a href="/resources/category/case-studies">Case Studies</a></li>
+          <ul class="resources-sidebar-categories-list">
+            <li class="<?php echo is_active('all'); ?>"><a class="unstyled" href="/resources">All</a></li>
+            <li class="<?php echo is_active('vero-updates'); ?>"><a class="unstyled" href="/resources/category/vero-updates/">Vero Updates</a></li>
+            <li class="<?php echo is_active('how-to'); ?>"><a class="unstyled" href="/resources/category/how-to">How To's</a></li>
+            <li class="<?php echo is_active('case-studies'); ?>"><a class="unstyled" href="/resources/category/case-studies">Case Studies</a></li>
           </ul>
         </div>
 
-        <form class="js-blog-subscribe-form top-margin-auto" action='https://app.getvero.com/forms/0eefc98b2dc881e7c0888ae698833577' method='post'>
-          <div class="form-group bottom-margin-tiny">
-            <input class="form-control input-width-full" id="sender_email_address" name="email" type="email" placeholder="email@address.com">
-            <input name="user[consent_marketing]" type="hidden" value="true">
-            <input name="user[consent_product_updates]" type="hidden" value="true">
-            <input name="event[blog_subscriber_source]" type="hidden" value="blog_header">
-            <input name="user[contact_by_fax_only]" type="checkbox" value="1" style="display:none !important" tabindex="-1" autocomplete="false">
-          </div>
-          <div class="form-group bottom-margin-tiny">
-            <input class="btn btn-success" type="submit" value="Subscribe to updates">
-          </div>
-          <p class="mini"><span class="faded">We're committed to keeping your information safe. Read our</span> <a href="/privacy">Privacy Policy</a>.</p>
+        <div class="top-margin-auto">
+          <form class="js-blog-subscribe-form" action='https://app.getvero.com/forms/0eefc98b2dc881e7c0888ae698833577' method='post'>
+            <div class="form-group bottom-margin-micro">
+              <input class="form-control input-width-full" id="sender_email_address" name="email" type="email" placeholder="email@address.com">
+              <input name="user[consent_marketing]" type="hidden" value="true">
+              <input name="user[consent_product_updates]" type="hidden" value="true">
+              <input name="event[blog_subscriber_source]" type="hidden" value="blog_header">
+              <input name="user[contact_by_fax_only]" type="checkbox" value="1" style="display:none !important" tabindex="-1" autocomplete="false">
+            </div>
+            <div class="form-group bottom-margin-tiny">
+              <input class="btn btn-success" type="submit" value="Subscribe to updates">
+            </div>
+            <p class="mini"><span class="faded">We're committed to keeping your information safe. Read our</span> <a href="/privacy">Privacy Policy</a>.</p>
 
-        </form>
+          </form>
+        </div>
       </div>
     </div>
 
