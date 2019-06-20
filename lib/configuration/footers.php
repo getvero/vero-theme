ba4b94a00451e4df51e64ff5aa301d0c96b1c52a
kachunchau <kachun@kachunchau.com>
2019-06-20 16:48:28 +0800

    add form

--- lib/configuration/footers.php
+++ lib/configuration/footers.php
@@ -12,7 +12,7 @@
 
   function custom_footer_html() {
     ?>
-    <footer class="footer" <?php if ( is_blog() ) { ?> style="padding-top: 9%;"<?php } ?>>
+    <footer class="footer" <?php if ( is_blog() ) { ?> style="padding-top: 130px;"<?php } ?>>
       <div class="inner xlarge-inner">
         <div class="flex flex-wrap">
           <div class="col-aside">
@@ -114,13 +114,29 @@
         <?php if( get_field('type_of_cta') == 'product' ): ?>
           <h4 class="tubs regular bottom-margin-smedium">Vero helps marketing and engineering teams use their data to create a better customer messaging experience</h4>
 
-          <a class="btn btn-success btn-large bottom-margin-smedium" href="https://app.getvero.com/signup">Start a free tial</a>
+          <a class="btn btn-success btn-large bottom-margin-smedium" href="https://app.getvero.com/signup">Start a free trial</a>
 
-          <span class="d-block annotation">No credit card details required</span>
+          <p class="annotation faded">No credit card details required</p>
         <?php else: ?>
-          <h4 class="tubs regular bottom-margin-smedium">Insights, tips and tools for creating better customer messaging and product experiences</h4>
+          <div class="js-enquire-menu-2 enquire-menu-2">
+            <h4 class="tubs regular bottom-margin-smedium">Insights, tips and tools for creating better customer messaging and product experiences</h4>
+          </div>
+          <div class="js-thanks-menu-2 thanks-menu-2 hide">
+            <h4 class="atomic regular bottom-margin-small">Almost there!</h4>
+            <p class="no-margin">We've sent you an email to confirm your subscription.</p>
+          </div>
 
-          <a class="btn btn-success btn-large" href="https://app.getvero.com/signup">Subscribe</a>
+          <form class="js-blog-subscribe-form-2 blog-subscribe-form-2" action="https://app.getvero.com/forms/0eefc98b2dc881e7c0888ae698833577" method="post">
+            <div class="flex flex-column md-lg-flex-row justify-center items-stretch bottom-margin-smedium">
+              <input class="form-control form-control-large input-width-full" id="sender_email_address" name="email" type="email" placeholder="name@mycompany.com">
+              <input name="user[consent_marketing]" type="hidden" value="true">
+              <input name="user[consent_product_updates]" type="hidden" value="true">
+              <input name="event[blog_subscriber_source]" type="hidden" value="popup">
+              <input name="user[contact_by_fax_only]" type="checkbox" value="1" style="display:none !important" tabindex="-1" autocomplete="false">
+              <input class="btn btn-success btn-large btn-no-shadow" type="submit" value="Subscribe to updates">
+            </div>
+            <p class="annotation faded">We're committed to keeping your information safe. Read our <a class="unstyled underline-link" href="/privacy">Privacy Policy</a>.</p>
+          </form>
         <?php endif; ?>
       </div>
     </section>
