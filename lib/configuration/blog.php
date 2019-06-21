dcd0f8af0007c3715b80c5d3283b983c4b4acdca
kachunchau <kachun@kachunchau.com>
2019-06-21 13:44:38 +0800

    add old categories and search to blog.php

--- lib/configuration/blog.php
+++ lib/configuration/blog.php
@@ -120,4 +120,45 @@
     }
   }
 
+  function add_categories_and_search() {
+    if ( is_blog_archive() || is_single() ){ ?>
+      <div class="js-resources-sidebar resources-sidebar">
+        <div class="flex flex-column nav-resources wrap">
+          <div class="resources-sidebar-categories bottom-margin-small">
+            <h3 class="semi-bold annotation uppercase faded bottom-margin-small">Categories</h3>
+
+            <ul class="resources-sidebar-categories-list">
+              <li class="<?php echo is_active('all'); ?>"><a class="unstyled" href="/resources">All</a></li>
+              <li class="<?php echo is_active('vero-updates'); ?>"><a class="unstyled" href="/resources/category/vero-updates/">Vero Updates</a></li>
+              <li class="<?php echo is_active('how-to'); ?>"><a class="unstyled" href="/resources/category/how-to">How To's</a></li>
+              <li class="<?php echo is_active('case-studies'); ?>"><a class="unstyled" href="/resources/category/case-studies">Case Studies</a></li>
+            </ul>
+          </div>
+
+          <?php echo get_search_form(); ?>
+
+          <div class="top-margin-auto">
+            <p class="hide md-show bottom-margin-small">Insights, tips and tools for creating better customer messaging and product experiences.</p>
+
+            <div class="js-thanks-3 hide">
+              <p>Thanks for subscribing</p>
+            </div>
+
+            <form class="js-blog-subscribe-form" action='https://app.getvero.com/forms/0eefc98b2dc881e7c0888ae698833577' method='post'>
+              <div class="form-group bottom-margin-micro">
+                <input class="form-control input-width-full" id="sender_email_address" name="email" type="email" placeholder="email@address.com">
+                <input name="user[consent_marketing]" type="hidden" value="true">
+                <input name="user[consent_product_updates]" type="hidden" value="true">
+                <input name="event[blog_subscriber_source]" type="hidden" value="blog_header">
+                <input name="user[contact_by_fax_only]" type="checkbox" value="1" style="display:none !important" tabindex="-1" autocomplete="false">
+              </div>
+              <input class="btn btn-success input-width-full bottom-margin-tiny" type="submit" value="Subscribe to updates">
+              <span class="d-block annotation"><span class="faded">We're committed to keeping your information safe. Read our</span> <a href="/privacy">Privacy Policy</a>.</span>
+            </form>
+          </div>
+        </div>
+      </div>
+    <?php }
+  }
+
 ?>
