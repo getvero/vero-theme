c213ce446b01ce3e67ded37dab78c2c57ba1024c
kachunchau <kachun@kachunchau.com>
2019-06-21 10:46:23 +0800

    remove old hooks

--- functions.php
+++ functions.php
@@ -83,7 +83,6 @@ function genesischild_theme_setup() {
   add_filter( 'get_search_form', 'change_search_form_type' );
 
   // Add categories and search
-  // add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
   add_filter( 'genesis_pre_get_option_site_layout', 'force_full_width_on_posts' );
   // add_action( 'genesis_before_content', 'add_blog_post_back_button' );
   // add_action( 'genesis_after_header', 'add_categories_and_search' );
@@ -97,14 +96,13 @@ function genesischild_theme_setup() {
   add_action( 'genesis_entry_footer', 'add_custom_read_more_link' );
 
   // Move featured image above title
-  add_action( 'genesis_entry_header', 'move_featured_image' );
+  add_action( 'genesis_entry_content', 'move_featured_image' );
 
   // Customise post entry
   add_action( 'genesis_before_loop', 'change_post_structure' );
-
   // add_action( 'genesis_after_content', 'add_subscribe_form' );
   remove_action( 'genesis_entry_footer', 'genesis_post_meta' );
-  add_action( 'genesis_before_entry', 'add_featured_image_to_post' );
+  add_action( 'genesis_post_info', 'add_featured_image_to_post' );
   add_action( 'genesis_entry_header', 'add_shares_to_post', 13 );
   add_filter( 'genesis_post_info', 'change_post_info' );
   // Add author after entry title on single posts
