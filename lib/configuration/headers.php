07932b95645afa45ea2ba8afc8e44099e007b93e
kachunchau <kachun@kachunchau.com>
2019-06-26 10:32:12 +0800

    add tutorial link

--- lib/configuration/headers.php
+++ lib/configuration/headers.php
@@ -63,7 +63,7 @@ function add_logo_and_menu_toggle_to_navbar($menu, $args) {
               <a class="unstyled" href="/resources/category/data-management">Data Management</a>
             </li>
             <li>
-              <a class="unstyled" href="/resources/category/data-management">Tutorials</a>
+              <a class="unstyled" href="/resources/category/tutorials">Tutorials</a>
             </li>
           <?
         }
