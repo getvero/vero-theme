2c7e6120bfb7266072443ef89e03472e1e157ae6
kachunchau <kachun@kachunchau.com>
2019-10-25 17:03:19 +0800

    remove highlight js

--- lib/configuration/assets.php
+++ lib/configuration/assets.php
@@ -12,12 +12,9 @@ function custom_load_custom_style_sheet() {
   wp_register_style('googlefont_merriweather', 'https://fonts.googleapis.com/css?family=Merriweather:700&display=swap');
   wp_register_style('featherlight-style', '/wp-content/themes/vero/assets/dist/stylesheets/featherlight.min.css', NULL, PARENT_THEME_VERSION );
   wp_register_style('prism-okaidia', '/wp-content/themes/vero/assets/dist/stylesheets/prism.min.css', NULL, PARENT_THEME_VERSION );
-  wp_register_style( 'highlight-style', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/styles/tomorrow-night-bright.min.css');
-  // wp_register_style( 'highlight-style', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.8.0/styles/tomorrow-night-bright.min.css');
 
-  if ( is_page('features') ||  is_blog() ) {
+  if ( is_page('features') || is_blog() ) {
     wp_enqueue_style( 'prism-okaidia' );
-
   }
   if($_SERVER["HTTP_HOST"] == "localhost:8888"){
     $base_url = "http://0.0.0.0:9000";
@@ -33,12 +30,6 @@ function custom_load_custom_style_sheet() {
   if ( is_front_page() || is_page('capterra') || is_page('data-management') || is_page('customer-engagement') || is_page('vero-segment') || is_page('vero-stitch') || is_page('careers') ) {
     wp_enqueue_style( 'googlefont_merriweather');
   }
-  if ( is_page('features') || is_blog() ) {
-    // wp_enqueue_style( 'featherlight-style');
-  }
-  if ( is_blog() ) {
-    // wp_enqueue_style( 'highlight-style');
-  }
 }
 
 // Add JS
@@ -51,10 +42,9 @@ function add_js() {
   wp_register_script('dev_message', get_stylesheet_directory_uri() . '/assets/dist/scripts/dev_message.js', NULL, NULL, true);
   wp_register_script('bxslider', '//cdnjs.cloudflare.com/ajax/libs/bxslider/4.2.15/jquery.bxslider.min.js', NULL, NULL, true);
   wp_register_script('prism', get_stylesheet_directory_uri() . '/assets/dist/scripts/vendor/prism.min.js', NULL, NULL, true);
-  // wp_register_script('prism', '//cdnjs.cloudflare.com/ajax/libs/prism/1.15.0/prism.min.js', NULL, NULL, true);
   wp_register_script('cookies', get_stylesheet_directory_uri() . '/assets/dist/scripts/vendor/jquery.cookie.min.js', array('jquery'), NULL, true);
   wp_register_script('lean_modal', get_stylesheet_directory_uri() . '/assets/dist/scripts/vendor/jquery.leanModal.min.js', array('jquery'), NULL, true);
-  wp_register_script('highlight-js', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.7/highlight.min.js', array(), NULL, true);
+  // wp_register_script('highlight-js', '//cdnjs.cloudflare.com/ajax/libs/highlight.js/8.7/highlight.min.js', array(), NULL, true);
   wp_register_script('smooth-scroll', '//cdnjs.cloudflare.com/ajax/libs/smooth-scroll/15.2.1/smooth-scroll.min.js', NULL, NULL, true);
   wp_register_script('featherlight', '//cdnjs.cloudflare.com/ajax/libs/featherlight/1.7.13/featherlight.min.js', NULL, NULL, true);
   wp_register_script('jquery-easing', get_stylesheet_directory_uri() . '/assets/dist/scripts/vendor/jquery.easing.1.3.js', NULL, NULL, true);
@@ -80,10 +70,6 @@ function add_js() {
   if ( is_page('features') ) {
     wp_enqueue_script('featherlight');
     wp_enqueue_script('prism');
-  }
-
-  if ( is_blog() ) {
-    wp_enqueue_script('featherlight');
   }
 
   if ( is_page('workflows') ) {
