09400d255219c3ec319034b5712473e85ef05831
kachunchau <kachun@kachunchau.com>
2019-11-04 20:14:08 +0800

    add tutorial loop

--- page-getting-started.php
+++ page-getting-started.php
@@ -20,18 +20,72 @@
 </section>
 <section class="bg-offwhite">
   <div class="inner large-inner center-text">
-    <div class="flex flex-column">
-      <div class="order-1 xs-order-0 xs-bottom-margin-smedium">
-        <picture class="d-inline-block bg-dots-offset">
-          <source media="(max-width: 768px)" srcset="/wp-content/themes/vero/assets/dist/images/landing-pages/push-notifications/workflow-mobile.png 1x, /wp-content/themes/vero/assets/dist/images/landing-pages/push-notifications/workflow-mobile@2x.png 2x">
-          <source media="(min-width: 769px)" srcset="/wp-content/themes/vero/assets/dist/images/landing-pages/push-notifications/workflow-desktop.png 1x, /wp-content/themes/vero/assets/dist/images/landing-pages/push-notifications/workflow-desktop@2x.png 2x">
-          <img class="align-middle responsive-image" src="/wp-content/themes/vero/assets/dist/images/landing-pages/push-notifications/workflow-desktop.png" alt="Workflow">
-        </picture>
-      </div>
+    <h2 class="chunk semi-bold">Vero Basics</h2>
+  </div>
+</section>
+<section>
+  <div class="inner xlarge-inner">
+    <div class="resources-section resources-section-secondary resources-section-thirds tutorials-posts">
+      <h2 class="chunk semi-bold center-text">Tutorials</h2>
 
-      <div class="order-0 xs-order-1 bottom-margin-smedium xs-no-bottom-margin">
-        <h2 class="chunk bottom-margin-micro xs-bottom-margin-tiny">One workflow for email and push messages</h2>
-        <p class="large">Easily manage and monitor complex customer journeys in one place.</p>
+      <div class="grid">
+        <?php
+          $custom_query = new WP_Query(array(
+            'posts_per_page' => 3,
+            'category_name'  => 'tutorials',
+            'post_type'      => array('post', 'tutorials')
+          ));
+
+          while( $custom_query->have_posts() ) : $custom_query->the_post();
+            $featured_image = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
+            $image_id       = get_post_thumbnail_id();
+            $image_alt      = get_post_meta($image_id, '_wp_attachment_image_alt', true);
+          ?>
+
+        <article class="entry entry-hover" itemprop="blogPosts" itemscope itemtype="http://schema.org/BlogPosting">
+          <a class="show entry-aside" href="<?php the_permalink(); ?>">
+            <?php if ( has_post_thumbnail() ): ?>
+              <?php
+                if( !empty($image_alt) ) {
+                  $alt_text = $image_alt;
+                } else {
+                  $alt_text = get_the_title();
+                }
+
+                the_post_thumbnail('', array(
+                  'class' => 'entry-image',
+                  'alt'   => $alt_text
+                ));
+              ?>
+            <?php endif; ?>
+          </a>
+
+          <div class="entry-body">
+            <div class="entry-header">
+              <h2 class="entry-title regular no-margin"><a href="<?php the_permalink(); ?>"><span class="entry-underline"><?php the_title(); ?></span></a></h2>
+            </div>
+
+            <div class="entry-content bottom-margin-smedium">
+              <?php if ( get_field('custom_excerpt') ): ?>
+                <p><?php the_field('custom_excerpt') ?></p>
+              <?php else: ?>
+                <?php the_excerpt(); ?>
+              <?php endif ?>
+            </div>
+
+            <div class="entry-footer">
+              <?php if ( get_field('custom_read_more') ): ?>
+                <a class="regular underline-link" href="<?php the_permalink(); ?>"><?php the_field('custom_read_more') ?></a>
+              <?php else: ?>
+                <a class="regular underline-link" href="<?php the_permalink(); ?>">Read&nbsp;more</a>
+              <?php endif ?>
+            </div>
+          </div>
+        </article>
+
+        <?php endwhile;
+          wp_reset_postdata();
+        ?>
       </div>
     </div>
   </div>
