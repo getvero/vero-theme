577772da01a03c89a39f1a530933fd4a568baf94
kachunchau <kachun@kachunchau.com>
2019-11-07 17:44:27 +0800

    video landing tweaks

--- page-getting-started.php
+++ page-getting-started.php
@@ -9,7 +9,7 @@
 
   include 'pages-shared/static-header.php';
 ?>
-<section class="getting-started-hero">
+<section class="double-padding getting-started-hero">
   <div class="inner tiny-inner  center-text">
     <!-- <span class="flex items-center justify-center feature-label bottom-margin-small xs-bottom-margin-medium"><a class="unstyled" href="/features">Resources</a>
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"><path stroke="#9D9D9D" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 11.5L10.5 8 7 4.5" fill="none" fill-rule="evenodd"/></svg> Push Notifications</span> -->
@@ -18,14 +18,14 @@
 
     <ul class="unstyled-list flex items-center justify-center subnav">
       <li class="subnav-item right-margin-smedium">
-        <a class="flex flex-column justify-center border-radius-2 border padding-small unstyled" href="#vero-basics">
+        <a class="flex flex-column justify-center border-radius-2 border-light padding-small unstyled" href="#vero-basics">
           <img class="bottom-margin-micro" src="https://s3.amazonaws.com/assets.getvero.com/marketing-redesign/icon/getting-started/vero-basics.svg" alt="Centralize your data">
 
           <span class="atomic">Vero Basics</span>
         </a>
       </li>
       <li class="subnav-item">
-        <a class="flex flex-column justify-center border-radius-2 border padding-small unstyled" href="#tutorials">
+        <a class="flex flex-column justify-center border-radius-2 border-light padding-small unstyled" href="#tutorials">
           <img class="bottom-margin-micro" src="https://s3.amazonaws.com/assets.getvero.com/marketing-redesign/icon/getting-started/tutorials.svg" alt="Centralize your data">
 
           <span class="atomic">Tutorials</span>
@@ -34,87 +34,68 @@
     </ul>
   </div>
 </section>
-<!-- <section class="no-padding">
-  <div class="inner xlarge-inner">
-    <ul class="unstyled-list flex items-center justify-center">
-      <li>
-        <a class="border-radius-2 border-light padding-small" href="#vero-basics">Vero Basics</a>
-      </li>
-      <li>
-        <a class="border-radius-2 border-light padding-small" href="#tutorials">Tutorials</a>
-      </li>
-    </ul>
-  </div>
-</section> -->
-<section class="jump-link" id="vero-basics">
+<section class="jump-link getting-started-videos" id="vero-basics">
   <div class="bg-offwhite">
     <div class="inner xlarge-inner center-text">
-      <h2 class="chunk semi-bold">Vero Basics</h2>
+      <h2 class="chunk semi-bold bottom-margin-smedium">Vero Basics</h2>
 
       <script src="//fast.wistia.com/assets/external/E-v1.js" async></script>
 
-
       <ul class="unstyled-list grid grid-halfs">
-        <li>
+        <li class="video-item">
           <script src="//fast.wistia.com/embed/medias/47fshhahqk.jsonp" async></script>
-          <div class="wistia_responsive_padding border border-radius-2 shadow-2 bottom-margin-small" style="padding:56.25% 0 0 0;position:relative;">
-            <div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
-              <div class="wistia_embed wistia_async_47fshhahqk seo=false videoFoam=true popover=true popoverAnimateThumbnail=true border-radius-2" style="height:100%;width:100%">&nbsp;</div>
-            </div>
+          <div class="video-ratio border-light bottom-margin-small">
+            <div class="video-embed wistia_embed wistia_async_47fshhahqk seo=false videoFoam=true popover=true popoverAnimateThumbnail=true"></div>
           </div>
 
           <div class="wistia_embed wistia_async_47fshhahqk popover=true popoverBorderWidth=2 popoverContent=link bottom-margin-micro">
-            <h3 class="atomic pointer no-margin">Creating a workflow</h3>
+            <h3 class="micro pointer no-margin">Creating a workflow</h3>
           </div>
 
-          <a class="pill pill--primary-light" href="https://help.getvero.com/articles/creating-a-new-workflow.html">View help article</a>
+          <a class="annotation pill pill--primary" href="https://help.getvero.com/articles/creating-a-new-workflow.html">View help article</a>
 
           <!-- https://getvero.wistia.com/medias/47fshhahqk -->
         </li>
-        <li>
+        <li class="video-item">
           <script src="//fast.wistia.com/embed/medias/wmvd7zhzp1.jsonp" async></script>
-          <div class="wistia_responsive_padding border border-radius-2 shadow-2 bottom-margin-small" style="padding:56.25% 0 0 0;position:relative;">
-            <div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
-              <div class="wistia_embed wistia_async_wmvd7zhzp1 seo=false videoFoam=true popover=true popoverAnimateThumbnail=true border-radius-2" style="height:100%;width:100%">&nbsp;</div>
-            </div>
+          <div class="video-ratio border-light bottom-margin-small">
+            <div class="video-embed wistia_embed wistia_async_wmvd7zhzp1 seo=false videoFoam=true popover=true popoverAnimateThumbnail=true"></div>
           </div>
 
-          <h3 class="atomic">Creating a newsletter</h3>
+          <h3 class="micro">Creating a newsletter</h3>
           <!-- https://getvero.wistia.com/medias/wmvd7zhzp1 -->
 
-          <a class="pill pill--primary" href="https://help.getvero.com/articles/create-a-segment.html">View help article</a>
+          <a class="annotation pill pill--primary" href="https://help.getvero.com/articles/create-a-segment.html">View help article</a>
         </li>
-        <li>
+        <li class="video-item">
           <script src="//fast.wistia.com/embed/medias/dndpvc0epi.jsonp" async></script>
-          <div class="wistia_responsive_padding border border-radius-2 shadow-2 bottom-margin-small" style="padding:56.25% 0 0 0;position:relative;">
-            <div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
-              <div class="wistia_embed wistia_async_dndpvc0epi seo=false videoFoam=true popover=true popoverAnimateThumbnail=true border-radius-2" style="height:100%;width:100%">&nbsp;</div>
-            </div>
+          <div class="video-ratio border-light bottom-margin-small">
+            <div class="video-embed wistia_embed wistia_async_dndpvc0epi seo=false videoFoam=true popover=true popoverAnimateThumbnail=true"></div>
           </div>
 
-          <h3 class="atomic">Creating a segment</h3>
+          <h3 class="micro">Creating a segment</h3>
           <!-- https://getvero.wistia.com/medias/dndpvc0epi -->
 
-          <a class="pill pill--primary" href="https://help.getvero.com/articles/create-a-segment.html">View help article</a>
+          <a class="annotation pill pill--primary" href="https://help.getvero.com/articles/create-a-segment.html">View help article</a>
         </li>
-        <li>
+        <li class="video-item">
           <script src="//fast.wistia.com/embed/medias/rvd9pidq6p.jsonp" async></script>
-          <div class="wistia_responsive_padding border border-radius-2 shadow-2 bottom-margin-small" style="padding:56.25% 0 0 0;position:relative;">
-            <div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
-              <div class="wistia_embed wistia_async_rvd9pidq6p seo=false videoFoam=true popover=true popoverAnimateThumbnail=true border-radius-2" style="height:100%;width:100%">&nbsp;</div>
-            </div>
+          <div class="video-ratio border-light bottom-margin-small">
+            <div class="video-embed wistia_embed wistia_async_rvd9pidq6p seo=false videoFoam=true popover=true popoverAnimateThumbnail=true"></div>
           </div>
 
-          <h3 class="atomic">Creating an event</h3>
+          <h3 class="micro">Creating an event</h3>
           <!-- https://getvero.wistia.com/medias/rvd9pidq6p -->
+
+          <a class="annotation pill pill--primary" href="https://help.getvero.com/articles/create-a-segment.html">View help article</a>
         </li>
       </ul>
     </div>
   </div>
 </section>
-<section class="jump-link" id="tutorials">
+<section class="jump-link border-bottom-light" id="tutorials">
   <div class="inner xlarge-inner center-text">
-    <h2 class="chunk semi-bold bottom-margin-small center-text">Tutorials</h2>
+    <h2 class="chunk semi-bold bottom-margin-smedium center-text">Tutorials</h2>
 
     <div class="grid grid-thirds">
       <?php
@@ -150,7 +131,7 @@
 
         <div class="entry-body">
           <div class="entry-header">
-            <h3 class="atomic semi-bold no-margin"><a class="unstyled" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+            <h3 class="micro semi-bold no-margin"><a class="unstyled" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
           </div>
         </div>
       </article>
@@ -163,9 +144,9 @@
 </section>
 <section class="bg-offwhite">
   <div class="inner tiny-inner center-text">
-    <h4 class="chunk bottom-margin-small">Design, manage and optimize your mobile push messages</h4>
+    <h4 class="chunk bottom-margin-smedium">Design, manage and optimize your mobile push messages</h4>
 
-    <a class="btn btn-success btn-large bottom-margin-small" href="https://app.getvero.com/signup">Get started</a>
+    <a class="btn btn-success btn-large bottom-margin-smedium" href="https://app.getvero.com/signup">Get started</a>
 
     <p class="annotation faded">No credit card details required</p>
   </div>
