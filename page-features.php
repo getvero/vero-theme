5a513c99dddc752c77f7d710edc1759e881d02c9
kachunchau <kachun@kachunchau.com>
2019-12-20 17:34:02 +0800

    add images

--- page-features.php
+++ page-features.php
@@ -28,17 +28,16 @@
     <p class="large">Your customers never sleep&mdash;they're 24/7, 365, global. Use automation to extend your team and help more customers, more often.</p>
   </div>
 
-
   <div class="inner">
-    <input class="hide automation-tab-radio--1" id="automation-tab-1" type="radio" name="automation-tabs" checked>
-    <input class="hide automation-tab-radio--2" id="automation-tab-2" type="radio" name="automation-tabs">
+    <input class="automation-tab-radio-1" id="automation-tab-1" type="radio" name="automation-tabs" checked>
+    <input class="automation-tab-radio-2" id="automation-tab-2" type="radio" name="automation-tabs">
 
     <ul class="unstyled-list bottom-margin-lg flex justify-center feature-tab-switcher border-bottom">
       <li>
-        <label class="feature-tab-label automation-label--1 semi-bold atomic" for="automation-tab-1">Workflows</label>
+        <label class="feature-tab-label automation-label-1 semi-bold atomic" for="automation-tab-1">Workflows</label>
       </li>
       <li>
-        <label class="feature-tab-label automation-label--2 semi-bold atomic" for="automation-tab-2">Recurring newsletters</label>
+        <label class="feature-tab-label automation-label-2 semi-bold atomic" for="automation-tab-2">Recurring newsletters</label>
       </li>
     </ul>
 
@@ -74,113 +73,143 @@
   </div>
 </section>
 <section class="double-padding bg-dark-blue-lighter">
-  <div class="inner tiny-inner center-text">
+  <div class="inner tiny-inner flush-bottom bottom-margin-lg center-text">
     <h2 class="chunk semi-bold bottom-margin-sm">Customers, not <strong>lists</strong></h2>
     <p class="large">Create one, rich, central view of each customer. Reduce duplication, create audiences faster and have confidence in what your customers are seeing.</p>
   </div>
   <div class="inner">
-    <ul class="unstyled-list flex justify-center feature-switcher">
-      <li><a class="feature-switcher-btn" href="#2a">Custom attributes</a></li>
-      <li><a class="feature-switcher-btn" href="#2b">Activity tracking</a></li>
-      <li><a class="feature-switcher-btn" href="#2c">Dynamic newsfeed</a></li>
+    <input class="hide customers-tab-radio-1" id="customers-tab-1" type="radio" name="customers-tabs" checked>
+    <input class="hide customers-tab-radio-2" id="customers-tab-2" type="radio" name="customers-tabs">
+    <input class="hide customers-tab-radio-3" id="customers-tab-3" type="radio" name="customers-tabs">
+
+    <ul class="unstyled-list bottom-margin-lg flex justify-center feature-tab-switcher border-bottom">
+      <li>
+        <label class="feature-tab-label customers-label-1 semi-bold atomic" for="customers-tab-1">Custom attributes</label>
+      </li>
+      <li>
+        <label class="feature-tab-label customers-label-2 semi-bold atomic" for="customers-tab-2">Activity tracking</label>
+      </li>
+      <li>
+        <label class="feature-tab-label customers-label-3 semi-bold atomic" for="customers-tab-3">Dynamic newsfeed</label>
+      </li>
     </ul>
 
-    <div class="w-sidebar w-sidebar--feature-default">
-      <div class="col-main">
-        <h3 class="tubs semi-bold">Custom attributes</h3>
+    <div class="customers-tab customers-tab--1">
+      <div class="w-sidebar w-sidebar--feature-default items-center">
+        <div class="col-main">
+          <h3 class="tubs semi-bold bottom-margin-sm">Custom attributes</h3>
 
-        <p class="medium">Use attributes to store customer inputs, demographic data, subscription preferences.</p>
+          <p class="medium bottom-margin-md">Use attributes to store customer inputs, demographic data, subscription preferences.</p>
 
-        <a href="/event-manager">Learn more</a>
-      </div>
-      <div class="col-aside">
-        <p>Image</p>
-      </div>
-    </div>
-  </div>
-  <div class="inner">
-    <div class="w-sidebar w-sidebar--feature-default">
-      <div class="col-main lg-order-1">
-        <p>Image</p>
-      </div>
-      <div class="col-aside lg-order-2">
-        <h3 class="tubs semi-bold">Activity tracking</h3>
-
-        <p class="medium">Keep a record of what your customers do (and don't do) on both your site and in your product.</p>
-
-        <p class="medium">Integrations along with our JavaScript, Ruby, Python and other libraries make powerful activity tracking simpler.</p>
-
-        <a href="">Learn more</a>
+          <a class="btn btn-primary btn--outline" href="/event-manager">Learn more</a>
+        </div>
+        <div class="col-aside lg-right-text">
+          <div class="d-inline-block bg-dots-offset">
+            <img src="/wp-content/themes/vero/assets/dist/images/features/custom-attributes.svg" alt="">
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="inner">
-    <div class="w-sidebar w-sidebar--feature-default">
-      <div class="col-main">
-        <h3 class="tubs semi-bold">Dynamic newsfeed</h3>
+    <div class="customers-tab customers-tab--2">
+      <div class="w-sidebar w-sidebar--feature-default">
+        <div class="col-main lg-order-1">
+          <p>Image</p>
+        </div>
+        <div class="col-aside lg-order-2">
+          <h3 class="tubs semi-bold">Activity tracking</h3>
 
-        <p class="medium">Pull data at the time your message is sent from your own, or public, APIs and personalize what your customers see at a whole new level.</p>
+          <p class="medium">Keep a record of what your customers do (and don't do) on both your site and in your product.</p>
 
-        <a href="/fusion">Learn more</a>
+          <p class="medium">Integrations along with our JavaScript, Ruby, Python and other libraries make powerful activity tracking simpler.</p>
+
+          <a href="">Learn more</a>
+        </div>
       </div>
-      <div class="col-aside">
-        <p>Image</p>
+    </div>
+    <div class="customers-tab customers-tab--3">
+      <div class="w-sidebar w-sidebar--feature-default items-center">
+        <div class="col-main">
+          <h3 class="tubs semi-bold bottom-margin-sm">Dynamic data feeds</h3>
+
+          <p class="medium bottom-margin-md">Pull data at the time your message is sent from your own, or public, APIs and personalize what your customers see at a whole new level.</p>
+
+          <a class="btn btn-primary btn--outline" href="/fusion">Learn more</a>
+        </div>
+        <div class="col-aside lg-right-text">
+          <div class="d-inline-block bg-dots-offset">
+            <img src="/wp-content/themes/vero/assets/dist/images/features/dynamic-data-feeds.svg" alt="">
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </section>
 <section class="double-padding">
-  <div class="inner tiny-inner center-text">
+  <div class="inner tiny-inner flush-bottom bottom-margin-lg center-text">
     <h2 class="chunk semi-bold bottom-margin-sm">A workflow the whole team can use to message customers</h2>
     <p class="large">Separate message design from message code. An easy-to-use UI to create, edit and deploy messages to customers.</p>
   </div>
   <div class="inner">
-    <ul class="unstyled-list flex justify-center feature-switcher">
-      <li><a class="feature-switcher-btn" href="#3a">Templates</a></li>
-      <li><a class="feature-switcher-btn" href="#3b">Personalize</a></li>
-      <li><a class="feature-switcher-btn" href="#3c">Multi-language</a></li>
+    <input class="hide something-tab-radio-1" id="something-tab-1" type="radio" name="something-tabs" checked>
+    <input class="hide something-tab-radio-2" id="something-tab-2" type="radio" name="something-tabs">
+    <input class="hide something-tab-radio-3" id="something-tab-3" type="radio" name="something-tabs">
+
+    <ul class="unstyled-list bottom-margin-lg flex justify-center feature-tab-switcher border-bottom">
+      <li>
+        <label class="feature-tab-label something-label-1 semi-bold atomic" for="something-tab-1">Templates</label>
+      </li>
+      <li>
+        <label class="feature-tab-label something-label-2 semi-bold atomic" for="something-tab-2">Personalize</label>
+      </li>
+      <li>
+        <label class="feature-tab-label something-label-3 semi-bold atomic" for="something-tab-3">Multi-language</label>
+      </li>
     </ul>
 
-    <div class="w-sidebar w-sidebar--feature-default">
-      <div class="col-main">
-        <h3 class="tubs semi-bold">Templates</h3>
-        <p class="medium">Manage email designs centrally and synchronise changes to automated campaigns instantly.</p>
-        <p class="medium">Include point-and-click regions to make creating and editing emails a safe and on-brand experience.</p>
-      </div>
-      <div class="col-aside">
-        <p>Image</p>
-      </div>
-    </div>
-  </div>
-  <div class="inner">
-    <div class="w-sidebar w-sidebar--feature-default">
-      <div class="col-main lg-order-2">
-        <h3 class="tubs semi-bold">Personalize emails with dynamic data and templating</h3>
-        <p class="medium">Bring the full power of the Liquid templating language to your campaigns and ensure your emails are personal.</p>
-        <p class="medium">Make including dynamic data like <code class="language-liquid">first_name</code> and <code class="language-liquid">last_name</code> a breeze. Use powerful <code class="language-liquid">if</code>, <code class="language-liquid">else</code> and case` statements to fully customise content or templates.</p>
-      </div>
-      <div class="col-aside lg-order-1">
-        <p>Image</p>
+    <div class="something-tab something-tab--1">
+      <div class="w-sidebar w-sidebar--feature-default items-center">
+        <div class="col-main">
+          <h3 class="tubs semi-bold">Templates</h3>
+          <p class="medium">Manage email designs centrally and synchronise changes to automated campaigns instantly.</p>
+          <p class="medium">Include point-and-click regions to make creating and editing emails a safe and on-brand experience.</p>
+        </div>
+        <div class="col-aside lg-right-text">
+          <img src="/wp-content/themes/vero/assets/dist/images/features/templates.svg" alt="">
+        </div>
       </div>
     </div>
-  </div>
-  <div class="inner">
-    <div class="w-sidebar w-sidebar--feature-default">
-      <div class="col-main">
-        <h3 class="tubs semi-bold">Talk to customers in their language</h3>
-        <p class="medium">Create a variant in any language, for any message campaign.</p>
 
-        <p class="medium bottom-margin-lg">Vero automatically sends each customer the right language variant, wherever it's available.</p>
-
-        <a class="medium" href="/multi-language-campaigns">Learn more</a>
+    <div class="something-tab something-tab--2">
+      <div class="w-sidebar w-sidebar--feature-default">
+        <div class="col-main lg-order-2">
+          <h3 class="tubs semi-bold">Personalize emails with dynamic data and templating</h3>
+          <p class="medium">Bring the full power of the Liquid templating language to your campaigns and ensure your emails are personal.</p>
+          <p class="medium">Make including dynamic data like <code class="language-liquid">first_name</code> and <code class="language-liquid">last_name</code> a breeze. Use powerful <code class="language-liquid">if</code>, <code class="language-liquid">else</code> and case` statements to fully customise content or templates.</p>
+        </div>
+        <div class="col-aside lg-order-1">
+          <p>Image</p>
+        </div>
       </div>
-      <div class="col-aside">
-        <p>Image</p>
+    </div>
+
+    <div class="something-tab something-tab--3">
+      <div class="w-sidebar w-sidebar--feature-default items-center">
+        <div class="col-main">
+          <h3 class="tubs semi-bold">Talk to customers in their language</h3>
+          <p class="medium">Create a variant in any language, for any message campaign.</p>
+
+          <p class="medium bottom-margin-lg">Vero automatically sends each customer the right language variant, wherever it's available.</p>
+
+          <a class="medium" href="/multi-language-campaigns">Learn more</a>
+        </div>
+        <div class="col-aside lg-right-text">
+          <img src="/wp-content/themes/vero/assets/dist/images/features/multi-language.svg" alt="">
+        </div>
       </div>
     </div>
   </div>
 </section>
-<section class="double-padding">
+<section class="double-padding bg-dark-blue-lighter">
   <div class="inner tiny-inner center-text">
     <h2 class="chunk semi-bold bottom-margin-sm">Iterate on and A/B test your messages</h2>
     <p class="large">Don't guess what works: test it. Built-in tools to test one-off and automated campaigns.</p>
