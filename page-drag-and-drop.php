5b7650738271c08c5ff65924d033d1e9d9bde74e
kachunchau <kachun@kachunchau.com>
2020-03-23 11:34:11 +0800

    add more styles

--- page-drag-and-drop.php
+++ page-drag-and-drop.php
@@ -9,7 +9,7 @@
 
   include 'pages-shared/static-header.php';
 ?>
-<section class="push-notifications-hero">
+<section class="drag-and-drop-hero">
   <section>
     <div class="inner tiny-inner flush-bottom center-text">
       <span class="flex items-center justify-center atomic semi-bold font-brand-gray-dark bottom-margin-sm xs-bottom-margin-lg">
@@ -28,9 +28,97 @@
       </div>
     </div>
     <div class="inner xlarge-inner">
-      <img src="/wp-content/themes/vero/assets/dist/images/landing-pages/drag-and-drop/hero-1.png" alt="Vero's drag and drop editor main screen">
+      <ul class="unstyled-list relative center-text drag-and-drop-hero-imgs">
+        <li class="drag-and-drop-hero-img drag-and-drop-hero-img--1 relative">
+          <img class="responsive-image align-middle border border-radius-2 shadow-1" src="/wp-content/themes/vero/assets/dist/images/landing-pages/drag-and-drop/hero-1.jpg" srcset="/wp-content/themes/vero/assets/dist/images/landing-pages/drag-and-drop/hero-1@2x.jpg 2x" alt="Vero's drag and drop editor main screen">
+        </li>
+        <li class="drag-and-drop-hero-img drag-and-drop-hero-img--2">
+          <img class="responsive-image align-middle border border-radius-2 shadow-1" src="/wp-content/themes/vero/assets/dist/images/landing-pages/drag-and-drop/hero-2.jpg" srcset="/wp-content/themes/vero/assets/dist/images/landing-pages/drag-and-drop/hero-2@2x.jpg 2x" alt="Image pane">
+        </li>
+      </ul>
     </div>
   </section>
+</section>
+<section>
+  <div class="inner">
+    <div class="w-sidebar w-sidebar--feature-md">
+      <div class="col-main order-2 md-order-1">
+        <h2 class="chunk bottom-margin-sm">Templates that work everywhere</h2>
+        <p class="large">Easily design and build templates that work across all major devices and clients responsively. With our drag and drop editor there's no need to know HTML and all it's quirks.</p>
+      </div>
+      <div class="col-aside order-1 md-order-2 center-text md-right-text">
+        <img class="responsive-image align-middle" src="/wp-content/themes/vero/assets/dist/images/landing-pages/drag-and-drop/templates-that-work.png" srcset="/wp-content/themes/vero/assets/dist/images/landing-pages/drag-and-drop/templates-that-work@2x.png 2x" alt="Templates that work everywhere">
+      </div>
+    </div>
+  </div>
+  <div class="inner">
+    <div class="w-sidebar w-sidebar--feature-md items-center">
+      <div class="col-main order-2">
+        <h2 class="chunk bottom-margin-sm">Keep your campaigns consistent</h2>
+        <p class="large">Our WYSIWYG editor gives each member of your team the ability to edit the content of each campaign without worrying about breaking the look and feel of your templates.</p>
+      </div>
+      <div class="col-aside order-1">
+        <img class="responsive-image align-middle" src="/wp-content/themes/vero/assets/dist/images/landing-pages/drag-and-drop/campaigns-consistent.svg" alt="Keep campaigns consistent">
+      </div>
+    </div>
+  </div>
+  <div class="inner">
+    <div class="w-sidebar w-sidebar--feature-md items-center">
+      <div class="col-main order-2 md-order-1">
+        <h2 class="chunk bottom-margin-sm">Content editing on the fly</h2>
+        <p class="large">Enable your team to build the content they need by creating a library of common snippets that can be placed in your templates on the fly.</p>
+      </div>
+      <div class="col-aside order-1 md-order-2">
+        <p>Image</p>
+      </div>
+    </div>
+  </div>
+  <div class="inner">
+    <div class="w-sidebar w-sidebar--feature-md items-center">
+      <div class="col-main order-2">
+        <h2 class="chunk bottom-margin-sm">Easily personalize your messages</h2>
+        <p class="large">Use dynamic variables throughout your drag-and-drop template with the click of a button.</p>
+      </div>
+      <div class="col-aside order-1">
+        <img class="responsive-image align-middle" src="/wp-content/themes/vero/assets/dist/images/landing-pages/drag-and-drop/personalizing-your-messages.svg" alt="Personalizing your messages">
+      </div>
+    </div>
+  </div>
+</section>
+<section class="bg-dark-blue-lighter">
+  <div class="inner large-inner">
+    <h2 class="chunk">And more features</h2>
+
+    <ul class="unstyled-list">
+      <li>
+        <h3 class="micro bottom-margin-xs">Template defaults</h3>
+        <p class="medium">Keep your templates on brand by setting default colors and fonts.</p>
+      </li>
+      <li>
+        <h3 class="micro bottom-margin-xs">Liquid data</h3>
+        <p class="medium">Easily personalise by inserting customer or event data into your templates with Liquid.</p>
+      </li>
+      <li>
+        <h3 class="micro bottom-margin-xs">Starting template</h3>
+        <p class="medium">Start from scratch or select one of our professionally designed templates as a starting point.</p>
+      </li>
+      <li>
+        <h3 class="micro bottom-margin-xs">WYSIWYG</h3>
+        <p class="medium">What you see is what you get. The content you're editing is exactly what your customers will see.</p>
+      </li>
+    </ul>
+  </div>
+</section>
+<section>
+  <div class="inner">
+    <h3>Coming soon</h3>
+    <ul class="unstyled-list">
+      <li>
+        <h3 class="micro">Data inspector</h3>
+        <p class="medium">Preview your campaigns with real data to see how exactly they'll look for your customers.</p>
+      </li>
+    </ul>
+  </div>
 </section>
 <?php
   no_content_genesis_footer();
