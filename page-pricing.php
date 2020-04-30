1abe6972eabbfde3d14e1b60ea30573bf74877c3
kachunchau <kachun@kachunchau.com>
2020-04-30 17:10:31 +0800

    style questions

--- page-pricing.php
+++ page-pricing.php
@@ -25,16 +25,19 @@ include 'pages-shared/static-header.php';
         </header>
 
         <div class="pricing-comp-table-body flex flex-column">
-          <span class="show semi-bold tubs">$49 p/m</span>
+          <div class="stack-xxxs">
+            <span class="font-gray-dark show regular annotation">Starting at</span>
+            <span class="show semi-bold tubs">$49/mo</span>
+          </div>
 
-          <div>
-            <span class="font-gray-dark show regular annotation bottom-margin-xxs">Including</span>
+          <div class="stack-xxs">
+            <span class="font-gray-dark show regular annotation">Including</span>
 
             <ul class="unstyled-list">
               <li class="no-margin"><strong>2,000</strong> Customers </li>
-              <li class="no-margin"><strong>10,000</strong> Messages p/m</li>
+              <li class="no-margin"><strong>10,000</strong> Messages/mo</li>
               <li class="no-margin flex items-center">
-                <span class="right-margin-xxs"><strong>1M</strong> Data points p/m</span>
+                <span class="right-margin-xxs"><strong>1M</strong> Data points/mo</span>
 
                 <span class="tooltip" data-label="This is a tooltip">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"><path fill="#CED0D5" d="M8 15A7 7 0 108 1a7 7 0 000 14zm-.412-8.518c.777 0 1.44.37 1.236 1.518-.204 1.148-.478 2.059-.671 2.85-.193.791 1.024.406 1.3.406v.45c-.56.274-1.107.412-1.642.412-.804 0-1.686-.412-1.43-1.581.254-1.17.486-1.903.626-2.537.14-.634-.14-.726-.493-.726-.235 0-.489.046-.763.139v-.564c.707-.245 1.32-.367 1.837-.367zm.906-2.847a1.07 1.07 0 110 2.141 1.07 1.07 0 010-2.14z" fill-rule="evenodd"/></svg>
@@ -44,7 +47,7 @@ include 'pages-shared/static-header.php';
             </ul>
           </div>
 
-          <div>
+          <div class="pricing-features">
             <span class="font-gray-dark show regular annotation bottom-margin-xs">Features</span>
 
             <ul class="unstyled-list annotation">
@@ -80,16 +83,19 @@ include 'pages-shared/static-header.php';
         </header>
 
         <div class="pricing-comp-table-body flex flex-column">
-          <span class="show semi-bold tubs">$199 p/m</span>
+          <div class="stack-xxxs">
+            <span class="font-gray-dark show regular annotation">Starting at</span>
+            <span class="show semi-bold tubs">$199/mo</span>
+          </div>
 
-          <div>
-            <span class="font-gray-dark show regular annotation bottom-margin-xxs">Including</span>
+          <div class="stack-xxs">
+            <span class="font-gray-dark show regular annotation">Including</span>
 
             <ul class="unstyled-list">
               <li class="no-margin"><strong>10,000</strong> Customers </li>
-              <li class="no-margin"><strong>75,000</strong> Messages p/m</li>
+              <li class="no-margin"><strong>75,000</strong> Messages/mo</li>
               <li class="no-margin flex items-center">
-                <span class="right-margin-xxs"><strong>7.5M</strong> Data points p/m</span>
+                <span class="right-margin-xxs"><strong>7.5M</strong> Data points/mo</span>
 
                 <span class="tooltip" data-label="This is a tooltip">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"><path fill="#CED0D5" d="M8 15A7 7 0 108 1a7 7 0 000 14zm-.412-8.518c.777 0 1.44.37 1.236 1.518-.204 1.148-.478 2.059-.671 2.85-.193.791 1.024.406 1.3.406v.45c-.56.274-1.107.412-1.642.412-.804 0-1.686-.412-1.43-1.581.254-1.17.486-1.903.626-2.537.14-.634-.14-.726-.493-.726-.235 0-.489.046-.763.139v-.564c.707-.245 1.32-.367 1.837-.367zm.906-2.847a1.07 1.07 0 110 2.141 1.07 1.07 0 010-2.14z" fill-rule="evenodd"/></svg>
@@ -99,7 +105,7 @@ include 'pages-shared/static-header.php';
             </ul>
           </div>
 
-          <div>
+          <div class="pricing-features">
             <span class="font-gray-dark show regular annotation bottom-margin-xs">Features</span>
 
             <ul class="unstyled-list annotation">
@@ -145,18 +151,21 @@ include 'pages-shared/static-header.php';
         </header>
 
         <div class="pricing-comp-table-body flex flex-column">
-          <span class="show semi-bold tubs">$599 p/m</span>
+          <div class="stack-xxxs">
+            <span class="font-gray-dark show regular annotation">Starting at</span>
+            <span class="show semi-bold tubs">$599/mo</span>
+          </div>
 
-          <div>
-            <span class="font-gray-dark show regular annotation bottom-margin-xxs">Including</span>
+          <div class="stack-xxs">
+            <span class="font-gray-dark show regular annotation">Including</span>
 
             <ul class="unstyled-list">
               <li class="no-margin"><strong>75,000</strong> Customers </li>
-              <li class="no-margin"><strong>375,000</strong> Messages p/m</li>
+              <li class="no-margin"><strong>375,000</strong> Messages/mo</li>
               <li class="no-margin flex items-center">
-                <span class="right-margin-xxs"><strong>37M</strong> Data points p/m</span>
+                <span class="right-margin-xxs"><strong>37M</strong> Data points/mo</span>
 
-                <span class="tooltip" data-label="This is a tooltip">
+                <span class="tooltip" data-label="A 'data point' is any event 'track' that you record, read more in our help docs.">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"><path fill="#CED0D5" d="M8 15A7 7 0 108 1a7 7 0 000 14zm-.412-8.518c.777 0 1.44.37 1.236 1.518-.204 1.148-.478 2.059-.671 2.85-.193.791 1.024.406 1.3.406v.45c-.56.274-1.107.412-1.642.412-.804 0-1.686-.412-1.43-1.581.254-1.17.486-1.903.626-2.537.14-.634-.14-.726-.493-.726-.235 0-.489.046-.763.139v-.564c.707-.245 1.32-.367 1.837-.367zm.906-2.847a1.07 1.07 0 110 2.141 1.07 1.07 0 010-2.14z" fill-rule="evenodd"/></svg>
                 </span>
               </li>
@@ -164,7 +173,7 @@ include 'pages-shared/static-header.php';
             </ul>
           </div>
 
-          <div>
+          <div class="pricing-features">
             <span class="font-gray-dark show regular annotation bottom-margin-xs">Features</span>
 
             <ul class="unstyled-list annotation">
@@ -196,7 +205,7 @@ include 'pages-shared/static-header.php';
             </ul>
           </div>
 
-          <a href="" class="btn btn--outline btn-success full-width top-margin-auto">Try for free</a>
+          <a href="#none" class="btn btn--outline btn-success full-width top-margin-auto">Try for free</a>
         </div>
       </div>
       <div class="flex flex-column">
@@ -205,16 +214,19 @@ include 'pages-shared/static-header.php';
         </header>
 
         <div class="pricing-comp-table-body flex flex-column">
-          <span class="show semi-bold tubs">$1,299 p/m &plus;</span>
+          <div class="stack-xxs">
+            <span class="font-gray-dark show regular annotation">Starting at</span>
+            <span class="show semi-bold tubs">$1,299/mo</span>
+          </div>
 
-          <div>
-            <span class="font-gray-dark show regular annotation bottom-margin-xxs">Including</span>
+          <div class="stack-xxs">
+            <span class="font-gray-dark show regular annotation">Including</span>
 
             <ul class="unstyled-list">
               <li class="no-margin"><strong>250,000&plus;</strong> Customers </li>
-              <li class="no-margin"><strong>1,250,000&plus;</strong> Messages p/m</li>
+              <li class="no-margin"><strong>1,250,000&plus;</strong> Messages/mo</li>
               <li class="no-margin flex items-center">
-                <span><strong>125M&plus;</strong> Data points p/m</span>
+                <span class="right-margin-xxs"><strong>125M&plus;</strong> Data points/mo</span>
 
                 <span class="tooltip" data-label="This is a tooltip">
                   <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"><path fill="#CED0D5" d="M8 15A7 7 0 108 1a7 7 0 000 14zm-.412-8.518c.777 0 1.44.37 1.236 1.518-.204 1.148-.478 2.059-.671 2.85-.193.791 1.024.406 1.3.406v.45c-.56.274-1.107.412-1.642.412-.804 0-1.686-.412-1.43-1.581.254-1.17.486-1.903.626-2.537.14-.634-.14-.726-.493-.726-.235 0-.489.046-.763.139v-.564c.707-.245 1.32-.367 1.837-.367zm.906-2.847a1.07 1.07 0 110 2.141 1.07 1.07 0 010-2.14z" fill-rule="evenodd"/></svg>
@@ -224,7 +236,7 @@ include 'pages-shared/static-header.php';
             </ul>
           </div>
 
-          <div>
+          <div class="pricing-features">
             <span class="font-gray-dark show regular annotation bottom-margin-xs">Features</span>
 
             <ul class="unstyled-list annotation">
@@ -246,13 +258,7 @@ include 'pages-shared/static-header.php';
             </ul>
           </div>
 
-          <footer class="pricing-comp-table-footer flex flex-column items-center stack-xxs">
-            <a href="#none" class="btn btn--outline btn--secondary full-width">Talk to us</a>
-
-            <span class="annotation">or</span>
-
-            <a href="#none" class="btn btn--outline btn-success full-width">Try for free</a>
-          </footer>
+          <a href="#none" class="btn btn--outline btn--secondary full-width top-margin-auto">Talk to us</a>
         </div>
       </div>
     </div>
@@ -758,64 +764,82 @@ include 'pages-shared/static-header.php';
   <div class="inner small-inner">
     <h2 class="chunk regular center-text bottom-margin-xl">Frequently Asked Questions</h2>
 
-    <details class="question-block bottom-margin-lg">
-      <summary class="flex question">
-        <h3 class="micro regular no-bottom-margin">What happens if I go over my subscriber limit?</h3>
+    <ul class="unstyled-list stack-md pricing-questions">
+      <li>
+        <details class="stack-sm">
+          <summary>
+            <div class="flex items-center">
+              <h3 class="micro regular no-bottom-margin">What happens if I go over my subscriber limit?</h3>
 
-        <img class="left-margin-auto question-caret" src="/wp-content/themes/vero/assets/dist/images/home/pricing/caret.svg" alt="">
-      </summary>
-      <summary class="answer">
-        <p>Vero will send you an email when you cross the subscriber threshold of your current plan. Your account will be automatically upgraded to a new plan, which will be reflected in your your next invoice.</p>
-        <p>As your usage fluctuates you can downgrade your plan during the next billing period, ensuring you minimize your spend each month.</p>
-      </summary>
-    </details>
+              <svg class="left-margin-auto" width="32" height="32" xmlns="http://www.w3.org/2000/svg"><path d="M14 23l7-7-7-7h0" stroke="#9D9D9D" stroke-width="4" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"/></svg>
+            </div>
+          </summary>
+          <div class="answer">
+            <p class="medium">Vero will send you an email when you cross the subscriber threshold of your current plan. Your account will be automatically upgraded to a new plan, which will be reflected in your your next invoice.</p>
+            <p class="medium">As your usage fluctuates you can downgrade your plan during the next billing period, ensuring you minimize your spend each month.</p>
+          </div>
+        </details>
+      </li>
+      <li>
+        <details class="stack-sm">
+          <summary>
+            <div class="flex items-center">
+              <h3 class="micro regular no-bottom-margin">What support does Vero provide?</h3>
 
-    <details class="question-block bottom-margin-lg">
-      <summary class="flex question">
-        <h3 class="micro regular no-bottom-margin">What support does Vero provide?</h3>
+              <svg class="left-margin-auto" width="32" height="32" xmlns="http://www.w3.org/2000/svg"><path d="M14 23l7-7-7-7h0" stroke="#9D9D9D" stroke-width="4" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"/></svg>
+            </div>
+          </summary>
+          <div class="answer">
+            <p class="medium">Our global support team is on hand to help 24 hours a day. We have dedicated support team members in Australia, Europe, and the USA. Our email support has a customer satisfaction rating of over 98%.</p>
 
-        <img class="left-margin-auto question-caret" src="/wp-content/themes/vero/assets/dist/images/home/pricing/caret.svg" alt="">
-      </summary>
-      <summary class="answer">
-        <p>Our global support team is on hand to help 24 hours a day. We have dedicated support team members in Australia, Europe, and the USA. Our email support has a customer satisfaction rating of over 98%.</p>
+            <p class="medium">We also provide urgent, emergency support for customers on our high-volume plans - <a class="typeform-share" data-mode="popup" href="https://getvero.typeform.com/to/d9wZ4V" target="_blank">talk to our sales team</a> to learn more.</p>
+          </div>
+        </details>
+      </li>
+      <li>
+        <details class="stack-sm">
+          <summary>
+            <div class="flex items-center">
+              <h3 class="micro regular no-bottom-margin">Can I pay annually?</h3>
 
-        <p>We also provide urgent, emergency support for customers on our high-volume plans - <a class="typeform-share"  data-mode="popup" href="https://getvero.typeform.com/to/d9wZ4V" target="_blank">talk to our sales team</a> to learn more.</p>
-      </summary>
-    </details>
+              <svg class="left-margin-auto" width="32" height="32" xmlns="http://www.w3.org/2000/svg"><path d="M14 23l7-7-7-7h0" stroke="#9D9D9D" stroke-width="4" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"/></svg>
+            </div>
+          </summary>
+          <div class="answer">
+            <p class="medium">You sure can. We offer a 15% discount when you subscribe to an annual plan. <a href="https://help.getvero.com/articles/annual-plan-discount.html">Find out more about annual plans</a>.</p>
+          </div>
+        </details>
+      </li>
+      <li>
+        <details class="stack-sm">
+          <summary>
+            <div class="flex items-center">
+              <h3 class="micro regular no-bottom-margin">Can I use sub-accounts?</h3>
 
-    <details class="question-block bottom-margin-lg">
-      <summary class="flex question">
-        <h3 class="micro regular no-bottom-margin">Can I pay annually?</h3>
+              <svg class="left-margin-auto" width="32" height="32" xmlns="http://www.w3.org/2000/svg"><path d="M14 23l7-7-7-7h0" stroke="#9D9D9D" stroke-width="4" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"/></svg>
+            </div>
+          </summary>
+          <div class="answer">
+            <p class="medium">In Vero, sub-accounts are called <strong>Projects</strong>. Each project has it's own subscriber database, API keys, and campaigns. In other words, it’s like having two or more Vero accounts that you can easily jump between. You can also think of them as <strong>free sandboxes</strong>.</p>
+            <p class="medium">If you’re an agency, you may also use projects to manage multiple clients.</p>
+          </div>
+        </details>
+      </li>
+      <li>
+        <details class="stack-sm">
+          <summary>
+            <div class="flex items-center">
+              <h3 class="micro regular no-bottom-margin">Does the data I send Vero belong to me?</h3>
 
-        <img class="left-margin-auto question-caret" src="/wp-content/themes/vero/assets/dist/images/home/pricing/caret.svg" alt="">
-      </summary>
-      <summary class="answer">
-        <p>You sure can. We offer a 15% discount when you subscribe to an annual plan. <a href="https://help.getvero.com/articles/annual-plan-discount.html">Find out more about annual plans</a>.</p>
-      </summary>
-    </details>
-
-    <details class="question-block bottom-margin-lg">
-      <summary class="flex question">
-        <h3 class="micro regular no-bottom-margin">Can I use sub-accounts?</h3>
-
-        <img class="left-margin-auto question-caret" src="/wp-content/themes/vero/assets/dist/images/home/pricing/caret.svg" alt="">
-      </summary>
-      <summary class="answer">
-        <p>In Vero, sub-accounts are called <strong>Projects</strong>. Each project has it's own subscriber database, API keys, and campaigns. In other words, it’s like having two or more Vero accounts that you can easily jump between. You can also think of them as <strong>free sandboxes</strong>.</p>
-        <p>If you’re an agency, you may also use projects to manage multiple clients.</p>
-      </summary>
-    </details>
-
-    <details class="question-block bottom-margin-lg">
-      <summary class="flex question">
-        <h3 class="micro regular no-bottom-margin">Does the data I send Vero belong to me?</h3>
-
-        <img class="left-margin-auto question-caret" src="/wp-content/themes/vero/assets/dist/images/home/pricing/caret.svg" alt="">
-      </summary>
-      <summary class="answer">
-        <p>Absolutely. The data you send us belongs to you. Vero collects and processes all data securely, in accordance with legislation, including EU GDPR. At any time, you can export or request us to remove your data. Read our <a href="https://www.getvero.com/terms-of-service/" rel="noopener" target="_blank">Terms of Service</a> for more details.</p>
-      </summary>
-    </details>
+              <svg class="left-margin-auto" width="32" height="32" xmlns="http://www.w3.org/2000/svg"><path d="M14 23l7-7-7-7h0" stroke="#9D9D9D" stroke-width="4" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"/></svg>
+            </div>
+          </summary>
+          <div class="answer">
+            <p class="medium">Absolutely. The data you send us belongs to you. Vero collects and processes all data securely, in accordance with legislation, including EU GDPR. At any time, you can export or request us to remove your data. Read our <a href="https://www.getvero.com/terms-of-service/" rel="noopener" target="_blank">Terms of Service</a> for more details.</p>
+          </div>
+        </details>
+      </li>
+    </ul>
   </div>
 </section>
 <section class="bg-gray-lighter">
