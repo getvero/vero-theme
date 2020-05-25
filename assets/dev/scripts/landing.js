b9cda570dd63ad0907617b333cb81321e62cf153
kachunchau <kachun@kachunchau.com>
2020-05-25 17:11:33 +0800

    fix overage switcher loop

--- assets/dev/scripts/landing.js
+++ assets/dev/scripts/landing.js
@@ -96,7 +96,7 @@ jQuery(document).ready(function() {
       pricingAdditionalPrice.textContent     = priceFormat.to(additionalPrice  * 0.01250);
     });
 
-    // Switch overage calculator
+    // Switch overage calculator based on plan
     var primaryLinks     = document.querySelectorAll('.js-overage-calculator');
     var secondaryLinks   = document.querySelectorAll('.js-test');
 
@@ -104,9 +104,8 @@ jQuery(document).ready(function() {
     var pricingPlanPrice = document.querySelector('.js-pricing-plan-price');
 
     function overageSwitcher(links) {
-      for (let [index, link] of links.entries()) {
+      for (const [index, link] of links.entries()) {
         link.addEventListener('click', function() {
-          // alert('Starter ' + index);
           pricingSlider.noUiSlider.on('update', function (values, handle) {
             var currentCustomers = numberFormat.from(pricingSlider.noUiSlider.get())
             var additionalPrice = currentCustomers - pricingPlans[index].customers;
@@ -115,17 +114,29 @@ jQuery(document).ready(function() {
             pricingAdditionalPrice.textContent     = priceFormat.to(additionalPrice * 0.01250);
           });
 
-          // Store values depeneding on plan
+          // Store values depending on plan
           var maxCustomers = 250000;
           var pipValues    = [2000, 15000, 75000, 250000];
           var step         = 1000;
 
-          if (link.textContent == pricingPlans[index].name) {
+          if (index) {
             console.log('Clicking on ' + pricingPlans[index].name);
 
-            pricingSlider.noUiSlider.on('update', function (values, handle) {
+            // Disable handle
+            pricingSlider.noUiSlider.on('change', function (values, handle) {
+              if (values[handle] == 250000) {
+                // return;
+              }
+
               if (numberFormat.from(values[handle]) < pricingPlans[index].customers) {
-                // console.log('Less than enterpise customers');
+                console.log('Less than ' + pricingPlans[index].name + ' customers');
+
+                // pricingSlider.noUiSlider.set(pricingPlans[index].customers);
+              } else if (values[handle] == 250000) {
+              // var origins = slider.getElementsByClassName('noUi-origin');
+                // pricingSlider.setAttribute('disabled', true);
+
+                console.log('Max customers');
               }
             });
           }
