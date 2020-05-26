bc24ebf84c944f34cc9fa733af9f82ddc5630b6a
kachunchau <kachun@kachunchau.com>
2020-05-26 13:08:56 +0800

    close dropdown when clicking outside of element

--- assets/dev/scripts/landing.js
+++ assets/dev/scripts/landing.js
@@ -157,25 +157,28 @@ jQuery(document).ready(function() {
     overageSwitcher(secondaryLinks);
 
     // Dropdown
-    document.addEventListener("click", (evt) => {
-      const flyoutElement = document.getElementById("flyout-example");
-      let targetElement = evt.target; // clicked element
+    var dropdown = document.querySelector('.js-pricing-plan-dropdown');
+
+    // Detect click outside element
+    document.addEventListener('click', function(evt) {
+      var flyoutElement = document.querySelector('.js-pricing-plan-dropdown');
+      var targetElement = evt.target;                                 // clicked element
 
       do {
-          if (targetElement == flyoutElement) {
-              // This is a click inside. Do nothing, just return.
-              document.getElementById("flyout-debug").textContent = "Clicked inside!";
-              return;
-          }
-          // Go up the DOM
-          targetElement = targetElement.parentNode;
+        if (targetElement == dropdown) {
+          console.log('Click inside');
+
+          // This is a click inside. Do nothing, just return.
+          return;
+        }
+        // Go up the DOM
+        targetElement = targetElement.parentNode;
       } while (targetElement);
 
       // This is a click outside.
-      document.getElementById("flyout-debug").textContent = "Clicked outside!";
+      // document.getElementById("flyout-debug").textContent = "Clicked outside!";
+      dropdown.classList.remove('fade');
     });
-
-    var dropdown = document.querySelector('.js-pricing-plan-dropdown');
 
     pricingPlanName.addEventListener('click', function() {
       dropdown.classList.toggle('fade');
