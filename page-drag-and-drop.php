22beb230c68ac6ae6f393a1046083e7313539b6d
kachunchau <kachun@kachunchau.com>
2020-06-04 10:56:07 +0800

    blog post center text

--- page-drag-and-drop.php
+++ page-drag-and-drop.php
@@ -11,7 +11,7 @@
 ?>
 <div class="bg-dark-blue drag-and-drop-banner z-9999">
   <a class="font-white" href="/resources/introducing-drag-and-drop-email-content">
-    <p><span>We built this editor <strong>from the ground up</strong> rather than just using a plugin.</span> <span class="font-white underline-link semi-bold">Find out why</span></p>
+    <p class="center-text"><span>We built this editor <strong>from the ground up</strong> rather than just using a plugin.</span> <span class="font-white underline-link semi-bold">Find out why</span></p>
   </a>
 </div>
 <?php
