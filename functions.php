e6d441e044adc65c4391babb0ee06b1ec464fb88
kachunchau <kachun@kachunchau.com>
2020-06-18 14:50:19 +0800

    remove block library css

--- functions.php
+++ functions.php
@@ -217,6 +217,14 @@ function genesischild_theme_setup() {
       $src = remove_query_arg( 'ver', $src );
     return $src;
   }
+
+  # Remove block library as we don't use the Gutenberg editor
+  add_action( 'wp_enqueue_scripts', 'remove_wp_block_library_css', 100 );
+  function remove_wp_block_library_css() {
+    wp_dequeue_style( 'wp-block-library' );
+    wp_dequeue_style( 'wp-block-library-theme' );
+    wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
+  }
 }
 
 ?>
