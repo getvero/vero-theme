c1d426d87b8afdfddae2f9cc71a220f34beb562c
kachunchau <kachun@kachunchau.com>
2020-06-18 15:04:59 +0800

    remove defer on all scripts

--- functions.php
+++ functions.php
@@ -225,14 +225,6 @@ function genesischild_theme_setup() {
     wp_dequeue_style( 'wp-block-library-theme' );
     wp_dequeue_style( 'wc-block-style' ); // Remove WooCommerce block CSS
   }
-
-  add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
-  function defer_parsing_of_js( $url ) {
-    if ( is_user_logged_in() ) return $url; //don't break WP Admin
-    if ( FALSE === strpos( $url, '.js' ) ) return $url;
-    if ( strpos( $url, 'jquery.js' ) ) return $url;
-    return str_replace( ' src', ' defer src', $url );
-  }
 }
 
 ?>
