c01902d44636d109d46f1b1732d5ac492bd55f73
kachunchau <kachun@kachunchau.com>
2020-07-23 18:23:31 +0800

    remove open subscribe form

--- assets/dev/scripts/landing.js
+++ assets/dev/scripts/landing.js
@@ -499,91 +499,91 @@ jQuery(document).ready(function() {
   var openResourcesSubscibeForm  = document.querySelector('.js-open-resources-subscribe-form');
   var closeResourcesSubscibeForm = document.querySelector('.js-close-resources-subscribe-form');
 
-  if (resourcesHeader) {
-    // Open subscribe form
-    openResourcesSubscibeForm.addEventListener('click', function() {
-      resourcesHeader.classList.add('is-active');
-    });
+  // if (resourcesHeader) {
+  //   // Open subscribe form
+  //   openResourcesSubscibeForm.addEventListener('click', function() {
+  //     resourcesHeader.classList.add('is-active');
+  //   });
 
-    // Close subscribe form
-    closeResourcesSubscibeForm.addEventListener('click', function() {
-      var formActive = resourcesHeader.classList.contains('is-active');
+  //   // Close subscribe form
+  //   closeResourcesSubscibeForm.addEventListener('click', function() {
+  //     var formActive = resourcesHeader.classList.contains('is-active');
 
-      if (formActive) {
-        resourcesHeader.classList.remove('is-active');
-      }
-    });
-  }
+  //     if (formActive) {
+  //       resourcesHeader.classList.remove('is-active');
+  //     }
+  //   });
+  // }
 
-  jQuery('.js-subscribe-form').each(function(index) {
-    jQuery(this).on('submit', function() {
-      event.preventDefault();
+  // jQuery('.js-subscribe-form').each(function(index) {
+  //   jQuery(this).on('submit', function() {
+  //     event.preventDefault();
 
-      try {anonymous_id = window.analytics._user.anonymousId();}
-      catch {}
+  //     try {anonymous_id = window.analytics._user.anonymousId();}
+  //     catch {}
 
-      if(typeof anonymous_id !== 'undefined'){
-        // Append the Segment.com anonymous_id
-        jQuery('<input />').attr('type', 'hidden')
-        .attr('name', 'anonymous_id')
-        .attr('value', anonymous_id)
-        .appendTo('.js-subscribe-form');
-      }
+  //     if(typeof anonymous_id !== 'undefined'){
+  //       // Append the Segment.com anonymous_id
+  //       jQuery('<input />').attr('type', 'hidden')
+  //       .attr('name', 'anonymous_id')
+  //       .attr('value', anonymous_id)
+  //       .appendTo('.js-subscribe-form');
+  //     }
 
-      // Append page path and URL
-      jQuery('<input />').attr('type', 'hidden')
-        .attr('name', 'page_path')
-        .attr('value', window.location.pathname)
-        .appendTo('.js-subscribe-form');
-      jQuery('<input />').attr('type', 'hidden')
-        .attr('name', 'page_url')
-        .attr('value', window.location.href)
-        .appendTo('.js-subscribe-form');
+  //     // Append page path and URL
+  //     jQuery('<input />').attr('type', 'hidden')
+  //       .attr('name', 'page_path')
+  //       .attr('value', window.location.pathname)
+  //       .appendTo('.js-subscribe-form');
+  //     jQuery('<input />').attr('type', 'hidden')
+  //       .attr('name', 'page_url')
+  //       .attr('value', window.location.href)
+  //       .appendTo('.js-subscribe-form');
 
-      // needs for recaptacha ready
-      grecaptcha.ready(function() {
-        // do request for recaptcha token
-        // response is promise with passed token
-        grecaptcha.execute('6LfUD_YUAAAAAO5FOQgHwsQSEMzOZYEPHEo_DZRX', {action: 'create_blog_subscription'}).then(function(token) {
+  //     // needs for recaptacha ready
+  //     grecaptcha.ready(function() {
+  //       // do request for recaptcha token
+  //       // response is promise with passed token
+  //       grecaptcha.execute('6LfUD_YUAAAAAO5FOQgHwsQSEMzOZYEPHEo_DZRX', {action: 'create_blog_subscription'}).then(function(token) {
 
-          // add token to form
-          jQuery('.js-subscribe-form').eq(index).prepend('<input type="hidden" name="g-recaptcha-response" value="' + token + '">');
+  //         // add token to form
+  //         jQuery('.js-subscribe-form').eq(index).prepend('<input type="hidden" name="g-recaptcha-response" value="' + token + '">');
 
-          var formEl = jQuery('.js-subscribe-form');
+  //         var formEl = jQuery('.js-subscribe-form');
 
-          jQuery.ajax({
-            type: 'POST',
-            url: formEl.prop('action'),
-            accept: {
-              javascript: 'application/javascript'
-            },
-            data: formEl.serialize()
-          }).done(function(data) {
-            // console.log('submitted');
+  //         jQuery.ajax({
+  //           type: 'POST',
+  //           url: formEl.prop('action'),
+  //           accept: {
+  //             javascript: 'application/javascript'
+  //           },
+  //           data: formEl.serialize()
+  //         }).done(function(data) {
+  //           // console.log('submitted');
 
-            var thisForm = jQuery('.js-subscribe-form').eq(index);
+  //           var thisForm = jQuery('.js-subscribe-form').eq(index);
 
-            thisForm.addClass('hide');
+  //           thisForm.addClass('hide');
 
-            if (index == 0) {
-              var subscribeMsg     = document.querySelector('.js-subscribe-form-msg');
-              var subscribeMsgText = document.createElement('p');
+  //           if (index == 0) {
+  //             var subscribeMsg     = document.querySelector('.js-subscribe-form-msg');
+  //             var subscribeMsgText = document.createElement('p');
 
-              subscribeMsg.querySelector('h3').textContent = 'Almost there!';
-              subscribeMsgText.textContent = "We've sent you an email to confirm your subscription.";
-              subscribeMsg.append(subscribeMsgText);
-            } else if (index == 1) {
-              var successMsgText = document.createElement('h3');
+  //             subscribeMsg.querySelector('h3').textContent = 'Almost there!';
+  //             subscribeMsgText.textContent = "We've sent you an email to confirm your subscription.";
+  //             subscribeMsg.append(subscribeMsgText);
+  //           } else if (index == 1) {
+  //             var successMsgText = document.createElement('h3');
 
-              successMsgText.className = 'no-margin';
-              successMsgText.textContent = "We've sent you an email to confirm your subscription.";
-              document.querySelector('.form-box').append(successMsgText);
-            }
-          });
-        });
-      });
-    });
-  });
+  //             successMsgText.className = 'no-margin';
+  //             successMsgText.textContent = "We've sent you an email to confirm your subscription.";
+  //             document.querySelector('.form-box').append(successMsgText);
+  //           }
+  //         });
+  //       });
+  //     });
+  //   });
+  // });
 
   // Add lax to Drag and Drop
   if (document.body.classList.contains('drag-and-drop')) {
@@ -597,6 +597,29 @@ jQuery(document).ready(function() {
 
       window.requestAnimationFrame(updateLax)
     }
+  }
+
+  if (document.body.classList.contains('single')) {
+    function onScroll() {
+      let veroPromoSticky = document.querySelector('.js-vero-promo-sticky');
+
+      let scrollTop = window.scrollY;
+      let docHeight = document.body.offsetHeight;
+      let winHeight = window.innerHeight;
+      let scrollPercent = scrollTop / (docHeight - winHeight);
+      let scrollPercentRounded = Math.round(scrollPercent * 100);
+
+      // console.log(scrollPercentRounded);
+
+      if (scrollPercentRounded >= 25) {
+        // alert('Greater than 25');
+        veroPromoSticky.classList.add('is-active');
+      } else {
+        veroPromoSticky.classList.remove('is-active');
+      }
+    }
+
+    window.addEventListener('scroll', onScroll)
   }
 
 });
