677cf324a4b6c751a741b735b5989c145a9a65f0
kachunchau <kachun@kachunchau.com>
2020-09-09 15:18:18 +0800

    include demo select for individual validation

--- assets/dev/scripts/demo.js
+++ assets/dev/scripts/demo.js
@@ -4,13 +4,15 @@ jQuery(document).ready(function() {
     errorElement: 'span'
   });
 
-  jQuery.each(jQuery('.js-demo-form input'), function(index, control) {
+  jQuery.each(jQuery('.js-demo-form input, .js-demo-form select'), function(index, control) {
     jQuery(control).focusout(function() {
-      if (jQuery('.js-demo-form').validate().element(this)) {
-          //Code for show a valid message or remove a invalid message
-      } else {
-          //Code for show a invalid message
-      }
+      // if (jQuery('.js-demo-form').validate().element(this)) {
+      //   // Code for show a valid message or remove a invalid message
+      // } else {
+      //   // Code for show a invalid message
+      // }
+
+      jQuery('.js-demo-form').validate().element(this);
     });
   });
 });
