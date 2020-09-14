db5f4a6e9118171dcf71b9eb539e76815d6b28d7
kachunchau <kachun@kachunchau.com>
2020-09-14 14:56:25 +0800

    move demo form script back to landing

--- assets/dev/scripts/demo.js
+++ assets/dev/scripts/demo.js
@@ -15,29 +15,4 @@ jQuery(document).ready(function() {
       jQuery('.js-demo-form').validate().element(this);
     });
   });
-
-  jQuery('.append-user-id').each(function(index) {
-    var self = this;
-
-    try {anonymous_id = window.analytics._user.anonymousId();}
-    catch {}
-
-    if(typeof anonymous_id !== 'undefined'){
-      // Append the Segment.com anonymous_id
-      jQuery('<input />').attr('type', 'hidden')
-      .attr('name', 'anonymous_id')
-      .attr('value', anonymous_id)
-      .appendTo(self);
-    }
-
-    // Append page path and URL
-    jQuery('<input />').attr('type', 'hidden')
-      .attr('name', 'page_path')
-      .attr('value', window.location.pathname)
-      .appendTo(self);
-    jQuery('<input />').attr('type', 'hidden')
-      .attr('name', 'page_url')
-      .attr('value', window.location.href)
-      .appendTo(self);
-  });
 });
