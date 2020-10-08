ff696ed70c0d8a2735184cf4ee21d5da0a52633f
kachunchau <kachun@kachunchau.com>
2020-10-08 17:37:14 +0800

    Fix dropdown

--- lib/configuration/headers.php
+++ lib/configuration/headers.php
@@ -104,7 +104,10 @@ function add_logo_and_menu_toggle_to_navbar($menu, $args) {
 
   ob_start();
   ?>
-  <div class="js-nav-primary-toggle nav-primary-toggle border-radius-1 semi-bold">Menu</div>
+  <input class="nav-primary-check hide" id="nav-primary-toggle" type="checkbox">
+  <label class="nav-primary-label show lg-hide z-999 left-margin-auto" for="nav-primary-toggle">
+    <svg class="align-middle" width="64" height="32" xmlns="http://www.w3.org/2000/svg"><defs/><g fill="none" fill-rule="evenodd"><path fill="none" d="M0 0h32v32H0z"/><path d="M25 21a1 1 0 010 2H7a1 1 0 010-2h18zm0-6a1 1 0 010 2H7a1 1 0 010-2h18zm0-6a1 1 0 010 2H7a1 1 0 010-2h18z" fill="#9D9D9D"/><g><path fill="none" d="M32 0h32v32H32z"/><path d="M55.071 8.929a1 1 0 010 1.414L49.414 16l5.657 5.657a1 1 0 01-1.414 1.414L48 17.414l-5.657 5.657a1 1 0 01-1.414-1.414L46.586 16l-5.657-5.657a1 1 0 011.414-1.414L48 14.586l5.657-5.657a1 1 0 011.414 0z" fill="#9D9D9D"/></g></g></svg>
+  </label>
   <?php $responsive_toggle = ob_get_clean();
 
   ob_start();
@@ -113,21 +116,21 @@ function add_logo_and_menu_toggle_to_navbar($menu, $args) {
     <svg class="show" width="20" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M13.079 14.49A8.001 8.001 0 0 1 2.515 2.516 8 8 0 0 1 14.49 13.079l.045.042 4.95 4.95a1 1 0 0 1-1.415 1.414l-4.95-4.95a1.015 1.015 0 0 1-.042-.044zm-.665-2.076A6 6 0 1 0 3.93 3.93a6 6 0 0 0 8.485 8.485z" fill="#9D9D9D" fill-rule="nonzero"/></svg>
   </label>
   <div class="js-category-toggle category-toggle">
-    <svg width="20" height="14" xmlns="http://www.w3.org/2000/svg"><path d="M1 0h18a1 1 0 0 1 0 2H1a1 1 0 0 1 0-2zm0 6h18a1 1 0 0 1 0 2H1a1 1 0 0 1 0-2zm0 6h18a1 1 0 0 1 0 2H1a1 1 0 0 1 0-2z" fill="#9D9D9D" fill-rule="nonzero"/></svg>
+    <svg class="align-middle" width="32" height="32" xmlns="http://www.w3.org/2000/svg"><defs/><g fill="none" fill-rule="evenodd"><path fill="none" d="M0 0h32v32H0z"/><path d="M25 21a1 1 0 010 2H7a1 1 0 010-2h18zm0-6a1 1 0 010 2H7a1 1 0 010-2h18zm0-6a1 1 0 010 2H7a1 1 0 010-2h18z" fill="#9D9D9D"/></g></svg>
   </div>
   <?php $category_toggle = ob_get_clean();
 
   if ( is_blog_archive() || is_single() ) {
     return $logo . $resources_menu . $category_toggle;
   } else {
-    return $logo . $menu . $responsive_toggle;
+    return $logo . $responsive_toggle . $menu;
   }
 }
 
 // Add description to submenu
 function add_menu_description( $item_output, $item ) {
   if ( !empty( $item->description ) ) {
-    $item_output = str_replace( $args->link_after . '</a>', '<div class="annotation light" style="flex: 1 1 100%">' . $item->description . '</div>' . $args->link_after . '</a>', $item_output );
+    $item_output = str_replace( $args->link_after . '</a>', '<div class="menu-item-desc annotation light">' . $item->description . '</div>' . $args->link_after . '</a>', $item_output );
   }
 
   return $item_output;
