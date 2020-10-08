f1cb4301a8fa640d926b10981eed8087c2f02a7f
kachunchau <kachun@kachunchau.com>
2020-10-08 15:53:33 +0800

    remove legacy js

--- assets/dev/scripts/core.js
+++ assets/dev/scripts/core.js
@@ -8,26 +8,3 @@ jQuery(window).scroll(function() {
     header.removeClass('sticky');
   }
 });
-
-jQuery(document).ready(function() {
-  // Responsive menu
-  jQuery('.js-nav-primary-toggle').on( 'click', function() {
-    var textcont =  jQuery( '.nav-primary-toggle' ).text();
-
-    if(textcont == 'Menu'){
-      jQuery('#menu-marketing-navbar-right').slideDown();
-      jQuery('.nav-primary-toggle').text('Close');
-    }
-    else if(textcont == 'Close'){
-      jQuery('#menu-marketing-navbar-right').slideUp();
-      jQuery('.nav-primary-toggle').text('Menu');
-      jQuery('#menu-marketing-navbar-right').addClass('slid');
-    }
-  });
-
-  jQuery(window).resize(function(){
-    if (jQuery('.nav-menu-toggle a').text() === 'Menu'){
-      jQuery( '#menu-marketing-navbar-right').removeAttr('style');
-    }
-  });
-});
