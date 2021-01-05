8b7fb0f0793d52e11df9787798343a5d7c99dacb
kachunchau <kachun@kachunchau.com>
2021-01-05 11:11:03 +0800

    update careers page

--- page-careers.php
+++ page-careers.php
@@ -12,40 +12,70 @@
   do_action( 'genesis_header' );
   do_action( 'genesis_after_header' );
 ?>
-<section class="hero double-padding font-white" id="careers-hero">
+<section class="hero double-padding">
+  <div class="inner stack-md center-text">
+    <div class="stack-xs">
+      <h1 class="hulk semi-bold">Working at Vero</h3>
+      <p class="large">We've come far. With your help we’ll go further.</p>
+    </div>
+
+    <a class="btn btn--large btn--secondary btn--outline" href="#careers-job-listings">View current openings</a>
+  </div>
+</section>
+<section>
+  <div class="inner xlarge-inner">
+    <div class="w-sidebar w-sidebar--md items-center">
+      <div class="col-main order-1">
+        <h2 class="chunk semi-bold">We believe data is changing our world</h2>
+        <p class="medium">Collecting realtime data and using it responsibly to create products and messages that deliver <strong>more value</strong> is the key to success for the next generation of products. At Vero, you'll help Product Teams harness the power of their data to <strong>create</strong> message and product experiences that matter.</p>
+      </div>
+      <div class="col-aside order-0">
+        <div class="grid careers-photo-wall">
+          <div>
+            <img class="align-middle full-width" src="/wp-content/themes/vero/assets/dist/images/careers/placeholders.jpg" alt="">
+          </div>
+          <div>
+            <img class="align-middle" src="/wp-content/themes/vero/assets/dist/images/careers/placeholders.jpg" alt="">
+          </div>
+          <div>
+            <img class="align-middle" src="/wp-content/themes/vero/assets/dist/images/careers/placeholders.jpg" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<section class="double-padding">
   <div class="inner">
-    <h1 class="hulk bold no-margin-top">We've come far.<br/>With your help we’ll go further.</h1>
-    <a class="btn btn--white btn--outline" data-scroll href="#careers-job-listings">View current openings</a>
-  </div>
-</section>
-<section id="careers-mission" class="double-padding">
-  <div class="inner small-inner flush-bottom">
-    <h2 class="chunk regular">We believe data is changing our world</h2>
-    <p class="medium">Collecting realtime data and using it responsibly to create products and messages that deliver <strong>more value</strong> is the key to success for the next generation of products. At Vero, you'll help Product Teams harness the power of their data to <strong>create</strong> message and product experiences that matter.</p>
-  </div>
-</section>
-<section class="double-padding" id="careers-why">
-  <div class="inner small-inner flush-bottom">
-    <h2 class="chunk regular no-margin bottom-margin-xl">Be empowered to do your best work</h2>
-  </div>
-  <div class="inner large-inner flush-top flush-bottom">
-    <ul class="unstyled-list grid grid-thirds">
-      <li class="no-margin">
-        <img class="bottom-margin-sm" src="/wp-content/themes/vero/assets/dist/images/careers/rubix.svg" alt="work on hard problems">
-        <h3 class="atomic semi-bold">Work on hard problems</h3>
-        <p>Smart people should work on the hardest problems. At Vero, you'll have the opportunity to tackle the kind of challenges that make work interesting.</p>
-      </li>
-      <li class="no-margin">
-        <img class="bottom-margin-sm" src="/wp-content/themes/vero/assets/dist/images/careers/meteor.svg" alt="Make an impact">
-        <h3 class="atomic semi-bold">Make an impact</h3>
-        <p>We believe in quality over quantity. As a young company with a growing team you will have the opportunity to make a big impact on all aspects of Vero.</p>
-      </li>
-      <li class="no-margin">
-        <img class="bottom-margin-sm" src="/wp-content/themes/vero/assets/dist/images/careers/grow.svg" alt="Opportunities to grow">
-        <h3 class="atomic semi-bold">Opportunities to grow</h3>
-        <p>We believe in growing and moving forward, as a company and as individuals. Our goal is to enable you to seek the knowledge and learn the skills that make you a better human.</p>
-      </li>
-    </ul>
+    <div class="w-sidebar w-sidebar--md">
+      <div class="col-main">
+        <h2 class="tubs semi-bold">Live to work, not the other way around</h2>
+
+        <p class="medium">We believe that happy teams are the key to a powerful business. At Vero we are an empowered group of people who accomplish a lot. By helping each other to overcome any challenge that comes our way we have built a company that thrives on trust, positivity and a pinch of adventure.</p>
+      </div>
+      <div class="col-aside md-left-padding-xl">
+        <ul class="unstyled-list stack-md careers-empowered-list">
+          <li class="flex">
+            <div>
+              <h3 class="atomic semi-bold">Work on hard problems</h3>
+              <p>Smart people should work on the hardest problems. At Vero, you'll have the opportunity to tackle the kind of challenges that make work interesting.</p>
+            </div>
+          </li>
+          <li class="flex">
+            <div>
+              <h3 class="atomic semi-bold">Make an impact</h3>
+              <p>We believe in quality over quantity. As a young company with a growing team you will have the opportunity to make a big impact on all aspects of Vero.</p>
+            </div>
+          </li>
+          <li class="flex">
+            <div>
+              <h3 class="atomic semi-bold">Opportunities to grow</h3>
+              <p>We believe in growing and moving forward, as a company and as individuals. Our goal is to enable you to seek the knowledge and learn the skills that make you a better human.</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </section>
 <section id="careers-vibe" class="double-padding">
@@ -60,10 +90,10 @@
     <div class="vibe-image" id="boat"></div>
   </div>
 </section>
-<section id="careers-profiles" class="double-padding">
-  <ul class="" id="careers-profiles-slider">
-    <li class="rory">
-      <div class="inner">
+<section class="double-padding bg-dark-blue-lighter">
+  <div class="inner">
+    <ul class="unstyled-list">
+      <li class="rory">
         <h3>Do exceptional work with extraordinary people</h3>
         <p>
           <q>Every day I get the chance to work with amazing people to help improve what is a mission critical product for many of our customers. Our customers are very passionate about what they do so each new release makes a difference.</q>
@@ -77,10 +107,9 @@
           <div class="bx-control bx-active" data-bx="0"></div>
           <div class="bx-control" data-bx="1"></div>
         </div>
-      </div>
-    </li>
-    <li class="reba">
-      <div class="inner">
+
+      </li>
+      <li>
         <h3>Solve challenging problems for interesting customers</h3>
         <p>
           <q>There's nothing more satisfying than cracking a hard problem for a customer. At Vero I get to work side by side with some of the best companies in the industry, where every day is different.</q>
@@ -94,14 +123,12 @@
           <div class="bx-control" data-bx="0"></div>
           <div class="bx-control bx-active" data-bx="1"></div>
         </div>
-      </div>
-    </li>
-  </ul>
-</section>
-<section class="double-padding bg-blue-light" id="careers-perks">
-  <div class="inner blue-box">
+      </li>
+    </ul>
+  </div>
+  <div class="inner">
     <h2 class="chunk regular md-center">Not just work life, real life</h2>
-    <ul class="feature-list unstyled-list grid">
+    <ul class="unstyled-list grid grid--thirds">
       <li>
         <img src="/wp-content/themes/vero/assets/dist/images/careers/remote.svg" alt="Remote-first">
         <p class="semi-bold">Remote-First</p>
@@ -130,47 +157,13 @@
     </ul>
   </div>
 </section>
-<section id="careers-remote" class="double-padding">
+<section class="double-padding">
   <div class="inner small-inner bottom-padding-xs">
     <h2 class="chunk regular">Be where you want to be</h2>
     <p class="medium">Living and traveling to different countries is a valuable and pleasurable part of life. At Vero, you are free to live and work wherever you want. We hire from countries around the world and we encourage people to travel and try something new. Here’s some of the locations Vero team members currently reside:</p>
   </div>
-  <div class="inner xlarge-inner flush-top flush-bottom">
-    <ul class="feature-list grid grid-sixths unstyled-list center-text">
-      <li>
-        <img src="/wp-content/themes/vero/assets/dist/images/careers/locations/sydney.png" srcset="/wp-content/themes/vero/assets/dist/images/careers/locations/sydney@2x.png 2x" alt="Sydney, Australia">
-        <p class="semi-bold no-bottom-margin">Sydney</p>
-        <p class="no-margin small">Australia</p>
-      </li>
-      <li>
-        <img src="/wp-content/themes/vero/assets/dist/images/careers/locations/florida.png" srcset="/wp-content/themes/vero/assets/dist/images/careers/locations/florida@2x.png 2x" alt="Florida, USA">
-        <p class="semi-bold no-bottom-margin">Florida</p>
-        <p class="no-margin small">USA</p>
-      </li>
-      <li>
-        <img src="/wp-content/themes/vero/assets/dist/images/careers/locations/hongkong.png" srcset="/wp-content/themes/vero/assets/dist/images/careers/locations/hongkong@2x.png 2x" alt="Hong Kong, Hong Kong">
-        <p class="semi-bold no-bottom-margin">Hong Kong</p>
-        <p class="no-margin small">Hong Kong</p>
-      </li>
-      <li>
-        <img src="/wp-content/themes/vero/assets/dist/images/careers/locations/brisbane.png" srcset="/wp-content/themes/vero/assets/dist/images/careers/locations/brisbane@2x.png 2x" alt="Brisbane, Australia">
-        <p class="semi-bold no-bottom-margin">Brisbane</p>
-        <p class="no-margin small">Australia</p>
-      </li>
-      <li>
-        <img src="/wp-content/themes/vero/assets/dist/images/careers/locations/amsterdam.png" srcset="/wp-content/themes/vero/assets/dist/images/careers/locations/amsterdam@2x.png 2x" alt="Amsterdam, Netherlands">
-        <p class="semi-bold no-bottom-margin">Amsterdam</p>
-        <p class="no-margin small">Netherlands</p>
-      </li>
-      <li>
-        <img src="/wp-content/themes/vero/assets/dist/images/careers/locations/la.png" srcset="/wp-content/themes/vero/assets/dist/images/careers/locations/la@2x.png 2x" alt="Amsterdam, Netherlands">
-        <p class="semi-bold no-bottom-margin">Los Angeles</p>
-        <p class="no-margin small">USA</p>
-      </li>
-    </ul>
-  </div>
 </section>
-<section class="double-padding" id="careers-job-listings">
+<section class="double-padding">
   <div class="inner small-inner">
     <h2 class="chunk regular">See where you fit in</h2>
 
