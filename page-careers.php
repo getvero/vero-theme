2f40f343afb12a6d98a0e55c840f18f3c0840aa6
kachunchau <kachun@kachunchau.com>
2021-01-14 17:33:49 +0800

    modify caurosel to use radio inputs

--- page-careers.php
+++ page-careers.php
@@ -12,7 +12,7 @@
   do_action( 'genesis_header' );
   do_action( 'genesis_after_header' );
 ?>
-<section class="hero double-padding">
+<section class="double-padding">
   <div class="inner stack-md center-text">
     <div class="stack-xs">
       <h1 class="hulk semi-bold">Working at Vero</h3>
@@ -23,7 +23,7 @@
   </div>
 </section>
 <section>
-  <div class="inner xlarge-inner">
+  <div class="inner xlarge-inner flush-top">
     <div class="w-sidebar w-sidebar--md items-center">
       <div class="col-main order-1">
         <h2 class="chunk semi-bold">We believe data is changing our world</h2>
@@ -78,25 +78,15 @@
     </div>
   </div>
 </section>
-<section id="careers-vibe" class="double-padding">
-  <div class="inner small-inner bottom-padding-lg">
-    <h2 class="chunk regular">Live to work, not the other way around</h2>
-    <p class="medium">We believe that happy teams are the key to a powerful business. At Vero we are an empowered group of people who accomplish a lot. By helping each other to overcome any challenge that comes our way we have built a company that thrives on trust, positivity and a pinch of adventure.</p>
-  </div>
-  <div class="inner flush-top large-inner vibe-images">
-    <div class="vibe-image" id="beach"></div>
-    <div class="vibe-image" id="cafe"></div>
-    <div class="vibe-image" id="bar"></div>
-    <div class="vibe-image" id="boat"></div>
-  </div>
-</section>
 <section class="double-padding bg-dark-blue-lighter">
   <div class="inner halfs">
     <div class="careers-slider">
-      <div class="careers-slides flex">
-        <div class="jump-link careers-slide flex" id="rory">
+      <input class="careers-radio-1" id="careers-radio-1" type="radio" name="careers-radios" checked>
+      <input class="careers-radio-2" id="careers-radio-2" type="radio" name="careers-radios">
+
+      <div class="careers-slides">
+        <div class="careers-slide careers-slide--rory" data-slide="rory">
           <div class="col-main stack-md">
-            <h3>Do exceptional work with extraordinary people</h3>
             <blockquote>
               <q class="large">Every day I get the chance to work with amazing people to help improve what is a mission critical product for many of our customers. Our customers are very passionate about what they do so each new release makes a difference.</q>
             </blockquote>
@@ -111,9 +101,8 @@
             <img class="align-middle  border-radius-2" src="/wp-content/themes/vero/assets/dist/images/careers/rory.jpg" srcset="/wp-content/themes/vero/assets/dist/images/careers/rory@2x.jpg 2x" alt="Rory">
           </div>
         </div>
-        <div class="jump-link careers-slide flex" id="reba">
+        <div class="careers-slide careers-slide--reba" data-slide="reba">
           <div class="col-main stack-md">
-            <h3>Solve challenging problems for interesting customers</h3>
             <blockquote>
               <q class="large">There's nothing more satisfying than cracking a hard problem for a customer. At Vero I get to work side by side with some of the best companies in the industry, where every day is different.</q>
             </blockquote>
@@ -131,8 +120,12 @@
       </div>
 
       <ul class="careers-slider-nav unstyled-list flex">
-        <li class="no-margin"><a class="rory border-radius-100" href="#rory"></a></li>
-        <li class="no-margin"><a class="reba border-radius-100" href="#reba"></a></li>
+        <li class="no-margin">
+          <label for="careers-radio-1">Rory</label>
+        </li>
+        <li class="no-margin">
+          <label for="careers-radio-2">Reba</label>
+        </li>
       </ul>
     </div>
   </div>
@@ -142,32 +135,50 @@
       <li class="stack-sm">
         <svg class="align-middle" width="56" height="56" xmlns="http://www.w3.org/2000/svg"><defs/><g fill="none" fill-rule="evenodd"><circle stroke="#01B2D0" stroke-width="2" cx="28" cy="28" r="23"/><path d="M28 14c4.97 0 9 3.76 9 8.4 0 3.093-3 9.626-9 19.6-6-9.974-9-16.507-9-19.6 0-4.64 4.03-8.4 9-8.4zm0 6a3 3 0 100 6 3 3 0 000-6z" fill="#01B2D0" opacity=".4"/></g></svg>
 
-        <h3 class="atomic semi-bold">Remote-First</h3>
-        <p>Embracing global culture and opportunity, you are free to live and work where you want in the world, whether it's our HQ in Sydney, or a cabin in the woods.</p>
+        <div class="stack-xxxs">
+          <h3 class="atomic semi-bold">Remote-First</h3>
+          <p>Embracing global culture and opportunity, you are free to live and work where you want in the world, whether it's our HQ in Sydney, or a cabin in the woods.</p>
+        </div>
       </li>
       <li class="stack-sm">
         <svg class="align-middle" width="56" height="56" xmlns="http://www.w3.org/2000/svg"><defs/><defs><circle id="a" cx="28" cy="28" r="24"/></defs><g fill="none" fill-rule="evenodd"><rect fill="#F7DF9B" x="27" y="12" width="2" height="18" rx="1"/><rect fill="#F7DF9B" transform="rotate(-90 34 29)" x="33" y="22" width="2" height="14" rx="1"/><circle stroke="#FFC111" stroke-width="2" cx="28" cy="28" r="23"/></g></svg>
 
-        <h3 class="atomic semi-bold">Flexibility</h3>
-        <p>We know people have different needs and work in different ways. We believe in asynchronous work and invest in processes that enable you to work the way that suits you.</p>
+        <div class="stack-xxxs">
+          <h3 class="atomic semi-bold">Flexibility</h3>
+          <p>We know people have different needs and work in different ways. We believe in asynchronous work and invest in processes that enable you to work the way that suits you.</p>
+        </div>
       </li>
       <li class="stack-sm">
         <svg class="align-middle" width="56" height="56" xmlns="http://www.w3.org/2000/svg"><defs/><g stroke="#6FD350" stroke-width="2" fill="none" fill-rule="evenodd"><path d="M15.02 11.53a.997.997 0 011.22.48h0l13.344 25.527a.997.997 0 01-.423 1.35h0L3.171 39a.997.997 0 01-1-1h0l12.28-25.958a.997.997 0 01.57-.512z"/><path d="M39.02 11.53a.997.997 0 011.22.48h0l13.344 25.527a.997.997 0 01-.423 1.35h0L27.171 39a.997.997 0 01-1-1h0l12.28-25.958a.997.997 0 01.57-.512z"/><path d="M26.702 7.53a.997.997 0 011.22.48h0l20.662 39.527a.997.997 0 01-.423 1.35h0L8.171 49a.997.997 0 01-1-1h0L26.133 8.042a.997.997 0 01.57-.512z" fill="#F3F4F7"/><path stroke-linecap="round" stroke-linejoin="round" d="M19.613 23l4 7 4-7 4 7 4-7"/></g></svg>
 
-        <h3 class="atomic semi-bold">Yearly Adventures</h3>
-        <p>Once a year we go on a team retreat somewhere exciting, challenging and inspiring. For our last trip we went to Thailand, we had a blast!</p>
+        <div class="stack-xxxs">
+          <h3 class="atomic semi-bold">Yearly Adventures</h3>
+          <p>Once a year we go on a team retreat somewhere exciting, challenging and inspiring. For our last trip we went to Thailand, we had a blast!</p>
+        </div>
       </li>
       <li class="stack-sm">
         <svg class="align-middle" width="56" height="56" xmlns="http://www.w3.org/2000/svg"><defs/><path d="M20 11a9.967 9.967 0 016.977 2.836 9.969 9.969 0 013.02 6.89h0L30 27h5.986c2.826.074 5.324 1.205 7.119 3A9.85 9.85 0 0146 37a9.967 9.967 0 01-2.836 6.977 9.969 9.969 0 01-6.889 3.02h0L10 47V21.013c.074-2.826 1.205-5.324 3-7.119A9.85 9.85 0 0120 11z" stroke="#6F0DFB" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>
 
-        <h3 class="atomic semi-bold">Health Conscious</h3>
-        <p>We believe in a healthy working environment. Sit-stand desks are provided, healthy snacks are on offer and our global team regularly discusses their exercise.</p>
+        <div class="stack-xxxs">
+          <h3 class="atomic semi-bold">Health Conscious</h3>
+          <p>We believe in a healthy working environment. Sit-stand desks are provided, healthy snacks are on offer and our global team regularly discusses their exercise.</p>
+        </div>
       </li>
       <li class="stack-sm">
-        <svg class="align-middle" width="56" height="56" xmlns="http://www.w3.org/2000/svg"><defs/><path d="M20 11a9.967 9.967 0 016.977 2.836 9.969 9.969 0 013.02 6.89h0L30 27h5.986c2.826.074 5.324 1.205 7.119 3A9.85 9.85 0 0146 37a9.967 9.967 0 01-2.836 6.977 9.969 9.969 0 01-6.889 3.02h0L10 47V21.013c.074-2.826 1.205-5.324 3-7.119A9.85 9.85 0 0120 11z" stroke="#6F0DFB" stroke-width="2" fill="none" fill-rule="evenodd"/></svg>
+        <svg class="align-middle" width="56" height="56" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><circle stroke="#F15242" stroke-width="2" cx="18" cy="13" r="7"/><rect stroke="#F15242" stroke-width="2" fill="#F3F4F7" x="19" y="14" width="22" height="36" rx="2"/><path fill-opacity=".4" fill="#F15242" d="M20 23h20v2H20z"/></g></svg>
 
-        <h3 class="atomic semi-bold">Team Activities</h3>
-        <p>From doing the Friday crossword to running and indoor climbing, we flex our brains and bodies in different ways, always with a little competition thrown in.</p>
+        <div class="stack-xxxs">
+          <h3 class="atomic semi-bold">Culture</h3>
+          <p>We are a semi-distrubuted team so try and keep social by using our non-work related Slack channel with Among Us and regular Zoom happy hours.</p>
+        </div>
+      </li>
+      <li class="stack-sm">
+        <svg class="align-middle" width="56" height="56" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><circle stroke="#F15242" stroke-width="2" cx="18" cy="13" r="7"/><rect stroke="#F15242" stroke-width="2" fill="#F3F4F7" x="19" y="14" width="22" height="36" rx="2"/><path fill-opacity=".4" fill="#F15242" d="M20 23h20v2H20z"/></g></svg>
+
+        <div class="stack-xxxs">
+          <h3 class="atomic semi-bold">Team Activities</h3>
+          <p>From doing the Friday crossword to running and indoor climbing, we flex our brains and bodies in different ways, always with a little competition thrown in.</p>
+        </div>
       </li>
     </ul>
   </div>
@@ -180,6 +191,8 @@
 </section>
 <section class="double-padding">
   <div class="inner small-inner">
+    <h2>Be where you want to be</h2>
+
     <h2 class="chunk regular">See where you fit in</h2>
 
     <p class="medium bottom-margin-md">Join us on our mission to empower businesses to put their data to work to create great product experiences.</p>
