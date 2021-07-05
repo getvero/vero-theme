7c19673ae49741eb981c5b0902cfcc3a8fabaee2
Chris Hexton <chris+github@getvero.com>
2021-07-05 11:25:02 +1000

    remove recaptcha properly

--- src/lib/configuration/assets.php
+++ src/lib/configuration/assets.php
@@ -11,12 +11,6 @@ function defer_parsing_of_js( $url ) {
   return str_replace( ' src', ' defer src', $url );
 }
 
-// function move_recaptcha_script() {
-//   ?>
-//   <script src="https://www.google.com/recaptcha/api.js?render=6LfUD_YUAAAAAO5FOQgHwsQSEMzOZYEPHEo_DZRX" defer async></script>
-//   <?php
-// }
-
 function remove_ver_css_js( $src ) {
   if ( strpos( $src, 'ver=' ) )
     $src = remove_query_arg( 'ver', $src );
